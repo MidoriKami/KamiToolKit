8f495bb90e47f9286bd4cd221e6e793acae2d1ce
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2024-06-03 18:15:21 -0700

    Find parent addon dynamically by searching up the list

--- Nodes/NodeBase/NodeBase.AttachNode.cs
+++ Nodes/NodeBase/NodeBase.AttachNode.cs
@@ -20,8 +20,9 @@ public abstract unsafe partial class NodeBase<T> where T : unmanaged, ICreatable
     private IAddonLifecycle? addonLifecycleService;
     
     // Attaches a self-removing node to an addon, when the addon finalizes, or the plugin unloads, attached nodes will automatically be removed.
-    public void AttachNodeSafe(IAddonLifecycle addonLifecycle, void* addon, AtkResNode* targetNode, NodePosition position) {
-        var atkUnitBase = (AtkUnitBase*) addon;
+    public void AttachNode(IAddonLifecycle addonLifecycle, AtkResNode* targetNode, NodePosition position) {
+        var atkUnitBase = GetAddonForNode(targetNode);
+        if (atkUnitBase is null) return;
 
         addonLifecycleService ??= addonLifecycle;
         
@@ -32,6 +33,43 @@ public abstract unsafe partial class NodeBase<T> where T : unmanaged, ICreatable
     private void AutomaticNodeCleanup(AddonEvent type, AddonArgs args) {
         addonLifecycleService!.UnregisterListener(AutomaticNodeCleanup);
         Dispose();
+    }
+    
+    /// <summary>
+    /// Searches all loaded addons and all nodes contained within to find the addon that owns the targed node.
+    /// </summary>
+    /// <param name="node">The node to search for</param>
+    /// <returns>AtkUnitBase* to the addon </returns>
+    private static AtkUnitBase* GetAddonForNode(AtkResNode* node) {
+        foreach (ref readonly var unit in AtkStage.Instance()->RaptureAtkUnitManager->AllLoadedUnitsList.Entries) {
+            if (unit.Value is null) continue;
+            
+            if (AddonContainsNode(ref unit.Value->UldManager, node)) {
+                return unit.Value;
+            }
+        }
+
+        return null;
+    }
+
+    private static bool AddonContainsNode(ref AtkUldManager uldManager, AtkResNode* targetNode) {
+        var nodeListCount = uldManager.NodeListCount;
+        
+        for (var index = 0; index < nodeListCount; ++index) {
+            ref var currentNode = ref uldManager.NodeList[index];
+            if (targetNode == currentNode) return true;
+
+            if ((uint) currentNode->Type > 1000u) {
+                var componentNode = ((AtkComponentNode*) currentNode)->Component;
+                if (componentNode is not null) {
+                    if (AddonContainsNode(ref componentNode->UldManager, targetNode)) {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
     }
     
     private void AttachNode(AtkUnitBase* parent, AtkResNode* targetNode, NodePosition position) {
