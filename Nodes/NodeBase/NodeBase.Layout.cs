e96881955d0ec45c848dc77e8e1f19f06cef9724
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2024-06-04 17:41:14 -0700

    Removed Padding and LayoutSize

--- Nodes/NodeBase/NodeBase.Layout.cs
+++ Nodes/NodeBase/NodeBase.Layout.cs
@@ -1,6 +1,4 @@
-﻿using System.Numerics;
-
-namespace KamiToolKit;
+﻿namespace KamiToolKit;
 
 public record Spacing(float Top, float Left, float Right, float Bottom) {
     public Spacing(float allSides) : this(allSides, allSides, allSides, allSides) { }
@@ -8,8 +6,4 @@ public record Spacing(float Top, float Left, float Right, float Bottom) {
 
 public abstract unsafe partial class NodeBase {
     public Spacing Margin { get; set; } = new(0.0f);
-    
-    public Spacing Padding { get; set; } = new(0.0f);
-
-    public Vector2 LayoutSize => Size;
 }
