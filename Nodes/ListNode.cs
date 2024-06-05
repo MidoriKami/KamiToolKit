87031d5c53f48ce4d6f16ee495a702e568d00272
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2024-06-04 17:41:06 -0700

    Rename Padding to Margin

--- Nodes/ListNode.cs
+++ Nodes/ListNode.cs
@@ -8,7 +8,7 @@ namespace KamiToolKit;
 
 // Custom Implementation of a Node that contains other nodes
 public unsafe class ListNode() : NodeBase<AtkResNode>(NodeType.Res) {
-    private List<NodeBase> nodeList = [];
+    private readonly List<NodeBase> nodeList = [];
     
     public required LayoutAnchor LayoutAnchor { get; set; }
 
@@ -33,28 +33,30 @@ public unsafe class ListNode() : NodeBase<AtkResNode>(NodeType.Res) {
         var runningPosition = GetLayoutStartPosition();
 
         foreach (var node in nodeList) {
+            if (!node.IsVisible) continue;
+            
             switch (LayoutAnchor) {
                 case LayoutAnchor.TopLeft: {
-                    node.Position = runningPosition + new Vector2(node.Padding.Left, node.Padding.Top);
-                    runningPosition.Y += node.Height + node.Padding.Bottom + node.Padding.Top;
+                    node.Position = runningPosition + new Vector2(node.Margin.Left, node.Margin.Top);
+                    runningPosition.Y += node.Height + node.Margin.Bottom + node.Margin.Top;
                     break;
                 }
 
                 case LayoutAnchor.TopRight: {
-                    node.Position = runningPosition - new Vector2(node.Padding.Right, 0.0f) + new Vector2(0.0f, node.Padding.Top) - new Vector2(node.Width, 0.0f);
-                    runningPosition.Y += node.Height + node.Padding.Bottom + node.Padding.Top;
+                    node.Position = runningPosition - new Vector2(node.Margin.Right, 0.0f) + new Vector2(0.0f, node.Margin.Top) - new Vector2(node.Width, 0.0f);
+                    runningPosition.Y += node.Height + node.Margin.Bottom + node.Margin.Top;
                     break;
                 }
                 
                 case LayoutAnchor.BottomLeft: {
-                    node.Position = runningPosition + new Vector2(node.Padding.Left, 0.0f) - new Vector2(0.0f, node.Padding.Bottom) - new Vector2(0.0f, node.Height);
-                    runningPosition.Y -= node.Height + node.Padding.Top + node.Padding.Bottom;
+                    node.Position = runningPosition + new Vector2(node.Margin.Left, 0.0f) - new Vector2(0.0f, node.Margin.Bottom) - new Vector2(0.0f, node.Height);
+                    runningPosition.Y -= node.Height + node.Margin.Top + node.Margin.Bottom;
                     break;
                 }
 
                 case LayoutAnchor.BottomRight: {
-                    node.Position = runningPosition - new Vector2(node.Padding.Right, 0.0f) - new Vector2(0.0f, node.Padding.Bottom) - new Vector2(node.Width, node.Height);
-                    runningPosition.Y -= node.Height + node.Padding.Top + node.Padding.Bottom;
+                    node.Position = runningPosition - new Vector2(node.Margin.Right, 0.0f) - new Vector2(0.0f, node.Margin.Bottom) - new Vector2(node.Width, node.Height);
+                    runningPosition.Y -= node.Height + node.Margin.Top + node.Margin.Bottom;
 
                     break;
                 }
