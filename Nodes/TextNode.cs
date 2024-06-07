7bceaff48a892dd100d80dffc2441d257f25e81e
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2024-06-07 15:23:09 -0700

    Remove SeString property from TextNode, treat all text assignments as SeStrings

--- Nodes/TextNode.cs
+++ Nodes/TextNode.cs
@@ -23,11 +23,6 @@ public unsafe class TextNode() : NodeBase<AtkTextNode>(NodeType.Text) {
         set => InternalNode->BackgroundColor = value.ToByteColor();
     }
 
-    public SeString SeString {
-        get => MemoryHelper.ReadSeStringNullTerminated((nint) InternalNode->GetText());
-        set => InternalNode->SetText(value.Encode());
-    }
-
     public uint SelectStart {
         get => InternalNode->SelectStart;
         set => InternalNode->SelectStart = value;
@@ -84,8 +79,8 @@ public unsafe class TextNode() : NodeBase<AtkTextNode>(NodeType.Text) {
     /// <summary>
     /// If you want the node to resize automatically, use TextFlags.AutoAdjustNodeSize <b><em>before</em></b> setting the String property.
     /// </summary>
-    public string String {
+    public SeString Text {
         get => MemoryHelper.ReadStringNullTerminated((nint) InternalNode->GetText());
-        set => InternalNode->SetText(value);
+        set => InternalNode->SetText(value.Encode());
     }
 }
