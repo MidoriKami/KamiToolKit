60fe2d7f43668e2d640aa97aedfef9295a488770
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2024-06-08 14:02:20 -0700

    Add Extension methods to make dealing with AddColor easier

---
+++ Extensions/KnownColorExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Drawing;
+using System.Numerics;
+using Dalamud.Interface;
+using Vector4 = System.Numerics.Vector4;
+
+namespace KamiToolKit.Extensions;
+
+public static class KnownColorExtensions {
+    public static Vector3 Vector3(this KnownColor color) {
+        var color4 = color.Vector();
+        return new Vector3(color4.X, color4.Y, color4.Z);
+    }
+
+    public static Vector3 AsVector3Color(this Vector4 vector4) {
+        return new Vector3(vector4.X, vector4.Y, vector4.Z);
+    }
+}
