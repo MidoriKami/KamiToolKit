f6c92b6ece28dc21dae7ef3333377b383fec9b4c
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2024-06-08 14:08:08 -0700

    ListNode.cs
    Implement IList inheritance
    Implement Horizontal Layout
    Implement background node, and pass-through properties

--- Nodes/ListNode.cs
+++ Nodes/ListNode.cs
@@ -1,37 +1,54 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Numerics;
 using FFXIVClientStructs.FFXIV.Component.GUI;
 using KamiToolKit.Classes;
-using KamiToolKit.Nodes.NodeBase;
+using KamiToolKit.Extensions;
 
 namespace KamiToolKit.Nodes;
 
 // Custom Implementation of a Node that contains other nodes
-public class ListNode() : NodeBase<AtkResNode>(NodeType.Res) {
-    private readonly List<NodeBase.NodeBase> nodeList = [];
+public class ListNode<T> : NodeBase<AtkResNode>, IList<T> where T : NodeBase {
+    private readonly List<T> nodeList = [];
+    private readonly ImageNode background;
+
+    private LayoutOrientation InternalLayoutOrientation { get; set; }
     
     public required LayoutAnchor LayoutAnchor { get; set; }
     
-    /// <summary>
-    /// Not implemented yet.
-    /// </summary>
-    public LayoutOrientation LayoutOrientation { get; set; }
-
-    public void AddNode(NodeBase.NodeBase node) {
-        node.AttachNode(this, NodePosition.AsLastChild);
-        nodeList.Add(node);
-
-        RecalculateLayout();
+    public LayoutOrientation LayoutOrientation {
+        get => InternalLayoutOrientation;
+        set {
+            InternalLayoutOrientation = value;
+            RecalculateLayout();
+        } 
     }
 
-    public void RemoveNode(NodeBase.NodeBase node) {
-        nodeList.Remove(node);
-        node.DetachNode();
-        
-        RecalculateLayout();
+    public Vector4 BackgroundColor {
+        get => new(background.AddColor.X, background.AddColor.Y, background.AddColor.Z, background.Color.W);
+        set {
+            background.Color = Vector4.Zero with { W = value.W };
+            background.AddColor = value.AsVector3Color();
+        }
+    }
+
+    public bool BackgroundVisible {
+        get => background.IsVisible;
+        set => background.IsVisible = value;
     }
     
+    public ListNode() : base(NodeType.Res) {
+        background = new ImageNode {
+            NodeID = 101_000,
+            Size = new Vector2(600.0f, 32.0f),
+            IsVisible = true,
+        };
+        
+        background.AttachNode(this, NodePosition.AsFirstChild);
+    }
+
     protected override void Dispose(bool isDisposing) {
         if (isDisposing) {
             foreach (var node in nodeList) {
@@ -42,9 +59,23 @@ public class ListNode() : NodeBase<AtkResNode>(NodeType.Res) {
         }
     }
 
-    private void RecalculateLayout() {
-        var runningPosition = GetLayoutStartPosition();
+    public void RecalculateLayout() {
+        switch (LayoutOrientation) {
+            case LayoutOrientation.Vertical:
+                CalculateVerticalLayout();
+                break;
+            
+            case LayoutOrientation.Horizontal:
+                CalculateHorizontalLayout();
+                break;
+        }
 
+        background.Size = Size;
+    }
+
+    private void CalculateVerticalLayout() {
+        var runningPosition = GetLayoutStartPosition();
+        
         foreach (var node in nodeList) {
             if (!node.IsVisible) continue;
             
@@ -76,6 +107,41 @@ public class ListNode() : NodeBase<AtkResNode>(NodeType.Res) {
             }
         }
     }
+    
+        
+    private void CalculateHorizontalLayout() {
+        var runningPosition = GetLayoutStartPosition();
+
+        foreach (var node in nodeList) {
+            if (!node.IsVisible) continue;
+            
+            switch (LayoutAnchor) {
+                case LayoutAnchor.TopLeft: {
+                    node.Position = runningPosition + new Vector2(node.Margin.Left, node.Margin.Top);
+                    runningPosition.X += node.Width * node.Scale.X + node.Margin.Right + node.Margin.Left;
+                    break;
+                }
+
+                case LayoutAnchor.TopRight: {
+                    node.Position = runningPosition - new Vector2(node.Margin.Right, 0.0f) + new Vector2(0.0f, node.Margin.Top) - new Vector2(node.Width * node.Scale.X, 0.0f);
+                    runningPosition.X -= node.Width * node.Scale.X + node.Margin.Left + node.Margin.Right;
+                    break;
+                }
+                
+                case LayoutAnchor.BottomLeft: {
+                    node.Position = runningPosition + new Vector2(node.Margin.Left, 0.0f) - new Vector2(0.0f, node.Margin.Bottom) - new Vector2(0.0f, node.Height * node.Scale.Y);
+                    runningPosition.X += node.Width * node.Scale.X + node.Margin.Left + node.Margin.Right;
+                    break;
+                }
+
+                case LayoutAnchor.BottomRight: {
+                    node.Position = runningPosition - new Vector2(node.Margin.Right, 0.0f) - new Vector2(0.0f, node.Margin.Bottom) - new Vector2(node.Width * node.Scale.X, node.Height * node.Scale.Y);
+                    runningPosition.X -= node.Width * node.Scale.X + node.Margin.Left + node.Margin.Right;
+                    break;
+                }
+            }
+        }
+    }
 
     private Vector2 GetLayoutStartPosition() => LayoutAnchor switch {
         LayoutAnchor.TopLeft => Vector2.Zero,
@@ -84,12 +150,94 @@ public class ListNode() : NodeBase<AtkResNode>(NodeType.Res) {
         LayoutAnchor.BottomRight => new Vector2(Width, Height),
         _ => throw new ArgumentOutOfRangeException()
     };
+
+    public IEnumerator<T> GetEnumerator() 
+        => nodeList.GetEnumerator();
+    
+    IEnumerator IEnumerable.GetEnumerator() 
+        => GetEnumerator();
+
+    public void Add(T item) {
+        item.AttachNode(this, NodePosition.AsLastChild);
+        nodeList.Add(item);
+        
+        RecalculateLayout();
+
+        unsafe {
+            if (AttachedAddonName is not null) {
+                var parent = AtkStage.Instance()->RaptureAtkUnitManager->GetAddonByName(AttachedAddonName);
+                if (parent is not null && parent->IsReady && parent->UldManager.LoadedState is AtkLoadState.Loaded) {
+                    parent->UpdateCollisionNodeList(false);
+                    parent->UldManager.UpdateDrawNodeList();
+                }
+            }
+        }
+    }
+    
+    public void Clear() {
+        foreach (var node in nodeList) {
+            node.DetachNode();
+        }
+        
+        nodeList.Clear();
+        RecalculateLayout();
+    }
+    
+    public bool Contains(T item) 
+        => nodeList.Contains(item);
+
+    public void CopyTo(T[] array, int arrayIndex)
+        => nodeList.CopyTo(array, arrayIndex);
+    
+    public bool Remove(T item) {
+        item.DetachNode();
+        nodeList.Remove(item);
+
+        unsafe {
+            if (AttachedAddonName is not null) {
+                var parent = AtkStage.Instance()->RaptureAtkUnitManager->GetAddonByName(AttachedAddonName);
+                if (parent is not null && parent->IsReady && parent->UldManager.LoadedState is AtkLoadState.Loaded) {
+                    parent->UpdateCollisionNodeList(false);
+                    parent->UldManager.UpdateDrawNodeList();
+                }
+            }
+        }
+        
+        RecalculateLayout();
+        
+        return true;
+    }
+
+    public int Count => nodeList.Count;
+
+    public bool IsReadOnly => false;
+    
+    public int IndexOf(T item) 
+        => nodeList.IndexOf(item);
+
+    public void Insert(int index, T item) 
+        => nodeList.Insert(index, item);
+
+    public void RemoveAt(int index) 
+        => nodeList.RemoveAt(index);
+
+    public T this[int index] {
+        get => nodeList[index];
+        set => nodeList[index] = value;
+    }
 }
 
 public enum LayoutAnchor {
+    [Description("Top Left")]
     TopLeft,
+    
+    [Description("Top Right")]
     TopRight,
+    
+    [Description("Bottom Left")]
     BottomLeft,
+    
+    [Description("Bottom Right")]
     BottomRight,
 }
 
