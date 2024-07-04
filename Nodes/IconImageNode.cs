41977f122e2e41476834bf5ce88f14d52209ae3e
MidoriKami <mi_kami@outlook.com>
2024-07-03 19:23:50 -0700

    Add Simple IconImageNode

---
+++ Nodes/IconImageNode.cs
@@ -0,0 +1,15 @@
+﻿namespace KamiToolKit.Nodes;
+
+public class IconImageNode : ImageNode {
+	private uint internalIconId;
+	
+	public uint IconId {
+		get => internalIconId;
+		set {
+			if (internalIconId != value) {
+				LoadIcon(value);
+				internalIconId = value;
+			}
+		}
+	}
+}
