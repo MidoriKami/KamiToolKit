37c55e18da0185a7940c8e0ad4add3c4e395d001
MidoriKami <mi_kami@outlook.com>
2024-08-04 09:35:54 -0700

    Add NineGridNode with single part

---
+++ Nodes/SimpleNineGridNode.cs
@@ -0,0 +1,49 @@
+﻿using System.Numerics;
+using KamiToolKit.Nodes.Parts;
+
+namespace KamiToolKit.Nodes;
+
+public class SimpleNineGridNode : NineGridNode {
+    public SimpleNineGridNode() {
+        PartsList.Add(new Part());
+    }
+    
+    public float U {
+        get => PartsList[0].U;
+        set => PartsList[0].U = (ushort) value;
+    }
+    
+    public float V {
+        get => PartsList[0].V;
+        set => PartsList[0].V = (ushort) value;
+    }
+
+    public Vector2 TextureCoordinates {
+        get => new(U, V);
+        set {
+            U = value.X;
+            V = value.Y;
+        }
+    }
+
+    public float TextureHeight {
+        get => PartsList[0].Height;
+        set => PartsList[0].Height = (ushort) value;
+    }
+    
+    public float TextureWidth {
+        get => PartsList[0].Width;
+        set => PartsList[0].Width = (ushort) value;
+    }
+
+    public Vector2 TextureSize {
+        get => new(TextureWidth, TextureHeight);
+        set {
+            TextureWidth = value.X;
+            TextureHeight = value.Y;
+        }
+    }
+    
+    public void LoadTexture(string path)
+        => PartsList[0].LoadTexture(path);
+}
