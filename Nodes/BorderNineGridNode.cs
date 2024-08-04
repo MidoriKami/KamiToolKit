fe65c247e405c0aee7db8e23815f2361970a5b8b
MidoriKami <mi_kami@outlook.com>
2024-08-04 09:36:22 -0700

    Add BorderNineGridNode

---
+++ Nodes/BorderNineGridNode.cs
@@ -0,0 +1,25 @@
+﻿using System.Numerics;
+using KamiToolKit.Nodes.Parts;
+
+namespace KamiToolKit.Nodes;
+
+public class BorderNineGridNode : NineGridNode {
+    public BorderNineGridNode() {
+        var renderPart = new Part {
+            TextureCoordinates = new Vector2(0.0f, 0.0f), 
+            Size = new Vector2(64.0f, 64.0f), 
+            Id = 0,
+        };
+        
+        renderPart.LoadTexture("ui/uld/PartyListTargetBase_hr1.tex");
+        
+        PartsList.Add(renderPart);
+        
+        TopOffset = 20;
+        LeftOffset = 20;
+        RightOffset = 20;
+        BottomOffset = 20;
+
+        PartsRenderType = PartsRenderType.Unknown;
+    }
+}
