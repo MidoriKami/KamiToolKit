89cc2127899b8c6e0dc15f8eea2d7d0d2e63765b
MidoriKami <mi_kami@outlook.com>
2024-08-04 09:36:42 -0700

    Use new PartsList in NineGridNode, remove texture passthroughs.

--- Nodes/NineGridNode.cs
+++ Nodes/NineGridNode.cs
@@ -1,82 +1,31 @@
 ﻿using System.Numerics;
 using FFXIVClientStructs.FFXIV.Component.GUI;
-using KamiToolKit.Classes;
+using KamiToolKit.Nodes.Parts;
 
 namespace KamiToolKit.Nodes;
 
 public unsafe class NineGridNode : NodeBase<AtkNineGridNode> {
+    protected readonly PartsList PartsList;
+    
     public NineGridNode() : base(NodeType.NineGrid) {
-        var asset = NativeMemoryHelper.UiAlloc<AtkUldAsset>();
-        asset->Id = 1;
-        asset->AtkTexture.Ctor();
-        
-        var part = NativeMemoryHelper.UiAlloc<AtkUldPart>();
-        part->UldAsset = asset;
-        part->U = 0;
-        part->V= 0;
-        part->Height = 0;
-        part->Width = 0;
+        PartsList = new PartsList {
+            Id = 2,
+        };
 
-        var partsList = NativeMemoryHelper.UiAlloc<AtkUldPartsList>();
-        partsList->Parts = part;
-        partsList->Id = 1;
-        partsList->PartCount = 1;
-
-        InternalNode->PartsList = partsList;
+        InternalNode->PartsList = PartsList.InternalPartsList;
     }
 
     protected override void Dispose(bool disposing) {
         if (disposing) {
-            InternalNode->PartsList->Parts->UldAsset->AtkTexture.ReleaseTexture();
-            
-            NativeMemoryHelper.UiFree(InternalNode->PartsList->Parts->UldAsset);
-            NativeMemoryHelper.UiFree(InternalNode->PartsList->Parts);
-            NativeMemoryHelper.UiFree(InternalNode->PartsList);
+            PartsList.Dispose();
             
             base.Dispose(disposing);
         }
     }
-
-    public float U {
-        get => InternalNode->PartsList->Parts->U;
-        set => InternalNode->PartsList->Parts->U = (ushort) value;
-    }
     
-    public float V {
-        get => InternalNode->PartsList->Parts->V;
-        set => InternalNode->PartsList->Parts->V = (ushort) value;
-    }
-
-    public Vector2 TextureCoordinates {
-        get => new(U, V);
-        set {
-            U = value.X;
-            V = value.Y;
-        }
-    }
-
-    public float TextureHeight {
-        get => InternalNode->PartsList->Parts->Height;
-        set => InternalNode->PartsList->Parts->Height = (ushort) value;
-    }
-    
-    public float TextureWidth {
-        get => InternalNode->PartsList->Parts->Width;
-        set => InternalNode->PartsList->Parts->Width = (ushort) value;
-    }
-
-    public Vector2 TextureSize {
-        get => new(TextureWidth, TextureHeight);
-        set {
-            TextureWidth = value.X;
-            TextureHeight = value.Y;
-        }
-    }
-    
-    public AtkUldPartsList* PartsList {
-        get => InternalNode->PartsList;
-        set => InternalNode->PartsList = value;
-    }
+    // The image node will take ownership of any parts added, be sure not to share parts between nodes
+    public void AddPart(Part part)
+        => PartsList.Add(part);
 
     public uint PartId {
         get => InternalNode->PartId;
@@ -122,12 +71,13 @@ public unsafe class NineGridNode : NodeBase<AtkNineGridNode> {
         get => (PartsRenderType) InternalNode->PartsTypeRenderType;
         set => InternalNode->PartsTypeRenderType = (byte) value;
     }
-
-    public void LoadTexture(string path)
-        => PartsList->Parts->UldAsset->AtkTexture.LoadTexture(path);
 }
 
 public enum PartsRenderType {
-    PartsType = 1 << 1,
-    RenderType = 1 << 2,
+    PartsType = 2,
+    RenderType = 4,
+    
+    /// Used in PartyList to draw the selected party member outline
+    /// This is clearly multiple flags at once, but each flag itself is unknown in function
+    Unknown = 108,
 }
