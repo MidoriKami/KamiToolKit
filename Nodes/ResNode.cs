e879464680625a670b830501df8b68ab6baf04c5
MidoriKami <mi_kami@outlook.com>
2024-08-11 12:44:06 -0700

    Add generic res node

---
+++ Nodes/ResNode.cs
@@ -0,0 +1,8 @@
+﻿using FFXIVClientStructs.FFXIV.Component.GUI;
+
+namespace KamiToolKit.Nodes;
+
+/// <summary>
+/// A generic basic resource node.
+/// </summary>
+public class ResNode() : NodeBase<AtkResNode>(NodeType.Res);
