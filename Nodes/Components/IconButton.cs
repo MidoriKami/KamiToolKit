503a293e2f4948962ec1fcc979f0027474f28fd5
MidoriKami <mi_kami@outlook.com>
2025-05-10 12:56:51 -0700

    Add IconButton (WIP)

---
+++ Nodes/Components/IconButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Numerics;
+using KamiToolKit.Classes;
+
+namespace KamiToolKit.Nodes;
+
+public class IconButton : ButtonBaseComponent {
+
+	protected override NodeBase DecorationNode => imageNode;
+	private readonly IconImageNode imageNode;
+
+	public IconButton() {
+		imageNode = new IconImageNode {
+			IsVisible = true,
+			IconId = 60071,
+			Position = new Vector2(16.0f, 3.0f),
+			Size = new Vector2(24.0f, 24.0f),
+			NodeID = 3,
+		};
+		
+		imageNode.AttachNode(this, NodePosition.AfterAllSiblings);
+	}
+
+	public uint IconId {
+		get => imageNode.IconId;
+		set => imageNode.IconId = value;
+	}
+}
