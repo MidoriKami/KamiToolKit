28b33a3feaac271369051c3053bc28156e9e9a65
MidoriKami <mi_kami@outlook.com>
2025-05-10 18:35:49 -0700

    Add IconPosition helpers to IconButton

--- Nodes/Components/IconButton.cs
+++ Nodes/Components/IconButton.cs
@@ -3,7 +3,10 @@ using KamiToolKit.Classes;
 
 namespace KamiToolKit.Nodes;
 
-public class IconButton : ButtonBaseComponent {
+/// <summary>
+/// Uses a GameIconId to display that icon as the decorator for the button.
+/// </summary>
+public class IconButton : ButtonBase {
 
 	protected override NodeBase DecorationNode => imageNode;
 	private readonly IconImageNode imageNode;
@@ -11,9 +14,6 @@ public class IconButton : ButtonBaseComponent {
 	public IconButton() {
 		imageNode = new IconImageNode {
 			IsVisible = true,
-			IconId = 60071,
-			Position = new Vector2(16.0f, 3.0f),
-			Size = new Vector2(24.0f, 24.0f),
 			NodeID = 3,
 		};
 		
@@ -23,5 +23,35 @@ public class IconButton : ButtonBaseComponent {
 	public uint IconId {
 		get => imageNode.IconId;
 		set => imageNode.IconId = value;
+	}
+
+	public float IconPositionX {
+		get => imageNode.X;
+		set => imageNode.X = value;
+	}
+
+	public float IconPositionY {
+		get => imageNode.Y;
+		set => imageNode.Y = value;
+	}
+
+	public Vector2 IconPosition {
+		get => imageNode.Position;
+		set => imageNode.Position = value;
+	}
+
+	public float IconWidth {
+		get => imageNode.Width;
+		set => imageNode.Width = value;
+	}
+
+	public float IconHeight {
+		get => imageNode.Height;
+		set => imageNode.Height = value;
+	}
+
+	public Vector2 IconSize {
+		get => imageNode.Size;
+		set => imageNode.Size = value;
 	}
 }
