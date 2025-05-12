b9be87203b93fbf2d87a2f7e1c1cd3dc18ee55e7
MidoriKami <mi_kami@outlook.com>
2025-05-11 20:33:18 -0700

    Add some experimental documentation

--- Experimental.cs
+++ Experimental.cs
@@ -1,4 +1,5 @@
-﻿using Dalamud.IoC;
+﻿using System.Runtime.CompilerServices;
+using Dalamud.IoC;
 using Dalamud.Plugin;
 using Dalamud.Plugin.Services;
 using Dalamud.Utility.Signatures;
@@ -6,7 +7,8 @@ using FFXIVClientStructs.FFXIV.Component.GUI;
 
 namespace KamiToolKit;
 
-public unsafe class Experimental {
+
+internal unsafe class Experimental {
 	private static Experimental? instance;
 	public static Experimental Instance => instance ??= new Experimental();
 	public static bool Initialized { get; private set; }
@@ -35,5 +37,36 @@ public unsafe class Experimental {
 internal static class Log {
 	internal static void Debug(string message) {
 		Experimental.Instance.Log.Debug(message);
+	}
+}
+
+// vf3  - [InitializeAtkUldManager] Initialize function, sets AtkUldManager fields to zero (mostly)
+// vf4  - [DeinitializeAtkUldManager] Deinitialize function, unregisters tween, clears resources, sets state to unloaded
+// vf6  - [LoadFromUld] Reads UldResourceHandle to build objects/nodes/parts
+// vf8  - (No base implementation) [RegisterEvents] Registers events with AtkComponentBase.AtkResNode as the target node
+// vf9  - (No base implementation)
+// vf11 - Something to do with Priority
+// vf12 - Something to do with Priority
+// vf13 - Potentially some kind of sound effect
+// vf15 - Iterates DuplicateObjectList and calls some function on each node
+// vf16 - (No base implementation)
+// vf18 - Some kind of collision check, returns AtkCollisionNode
+internal static unsafe class AtkComponentBaseExtensions {
+	public static void RegisterEvents(ref this AtkComponentBase atkComponentBase) {
+		var ptr = (AtkComponentBase*) Unsafe.AsPointer(ref atkComponentBase);
+		
+		((delegate* unmanaged<AtkComponentBase*, void>) (*(nint**) ptr)[8])(ptr);
+	}
+
+	public static void InitializeAtkUldManager(ref this AtkComponentBase atkComponentBase) {
+		var ptr = (AtkComponentBase*) Unsafe.AsPointer(ref atkComponentBase);
+		
+		((delegate* unmanaged<AtkComponentBase*, void>) (*(nint**) ptr)[3])(ptr);
+	}
+
+	public static void DeinitializeAtkUldManager(ref this AtkComponentBase atkComponentBase) {
+		var ptr = (AtkComponentBase*) Unsafe.AsPointer(ref atkComponentBase);
+		
+		((delegate* unmanaged<AtkComponentBase*, void>) (*(nint**) ptr)[4])(ptr);
 	}
 }
