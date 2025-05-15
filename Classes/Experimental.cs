c823aeee52a7800808195d5c2e651bad2c4e77fb
MidoriKami <mi_kami@outlook.com>
2025-05-14 20:33:20 -0700

    Add, but disable debugging test methods

--- Classes/Experimental.cs
+++ Classes/Experimental.cs
@@ -1,4 +1,6 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System;
+using System.Runtime.CompilerServices;
+using Dalamud.Hooking;
 using Dalamud.Utility.Signatures;
 using FFXIVClientStructs.FFXIV.Component.GUI;
 
@@ -9,6 +11,16 @@ internal unsafe class Experimental {
 	private static Experimental? instance;
 	public static Experimental Instance => instance ??= new Experimental();
 
+	public void EnableHooks() {
+		// OnUldManagerUpdateHook?.Enable();
+		// UpdateUldFromParentHook?.Enable();
+	}
+
+	public void DisposeHooks() {
+		// OnUldManagerUpdateHook?.Dispose();
+		// UpdateUldFromParentHook?.Dispose();
+	}
+
 	public delegate void ExpandNodeListSizeDelegate(AtkUldManager* atkUldManager, int newSize);
 
 	[Signature("E8 ?? ?? ?? ?? 66 41 3B B7 ?? ?? ?? ??")]
@@ -18,6 +30,57 @@ internal unsafe class Experimental {
 	
 	[Signature("40 57 48 83 EC 30 0F B6 81 ?? ?? ?? ?? 48 8B F9 A8 01")]
 	public DestroyUldManagerDelegate? DestroyUldManager = null;
+	
+	public delegate void OnUldManagerUpdateDelegate(AtkUldManager* atkUldManager);
+
+	[Signature("E8 ?? ?? ?? ?? 80 BB ?? ?? ?? ?? ?? 74 49", DetourName = nameof(OnUldManagerUpdate))]
+	public Hook<OnUldManagerUpdateDelegate>? OnUldManagerUpdateHook = null;
+
+	public void OnUldManagerUpdate(AtkUldManager* atkUldManager) {
+		try {
+			OnUldManagerUpdateHook!.Original(atkUldManager);
+
+			if (atkUldManager->Assets is not null) {
+				if (atkUldManager->Assets->AtkTexture.Resource is not null) {
+					if (atkUldManager->Assets->AtkTexture.Resource->TexFileResourceHandle is not null) {
+						var path = atkUldManager->Assets->AtkTexture.Resource->TexFileResourceHandle->FileName.ToString();
+
+					}
+				}
+			}
+		}
+		catch (Exception e) {
+			Log.Exception(e);
+		}
+	}
+	
+	public delegate void UpdateUldFromParentDelegate(AtkUldManager* atkUldManager, AtkResNode* atkResNode, nint a3, byte a4);
+	
+	[Signature("E8 ?? ?? ?? ?? 40 84 F6 74 2A", DetourName = nameof(UpdateUldFromParent))]
+	public Hook<UpdateUldFromParentDelegate>? UpdateUldFromParentHook = null;
+
+	public void UpdateUldFromParent(AtkUldManager* atkUldManager, AtkResNode* atkResNode, nint a3, byte a4) {
+		try {
+			if (atkUldManager->Assets is not null) {
+				if (atkUldManager->Assets->AtkTexture.Resource is not null) {
+					if (atkUldManager->Assets->AtkTexture.Resource->TexFileResourceHandle is not null) {
+						var path = atkUldManager->Assets->AtkTexture.Resource->TexFileResourceHandle->FileName.ToString();
+						// Log.Debug(path);
+					}
+				}
+			}
+			
+			if (atkResNode is null) {
+				Log.Fatal("CAUGHT FATAL ERROR, ATKRESNODE IS NULL");
+				return;
+			}
+		}
+		catch (Exception e) {
+			Log.Exception(e);
+		}
+		
+		UpdateUldFromParentHook!.Original(atkUldManager, atkResNode, a3, a4);
+	}
 }
 
 // vf3  - [InitializeAtkUldManager] Initialize function, sets AtkUldManager fields to zero (mostly)
