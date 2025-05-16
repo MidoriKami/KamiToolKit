6ef819cf178129c1b5cda26a033c974136b2d21e
MidoriKami <mi_kami@outlook.com>
2025-05-16 08:43:12 -0700

    Cleanup UldObjects.NodeList

--- System/NodeBase.Linking.cs
+++ System/NodeBase.Linking.cs
@@ -23,6 +23,11 @@ public abstract unsafe partial class NodeBase {
 
     internal void DetachNode() {
         NodeLinker.DetachNode(InternalResNode);
+
+        if (this is ComponentNode node) {
+            RemoveNodeFromUldObjectList(&node.ComponentBase->UldManager, InternalResNode);
+            node.ComponentBase->UldManager.UpdateDrawNodeList();
+        }
     }
 
     private void AddNodeToUldObjectList(AtkUldManager* uldManager, AtkResNode* newNode) {
@@ -36,6 +41,25 @@ public abstract unsafe partial class NodeBase {
         
         newBuffer[newSize - 1] = newNode;
         
+        NativeMemoryHelper.Free(uldManager->Objects->NodeList, (ulong)(oldSize * 8));
+        uldManager->Objects->NodeList = newBuffer;
+        uldManager->Objects->NodeCount = newSize;
+    }
+
+    private void RemoveNodeFromUldObjectList(AtkUldManager* uldManager, AtkResNode* nodeToRemove) {
+        var oldSize = uldManager->Objects->NodeCount;
+        var newSize = oldSize - 1;
+        var newBuffer = (AtkResNode**) NativeMemoryHelper.Malloc((ulong)(newSize * 8));
+
+        var newIndex = 0;
+        foreach (var index in Enumerable.Range(0, oldSize)) {
+            if (uldManager->Objects->NodeList[index] != nodeToRemove) {
+                newBuffer[newIndex] = uldManager->Objects->NodeList[index];
+            }
+            
+            newIndex++;
+        }
+
         NativeMemoryHelper.Free(uldManager->Objects->NodeList, (ulong)(oldSize * 8));
         uldManager->Objects->NodeList = newBuffer;
         uldManager->Objects->NodeCount = newSize;
