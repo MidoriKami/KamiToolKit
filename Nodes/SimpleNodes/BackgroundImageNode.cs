b0a1cb08cd13971effa198f975e0199296ac14e1
MidoriKami <mi_kami@outlook.com>
2025-05-21 19:30:33 -0700

    Fix Background Image Node returning wrong alpha

--- Nodes/SimpleNodes/BackgroundImageNode.cs
+++ Nodes/SimpleNodes/BackgroundImageNode.cs
@@ -1,15 +1,17 @@
 using System.Numerics;
 using Dalamud.Utility.Numerics;
 using KamiToolKit.Extensions;
+using Newtonsoft.Json;
 
 namespace KamiToolKit.Nodes;
 
 /// <summary>
 /// A simple image node that makes it easy to display a single color.
 /// </summary>
+[JsonObject(MemberSerialization.OptIn)]
 public unsafe class BackgroundImageNode : SimpleImageNode {
-    public new Vector4 Color {
-        get => new(AddColor.X, AddColor.Y, AddColor.Z, InternalResNode->Color.A);
+    [JsonProperty] public new Vector4 Color {
+        get => new(AddColor.X, AddColor.Y, AddColor.Z, InternalResNode->Color.A / 255.0f);
         set {
             InternalResNode->Color = new Vector4(0.0f, 0.0f, 0.0f, value.W).ToByteColor();
             AddColor = value.AsVector3Color();
