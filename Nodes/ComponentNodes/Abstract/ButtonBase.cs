27facd810084027693cf4c9658df326ca1e9cd71
MidoriKami <mi_kami@outlook.com>
2025-05-24 15:15:07 -0700

    Delegate timeline creation to specific button type

--- Nodes/ComponentNodes/Abstract/ButtonBase.cs
+++ Nodes/ComponentNodes/Abstract/ButtonBase.cs
@@ -4,13 +4,12 @@ using Dalamud.Game.Addon.Events;
 using Dalamud.Plugin.Services;
 using FFXIVClientStructs.FFXIV.Component.GUI;
 using KamiToolKit.Classes;
-using KamiToolKit.NodeParts;
 using KamiToolKit.System;
 
 namespace KamiToolKit.Nodes.ComponentNodes.Abstract;
 
 public abstract unsafe class ButtonBase : ComponentNode<AtkComponentButton, AtkUldComponentDataButton> {
-	protected readonly NineGridNode BackgroundNode;
+	internal readonly NineGridNode BackgroundNode;
 	protected abstract NodeBase DecorationNode { get; }
 
 	protected AtkComponentButton* ButtonNode => (AtkComponentButton*) InternalNode;
@@ -27,86 +26,6 @@ public abstract unsafe class ButtonBase : ComponentNode<AtkComponentButton, AtkU
 			RightOffset = 16.0f,
 			NodeId = 2,
 		};
-		
-		AddTimeline(new Timeline {
-			Mask = (AtkTimelineMask) 0xFF,
-			LabelEndFrameIdx = 53,
-			LabelFrameIdxDuration = 52,
-			LabelSets = [
-				new TimelineLabelSet {
-					StartFrameId = 1, EndFrameId = 53, Labels = [
-						new TimelineKeyFrame { FrameIndex = 1, Label = new AtkTimelineLabel{ JumpBehavior = AtkTimelineJumpBehavior.Start, LabelId = 1, JumpLabelId = 0 }},
-						new TimelineKeyFrame { FrameIndex = 10, Label = new AtkTimelineLabel{ JumpBehavior = AtkTimelineJumpBehavior.PlayOnce, LabelId = 0, JumpLabelId = 0 }},
-						new TimelineKeyFrame { FrameIndex = 11, Label = new AtkTimelineLabel{ JumpBehavior = AtkTimelineJumpBehavior.Start, LabelId = 2, JumpLabelId = 0 }},
-						new TimelineKeyFrame { FrameIndex = 17, Label = new AtkTimelineLabel{ JumpBehavior = AtkTimelineJumpBehavior.PlayOnce, LabelId = 0, JumpLabelId = 0 }},
-						new TimelineKeyFrame { FrameIndex = 18, Label = new AtkTimelineLabel{ JumpBehavior = AtkTimelineJumpBehavior.Start, LabelId = 3, JumpLabelId = 0 }},
-						new TimelineKeyFrame { FrameIndex = 26, Label = new AtkTimelineLabel{ JumpBehavior = AtkTimelineJumpBehavior.PlayOnce, LabelId = 0, JumpLabelId = 0 }},
-						new TimelineKeyFrame { FrameIndex = 27, Label = new AtkTimelineLabel{ JumpBehavior = AtkTimelineJumpBehavior.Start, LabelId = 7, JumpLabelId = 0 }},
-						new TimelineKeyFrame { FrameIndex = 36, Label = new AtkTimelineLabel{ JumpBehavior = AtkTimelineJumpBehavior.PlayOnce, LabelId = 0, JumpLabelId = 0 }},
-						new TimelineKeyFrame { FrameIndex = 37, Label = new AtkTimelineLabel{ JumpBehavior = AtkTimelineJumpBehavior.Start, LabelId = 6, JumpLabelId = 0 }},
-						new TimelineKeyFrame { FrameIndex = 46, Label = new AtkTimelineLabel{ JumpBehavior = AtkTimelineJumpBehavior.PlayOnce, LabelId = 0, JumpLabelId = 0 }},
-						new TimelineKeyFrame { FrameIndex = 47, Label = new AtkTimelineLabel{ JumpBehavior = AtkTimelineJumpBehavior.Start, LabelId = 4, JumpLabelId = 0 }},
-						new TimelineKeyFrame { FrameIndex = 53, Label = new AtkTimelineLabel{ JumpBehavior = AtkTimelineJumpBehavior.PlayOnce, LabelId = 0, JumpLabelId = 0 }},
-					],
-				},
-			],
-		});
-		
-		BackgroundNode.AddTimeline(new Timeline {
-			Mask = AtkTimelineMask.VendorSpecific2,
-			Animations = [ 
-				new TimelineAnimation {
-					StartFrameId = 1, EndFrameId = 10, KeyFrames = [ 
-						new TimelineKeyFrame { FrameIndex = 1, Position = new Vector2(0.0f, 0.0f) }, 
-						new TimelineKeyFrame { FrameIndex = 1, Alpha = 255 }, 
-						new TimelineKeyFrame { FrameIndex = 1, NodeTint = new NodeTint { MultiplyColor = new Vector3(100.0f, 100.0f, 100.0f) } },
-					],
-				},
-				new TimelineAnimation {
-					StartFrameId = 11, EndFrameId = 17, KeyFrames = [ 
-						new TimelineKeyFrame { FrameIndex = 11, Position = new Vector2(0.0f, 0.0f) }, 
-						new TimelineKeyFrame { FrameIndex = 11, Alpha = 255 }, 
-						new TimelineKeyFrame { FrameIndex = 11, NodeTint = new NodeTint { MultiplyColor = new Vector3(100.0f, 100.0f, 100.0f) } },
-
-						new TimelineKeyFrame { FrameIndex = 13, Position = new Vector2(0.0f, 0.0f) }, 
-						new TimelineKeyFrame { FrameIndex = 13, Alpha = 255 }, 
-						new TimelineKeyFrame { FrameIndex = 13, NodeTint = new NodeTint { MultiplyColor = new Vector3(100.0f, 100.0f, 100.0f), AddColor = new Vector3(16.0f, 16.0f, 16.0f) } },
-					],
-				},
-				new TimelineAnimation {
-					StartFrameId = 18, EndFrameId = 26, KeyFrames = [
-						new TimelineKeyFrame { FrameIndex = 18, Position = new Vector2(0.0f, 1.0f) }, 
-						new TimelineKeyFrame { FrameIndex = 18, Alpha = 255 }, 
-						new TimelineKeyFrame { FrameIndex = 18, NodeTint = new NodeTint { MultiplyColor = new Vector3(100.0f, 100.0f, 100.0f), AddColor = new Vector3(16.0f, 16.0f, 16.0f) } },
-					],
-				},
-				new TimelineAnimation {
-					StartFrameId = 27, EndFrameId = 36, KeyFrames = [
-						new TimelineKeyFrame { FrameIndex = 27, Position = new Vector2(0.0f, 0.0f) }, 
-						new TimelineKeyFrame { FrameIndex = 27, Alpha = 178 }, 
-						new TimelineKeyFrame { FrameIndex = 27, NodeTint = new NodeTint { MultiplyColor = new Vector3(50.0f, 50.0f, 50.0f) } },
-					],
-				},
-				new TimelineAnimation {
-					StartFrameId = 37, EndFrameId = 46, KeyFrames = [
-						new TimelineKeyFrame { FrameIndex = 37, Position = new Vector2(0.0f, 0.0f) }, 
-						new TimelineKeyFrame { FrameIndex = 37, Alpha = 255 }, 
-						new TimelineKeyFrame { FrameIndex = 37, NodeTint = new NodeTint { MultiplyColor = new Vector3(100.0f, 100.0f, 100.0f), AddColor = new Vector3(16.0f, 16.0f, 16.0f) } },
-					],
-				},
-				new TimelineAnimation {
-					StartFrameId = 47, EndFrameId = 53, KeyFrames = [
-						new TimelineKeyFrame { FrameIndex = 47, Position = new Vector2(0.0f, 0.0f) }, 
-						new TimelineKeyFrame { FrameIndex = 47, Alpha = 255 }, 
-						new TimelineKeyFrame { FrameIndex = 47, NodeTint = new NodeTint { MultiplyColor = new Vector3(100.0f, 100.0f, 100.0f), AddColor = new Vector3(16.0f, 16.0f, 16.0f) } },
-
-						new TimelineKeyFrame { FrameIndex = 53, Position = new Vector2(0.0f, 0.0f) }, 
-						new TimelineKeyFrame { FrameIndex = 53, Alpha = 255 }, 
-						new TimelineKeyFrame { FrameIndex = 53, NodeTint = new NodeTint { MultiplyColor = new Vector3(100.0f, 100.0f, 100.0f) } },
-					],
-				},
-			],
-		});
 
 		BackgroundNode.AttachNode(this, NodePosition.AfterAllSiblings);
 	}
--- Nodes/ComponentNodes/Buttons/TextButton.cs
+++ Nodes/ComponentNodes/Buttons/TextButton.cs
@@ -27,56 +27,8 @@ public unsafe class TextButton : ButtonBase {
 			LineSpacing = 12,
 		};
 		
-		LabelNode.AddTimeline(new Timeline {
-			Animations = [ 
-				new TimelineAnimation {
-					StartFrameId = 1, EndFrameId = 10, KeyFrames = [ 
-						new TimelineKeyFrame { FrameIndex = 1, Position = new Vector2(16.0f, 3.0f) }, 
-						new TimelineKeyFrame { FrameIndex = 1, Alpha = 255 }, 
-						new TimelineKeyFrame { FrameIndex = 1, NodeTint = new NodeTint { MultiplyColor = new Vector3(100.0f, 100.0f, 100.0f) } },
-					],
-				},
-				new TimelineAnimation {
-					StartFrameId = 11, EndFrameId = 17, KeyFrames = [ 
-						new TimelineKeyFrame { FrameIndex = 11, Position = new Vector2(16.0f, 3.0f) }, 
-						new TimelineKeyFrame { FrameIndex = 11, Alpha = 255 }, 
-						new TimelineKeyFrame { FrameIndex = 11, NodeTint = new NodeTint { MultiplyColor = new Vector3(100.0f, 100.0f, 100.0f) } },
-					],
-				},
-				new TimelineAnimation {
-					StartFrameId = 18, EndFrameId = 26, KeyFrames = [
-						new TimelineKeyFrame { FrameIndex = 18, Position = new Vector2(16.0f, 4.0f) }, 
-						new TimelineKeyFrame { FrameIndex = 18, Alpha = 255 }, 
-						new TimelineKeyFrame { FrameIndex = 18, NodeTint = new NodeTint { MultiplyColor = new Vector3(100.0f, 100.0f, 100.0f) } },
-					],
-				},
-				new TimelineAnimation {
-					StartFrameId = 27, EndFrameId = 36, KeyFrames = [
-						new TimelineKeyFrame { FrameIndex = 27, Position = new Vector2(16.0f, 3.0f) }, 
-						new TimelineKeyFrame { FrameIndex = 27, Alpha = 153 }, 
-						new TimelineKeyFrame { FrameIndex = 27, NodeTint = new NodeTint { MultiplyColor = new Vector3(80.0f, 80.0f, 80.0f) } },
-					],
-				},
-				new TimelineAnimation {
-					StartFrameId = 37, EndFrameId = 46, KeyFrames = [
-						new TimelineKeyFrame { FrameIndex = 37, Position = new Vector2(16.0f, 3.0f) }, 
-						new TimelineKeyFrame { FrameIndex = 37, Alpha = 255 }, 
-						new TimelineKeyFrame { FrameIndex = 37, NodeTint = new NodeTint { MultiplyColor = new Vector3(100.0f, 100.0f, 100.0f) } },
-					],
-				},
-				new TimelineAnimation {
-					StartFrameId = 47, EndFrameId = 53, KeyFrames = [
-						new TimelineKeyFrame { FrameIndex = 47, Position = new Vector2(16.0f, 3.0f) }, 
-						new TimelineKeyFrame { FrameIndex = 47, Alpha = 255 }, 
-						new TimelineKeyFrame { FrameIndex = 47, NodeTint = new NodeTint { MultiplyColor = new Vector3(100.0f, 100.0f, 100.0f) } },
-					],
-				},
-			],
-		});
+		LoadTimelines();
 
-		AddFlags(NodeFlags.AnchorTop, NodeFlags.AnchorLeft, NodeFlags.Visible, NodeFlags.Enabled, NodeFlags.EmitsEvents);
-		LabelNode.AddFlags(NodeFlags.AnchorLeft, NodeFlags.AnchorRight, NodeFlags.Visible, NodeFlags.Enabled, NodeFlags.EmitsEvents);
-		
 		LabelNode.AttachNode(this, NodePosition.AfterAllSiblings);
 		
 		InitializeComponentEvents();
@@ -129,5 +81,134 @@ public unsafe class TextButton : ButtonBase {
 			Width = value.X;
 			Height = value.Y;
 		}
+	}
+
+	private void LoadTimelines() {
+		AddTimeline(new Timeline {
+			Mask = (AtkTimelineMask) 0xFF,
+			LabelEndFrameIdx = 53,
+			LabelFrameIdxDuration = 52,
+			LabelSets = [
+				new TimelineLabelSet {
+					StartFrameId = 1, EndFrameId = 53, Labels = [
+						new TimelineKeyFrame { FrameIndex = 1, Label = new AtkTimelineLabel{ JumpBehavior = AtkTimelineJumpBehavior.Start, LabelId = 1, JumpLabelId = 0 }},
+						new TimelineKeyFrame { FrameIndex = 10, Label = new AtkTimelineLabel{ JumpBehavior = AtkTimelineJumpBehavior.PlayOnce, LabelId = 0, JumpLabelId = 0 }},
+						new TimelineKeyFrame { FrameIndex = 11, Label = new AtkTimelineLabel{ JumpBehavior = AtkTimelineJumpBehavior.Start, LabelId = 2, JumpLabelId = 0 }},
+						new TimelineKeyFrame { FrameIndex = 17, Label = new AtkTimelineLabel{ JumpBehavior = AtkTimelineJumpBehavior.PlayOnce, LabelId = 0, JumpLabelId = 0 }},
+						new TimelineKeyFrame { FrameIndex = 18, Label = new AtkTimelineLabel{ JumpBehavior = AtkTimelineJumpBehavior.Start, LabelId = 3, JumpLabelId = 0 }},
+						new TimelineKeyFrame { FrameIndex = 26, Label = new AtkTimelineLabel{ JumpBehavior = AtkTimelineJumpBehavior.PlayOnce, LabelId = 0, JumpLabelId = 0 }},
+						new TimelineKeyFrame { FrameIndex = 27, Label = new AtkTimelineLabel{ JumpBehavior = AtkTimelineJumpBehavior.Start, LabelId = 7, JumpLabelId = 0 }},
+						new TimelineKeyFrame { FrameIndex = 36, Label = new AtkTimelineLabel{ JumpBehavior = AtkTimelineJumpBehavior.PlayOnce, LabelId = 0, JumpLabelId = 0 }},
+						new TimelineKeyFrame { FrameIndex = 37, Label = new AtkTimelineLabel{ JumpBehavior = AtkTimelineJumpBehavior.Start, LabelId = 6, JumpLabelId = 0 }},
+						new TimelineKeyFrame { FrameIndex = 46, Label = new AtkTimelineLabel{ JumpBehavior = AtkTimelineJumpBehavior.PlayOnce, LabelId = 0, JumpLabelId = 0 }},
+						new TimelineKeyFrame { FrameIndex = 47, Label = new AtkTimelineLabel{ JumpBehavior = AtkTimelineJumpBehavior.Start, LabelId = 4, JumpLabelId = 0 }},
+						new TimelineKeyFrame { FrameIndex = 53, Label = new AtkTimelineLabel{ JumpBehavior = AtkTimelineJumpBehavior.PlayOnce, LabelId = 0, JumpLabelId = 0 }},
+					],
+				},
+			],
+		});
+		
+		BackgroundNode.AddTimeline(new Timeline {
+			Mask = AtkTimelineMask.VendorSpecific2,
+			Animations = [ 
+				new TimelineAnimation {
+					StartFrameId = 1, EndFrameId = 10, KeyFrames = [ 
+						new TimelineKeyFrame { FrameIndex = 1, Position = new Vector2(0.0f, 0.0f) }, 
+						new TimelineKeyFrame { FrameIndex = 1, Alpha = 255 }, 
+						new TimelineKeyFrame { FrameIndex = 1, NodeTint = new NodeTint { MultiplyColor = new Vector3(100.0f, 100.0f, 100.0f) } },
+					],
+				},
+				new TimelineAnimation {
+					StartFrameId = 11, EndFrameId = 17, KeyFrames = [ 
+						new TimelineKeyFrame { FrameIndex = 11, Position = new Vector2(0.0f, 0.0f) }, 
+						new TimelineKeyFrame { FrameIndex = 11, Alpha = 255 }, 
+						new TimelineKeyFrame { FrameIndex = 11, NodeTint = new NodeTint { MultiplyColor = new Vector3(100.0f, 100.0f, 100.0f) } },
+
+						new TimelineKeyFrame { FrameIndex = 13, Position = new Vector2(0.0f, 0.0f) }, 
+						new TimelineKeyFrame { FrameIndex = 13, Alpha = 255 }, 
+						new TimelineKeyFrame { FrameIndex = 13, NodeTint = new NodeTint { MultiplyColor = new Vector3(100.0f, 100.0f, 100.0f), AddColor = new Vector3(16.0f, 16.0f, 16.0f) } },
+					],
+				},
+				new TimelineAnimation {
+					StartFrameId = 18, EndFrameId = 26, KeyFrames = [
+						new TimelineKeyFrame { FrameIndex = 18, Position = new Vector2(0.0f, 1.0f) }, 
+						new TimelineKeyFrame { FrameIndex = 18, Alpha = 255 }, 
+						new TimelineKeyFrame { FrameIndex = 18, NodeTint = new NodeTint { MultiplyColor = new Vector3(100.0f, 100.0f, 100.0f), AddColor = new Vector3(16.0f, 16.0f, 16.0f) } },
+					],
+				},
+				new TimelineAnimation {
+					StartFrameId = 27, EndFrameId = 36, KeyFrames = [
+						new TimelineKeyFrame { FrameIndex = 27, Position = new Vector2(0.0f, 0.0f) }, 
+						new TimelineKeyFrame { FrameIndex = 27, Alpha = 178 }, 
+						new TimelineKeyFrame { FrameIndex = 27, NodeTint = new NodeTint { MultiplyColor = new Vector3(50.0f, 50.0f, 50.0f) } },
+					],
+				},
+				new TimelineAnimation {
+					StartFrameId = 37, EndFrameId = 46, KeyFrames = [
+						new TimelineKeyFrame { FrameIndex = 37, Position = new Vector2(0.0f, 0.0f) }, 
+						new TimelineKeyFrame { FrameIndex = 37, Alpha = 255 }, 
+						new TimelineKeyFrame { FrameIndex = 37, NodeTint = new NodeTint { MultiplyColor = new Vector3(100.0f, 100.0f, 100.0f), AddColor = new Vector3(16.0f, 16.0f, 16.0f) } },
+					],
+				},
+				new TimelineAnimation {
+					StartFrameId = 47, EndFrameId = 53, KeyFrames = [
+						new TimelineKeyFrame { FrameIndex = 47, Position = new Vector2(0.0f, 0.0f) }, 
+						new TimelineKeyFrame { FrameIndex = 47, Alpha = 255 }, 
+						new TimelineKeyFrame { FrameIndex = 47, NodeTint = new NodeTint { MultiplyColor = new Vector3(100.0f, 100.0f, 100.0f), AddColor = new Vector3(16.0f, 16.0f, 16.0f) } },
+
+						new TimelineKeyFrame { FrameIndex = 53, Position = new Vector2(0.0f, 0.0f) }, 
+						new TimelineKeyFrame { FrameIndex = 53, Alpha = 255 }, 
+						new TimelineKeyFrame { FrameIndex = 53, NodeTint = new NodeTint { MultiplyColor = new Vector3(100.0f, 100.0f, 100.0f) } },
+					],
+				},
+			],
+		});
+		
+		LabelNode.AddTimeline(new Timeline {
+			Animations = [ 
+				new TimelineAnimation {
+					StartFrameId = 1, EndFrameId = 10, KeyFrames = [ 
+						new TimelineKeyFrame { FrameIndex = 1, Position = new Vector2(16.0f, 3.0f) }, 
+						new TimelineKeyFrame { FrameIndex = 1, Alpha = 255 }, 
+						new TimelineKeyFrame { FrameIndex = 1, NodeTint = new NodeTint { MultiplyColor = new Vector3(100.0f, 100.0f, 100.0f) } },
+					],
+				},
+				new TimelineAnimation {
+					StartFrameId = 11, EndFrameId = 17, KeyFrames = [ 
+						new TimelineKeyFrame { FrameIndex = 11, Position = new Vector2(16.0f, 3.0f) }, 
+						new TimelineKeyFrame { FrameIndex = 11, Alpha = 255 }, 
+						new TimelineKeyFrame { FrameIndex = 11, NodeTint = new NodeTint { MultiplyColor = new Vector3(100.0f, 100.0f, 100.0f) } },
+					],
+				},
+				new TimelineAnimation {
+					StartFrameId = 18, EndFrameId = 26, KeyFrames = [
+						new TimelineKeyFrame { FrameIndex = 18, Position = new Vector2(16.0f, 4.0f) }, 
+						new TimelineKeyFrame { FrameIndex = 18, Alpha = 255 }, 
+						new TimelineKeyFrame { FrameIndex = 18, NodeTint = new NodeTint { MultiplyColor = new Vector3(100.0f, 100.0f, 100.0f) } },
+					],
+				},
+				new TimelineAnimation {
+					StartFrameId = 27, EndFrameId = 36, KeyFrames = [
+						new TimelineKeyFrame { FrameIndex = 27, Position = new Vector2(16.0f, 3.0f) }, 
+						new TimelineKeyFrame { FrameIndex = 27, Alpha = 153 }, 
+						new TimelineKeyFrame { FrameIndex = 27, NodeTint = new NodeTint { MultiplyColor = new Vector3(80.0f, 80.0f, 80.0f) } },
+					],
+				},
+				new TimelineAnimation {
+					StartFrameId = 37, EndFrameId = 46, KeyFrames = [
+						new TimelineKeyFrame { FrameIndex = 37, Position = new Vector2(16.0f, 3.0f) }, 
+						new TimelineKeyFrame { FrameIndex = 37, Alpha = 255 }, 
+						new TimelineKeyFrame { FrameIndex = 37, NodeTint = new NodeTint { MultiplyColor = new Vector3(100.0f, 100.0f, 100.0f) } },
+					],
+				},
+				new TimelineAnimation {
+					StartFrameId = 47, EndFrameId = 53, KeyFrames = [
+						new TimelineKeyFrame { FrameIndex = 47, Position = new Vector2(16.0f, 3.0f) }, 
+						new TimelineKeyFrame { FrameIndex = 47, Alpha = 255 }, 
+						new TimelineKeyFrame { FrameIndex = 47, NodeTint = new NodeTint { MultiplyColor = new Vector3(100.0f, 100.0f, 100.0f) } },
+					],
+				},
+			],
+		});
 	}
 }
