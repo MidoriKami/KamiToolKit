82cd7951652f185c0bab430ce315ca5d2829bc2b
MidoriKami <mi_kami@outlook.com>
2025-05-27 19:25:23 -0700

    Remove default sizes

--- Nodes/ComponentNodes/Window/WindowNode.cs
+++ Nodes/ComponentNodes/Window/WindowNode.cs
@@ -21,7 +21,6 @@ public unsafe class WindowNode : ComponentNode<AtkComponentWindow, AtkUldCompone
 		headerCollisionNode = new CollisionNode {
 			Uses = 2,
 			NodeId = 12,
-			Size = new Vector2(468.0f, 28.0f),
 			Position = new Vector2(8.0f, 8.0f),
 			NodeFlags = NodeFlags.AnchorTop | NodeFlags.AnchorLeft | NodeFlags.AnchorRight | NodeFlags.Visible |NodeFlags.Enabled |NodeFlags.HasCollision | NodeFlags.RespondToMouse | NodeFlags.EmitsEvents,
 		};
@@ -30,7 +29,6 @@ public unsafe class WindowNode : ComponentNode<AtkComponentWindow, AtkUldCompone
 
 		backgroundNode = new WindowBackgroundNode(false) {
 			NodeId = 11,
-			Size = new Vector2(482.0f, 636.0f),
 			Position = Vector2.Zero,
 			Offsets = new Vector4(64.0f, 32.0f, 32.0f, 32.0f),
 			NodeFlags = NodeFlags.AnchorTop | NodeFlags.AnchorLeft | NodeFlags.Visible | NodeFlags.Enabled | NodeFlags.Fill | NodeFlags.EmitsEvents,
@@ -41,7 +39,6 @@ public unsafe class WindowNode : ComponentNode<AtkComponentWindow, AtkUldCompone
 
 		borderNode = new WindowBackgroundNode(true) {
 			NodeId = 10,
-			Size = new Vector2(482.0f, 636.0f),
 			Position = Vector2.Zero,
 			Offsets = new Vector4(64.0f, 32.0f, 32.0f, 32.0f),
 			NodeFlags = NodeFlags.AnchorTop | NodeFlags.AnchorLeft | NodeFlags.Enabled | NodeFlags.Fill | NodeFlags.EmitsEvents,
@@ -52,8 +49,6 @@ public unsafe class WindowNode : ComponentNode<AtkComponentWindow, AtkUldCompone
 
 		backgroundImageNode = new SimpleImageNode {
 			NodeId = 9,
-			Position = new Vector2(4.0f, 4.0f),
-			Size = new Vector2(474.0f, 620.0f),
 			WrapMode = 2,
 			ImageNodeFlags = 0,
 			NodeFlags = NodeFlags.AnchorTop | NodeFlags.AnchorLeft | NodeFlags.AnchorBottom | NodeFlags.AnchorRight | NodeFlags.Visible | NodeFlags.Enabled | NodeFlags.EmitsEvents,
