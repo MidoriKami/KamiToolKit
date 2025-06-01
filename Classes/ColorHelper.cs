9c2e7e6a92cccf800fc6bf573de49660e6fa7b6d
MidoriKami <mi_kami@outlook.com>
2025-05-31 17:02:35 -0700

    Remove dalamud dependency for get color

--- Classes/ColorHelper.cs
+++ Classes/ColorHelper.cs
@@ -5,20 +5,8 @@ using Lumina.Excel.Sheets;
 namespace KamiToolKit.Classes;
 
 public static unsafe class ColorHelper {
-	public static Vector4 GetColor(uint colorId) {
-		var colorEntry = DalamudInterface.Instance.DataManager.GetExcelSheet<UIColor>().GetRow(colorId);
-
-		return CurrentTheme() switch {
-			0 => ConvertToVector4(colorEntry.Dark),
-			1 => ConvertToVector4(colorEntry.Light),
-			2 => ConvertToVector4(colorEntry.ClassicFF),
-			3 => ConvertToVector4(colorEntry.ClearBlue),
-			_ => Vector4.One,
-		};
-	}
-
-	private static int CurrentTheme()
-		=> AtkStage.Instance()->AtkUIColorHolder->ActiveColorThemeType;
+	public static Vector4 GetColor(uint colorId)
+		=> ConvertToVector4(AtkStage.Instance()->AtkUIColorHolder->GetColor(true, colorId));
 	
 	private static Vector4 ConvertToVector4(uint color) {
 		var r = (byte)(color >> 24);
