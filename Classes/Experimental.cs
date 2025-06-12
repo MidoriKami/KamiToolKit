f80c382317cbc49cbdd4cebff68412403483c283
MidoriKami <mi_kami@outlook.com>
2025-06-11 19:05:37 -0700

    Remove experimental functions that are in ClientStructs now

--- Classes/Experimental.cs
+++ Classes/Experimental.cs
@@ -1,10 +1,8 @@
 ﻿using Dalamud.Utility.Signatures;
-using FFXIVClientStructs.FFXIV.Client.UI;
-using FFXIVClientStructs.FFXIV.Component.GUI;
 
 namespace KamiToolKit.Classes;
 
-internal unsafe class Experimental {
+internal class Experimental {
 	private static Experimental? instance;
 	public static Experimental Instance => instance ??= new Experimental();
 
@@ -16,9 +14,4 @@ internal unsafe class Experimental {
 
 	[Signature("4C 8D 3D ?? ?? ?? ?? 4C 89 3F 41 F6 C4")]
 	public nint AtkEventListenerVirtualTable = nint.Zero;
-
-	public delegate AtkUnitBase* GetAddonByNodeDelegate(RaptureAtkUnitManager* manager, AtkResNode* node);
-
-	[Signature("E8 ?? ?? ?? ?? 48 3B E8 75 0E")]
-	public GetAddonByNodeDelegate? GetAddonByNode = null;
 }
