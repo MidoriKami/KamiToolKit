5369d8a03df2c9f672f4e389a9532cbfdbd9eff4
MidoriKami <mi_kami@outlook.com>
2025-06-19 22:00:58 -0700

    Add Debug-Only AddonOpener

--- Classes/Experimental.cs
+++ Classes/Experimental.cs
@@ -1,8 +1,12 @@
 ﻿using Dalamud.Utility.Signatures;
+using FFXIVClientStructs.FFXIV.Client.UI.Agent;
 
 namespace KamiToolKit.Classes;
-
+#if DEBUG
+public class Experimental {
+#else
 internal class Experimental {
+#endif
 	private static Experimental? instance;
 	public static Experimental Instance => instance ??= new Experimental();
 
@@ -14,4 +18,22 @@ internal class Experimental {
 
 	[Signature("4C 8D 3D ?? ?? ?? ?? 4C 89 3F 41 F6 C4")]
 	public nint AtkEventListenerVirtualTable = nint.Zero;
+	
+#if DEBUG
+	// WARNING: May result in undefined state or accidental network requests
+	// Use at your own risk.
+	public static unsafe void ForceOpenAddon(AgentId agentId) {
+		DalamudInterface.Instance.Framework.RunOnFrameworkThread(() => {
+			AgentModule.Instance()->GetAgentByInternalId(agentId)->Show();
+		});
+	}
+
+	// WARNING: May result in undefined state or accidental network requests
+	// Use at your own risk.
+	public static unsafe void ForceCloseAddon(AgentId agentId) {
+		DalamudInterface.Instance.Framework.RunOnFrameworkThread(() => {
+			AgentModule.Instance()->GetAgentByInternalId(agentId)->Hide();
+		});
+	}
+#endif
 }
