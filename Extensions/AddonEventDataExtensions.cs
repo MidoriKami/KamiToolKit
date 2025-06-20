74e2adb1e909bc52390810cf1650bc9bf666953a
MidoriKami <mi_kami@outlook.com>
2025-06-20 13:35:38 -0700

    Implement node click-drag

---
+++ Extensions/AddonEventDataExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Numerics;
+using Dalamud.Game.Addon.Events;
+using FFXIVClientStructs.FFXIV.Component.GUI;
+
+namespace KamiToolKit.Extensions;
+
+public static unsafe class AddonEventDataExtensions {
+	public static void SetHandled(this AddonEventData data, bool forced = true)
+		=> data.GetEventData()->SetEventIsHandled(forced);
+
+	public static Vector2 GetMousePosition(this AddonEventData data)
+		=> new(data.GetMouseData().PosX, data.GetMouseData().PosY);
+
+	private static ref AtkEventData.AtkMouseData GetMouseData(this AddonEventData data)
+		=> ref ((AtkEventData*) data.AtkEventDataPointer)->MouseData;
+
+	private static AtkEvent* GetEventData(this AddonEventData data)
+		=>  (AtkEvent*)data.AtkEventDataPointer;
+}
---
+++ System/NodeBase.DragDrop.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Numerics;
+using Dalamud.Game.Addon.Events;
+using KamiToolKit.Extensions;
+
+namespace KamiToolKit.System;
+
+public abstract partial class NodeBase {
+
+	private bool clickDragEventsRegistered;
+
+	private bool isDragging;
+	private Vector2 clickStart = Vector2.Zero;
+
+	public Action? OnClickDragComplete { get; set; }
+
+	public void EnableClickDrag() {
+		if (clickDragEventsRegistered) return;
+		
+		AddEvent(AddonEventType.MouseOver, ClickDragMouseOver);
+		AddEvent(AddonEventType.MouseDown, ClickDragStart);
+		AddEvent(AddonEventType.MouseMove, ClickDragMove);
+		AddEvent(AddonEventType.MouseUp, ClickDragEnd);
+		AddEvent(AddonEventType.MouseOut, ClickDragMouseOut);
+		
+		clickDragEventsRegistered = true;
+	}
+
+	public void DisableClickDrag() {
+		if (!clickDragEventsRegistered) return;
+		
+		RemoveEvent(AddonEventType.MouseOver, ClickDragMouseOver);
+		RemoveEvent(AddonEventType.MouseDown, ClickDragStart);
+		RemoveEvent(AddonEventType.MouseMove, ClickDragMove);
+		RemoveEvent(AddonEventType.MouseUp, ClickDragEnd);
+		RemoveEvent(AddonEventType.MouseOut, ClickDragMouseOut);
+		
+		clickDragEventsRegistered = false;
+	}
+
+	private void ClickDragMouseOver(AddonEventData eventData) {
+		SetCursor(AddonCursorType.Hand);
+		
+		eventData.SetHandled();
+	}
+	
+	private void ClickDragStart(AddonEventData eventData) {
+		isDragging = true;
+		clickStart = eventData.GetMousePosition();
+		SetCursor(AddonCursorType.Grab);
+		
+		eventData.SetHandled();
+	}
+	
+	private void ClickDragMove(AddonEventData eventData) {
+		if (!isDragging) return;
+		
+		var newPosition = eventData.GetMousePosition();
+		var delta = newPosition - clickStart;
+		
+		Position += delta;
+		clickStart = newPosition;
+		
+		eventData.SetHandled();
+	}
+
+	private void ClickDragEnd(AddonEventData eventData) {
+		isDragging = false;
+		SetCursor(AddonCursorType.Hand);
+		
+		OnClickDragComplete?.Invoke();
+		
+		eventData.SetHandled();
+	}
+
+	private void ClickDragMouseOut(AddonEventData eventData) {
+		isDragging = false;
+		ResetCursor();
+
+		OnClickDragComplete?.Invoke();
+		
+		eventData.SetHandled();
+	}
+}
