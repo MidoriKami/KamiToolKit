8e32f0671f656c260e1b7769b30c467b71293831
MidoriKami <mi_kami@outlook.com>
2025-06-21 21:28:59 -0700

    TabBars

---
+++ Nodes/TabBar/TabBarNode.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Dalamud.Game.Text.SeStringHandling;
+using FFXIVClientStructs.FFXIV.Component.GUI;
+using KamiToolKit.Classes.TimelineBuilding;
+
+namespace KamiToolKit.Nodes.TabBar;
+
+public class TabBarNode : ResNode {
+
+	private List<TabBarRadioButtonNode> radioButtons = [];
+	
+	public TabBarNode() {
+		BuildTimelines();
+	}
+
+	public TabBarRadioButtonNode AddTab(SeString label, Action callback) {
+		var newButton = new TabBarRadioButtonNode {
+			Height = Height, 
+			IsVisible = true,
+			Label = label,
+			OnClick = callback,
+		};
+		
+		radioButtons.Add(newButton);
+		newButton.AttachNode(this);
+
+		if (radioButtons.Count is 1) {
+			newButton.IsSelected = true;
+		}
+		
+		RecalculateLayout();
+		
+		return newButton;
+	}
+
+	private void ClickHandler(TabBarRadioButtonNode button) {
+		foreach (var radioButton in radioButtons) {
+			radioButton.IsChecked = false;
+			radioButton.IsSelected = false;
+		}
+		
+		button.IsChecked = true;
+		button.IsSelected = true;
+	}
+
+	public void RemoveTab(TabBarRadioButtonNode button) {
+		button.DetachNode();
+		radioButtons.Remove(button);
+		RecalculateLayout();
+	}
+
+	public void Clear() {
+		foreach (var node in radioButtons) {
+			node.DetachNode();
+		}
+		
+		radioButtons.Clear();
+	}
+
+	private void RecalculateLayout() {
+		var step = Width / radioButtons.Count;
+		
+		foreach (var index in Enumerable.Range(0, radioButtons.Count)) {
+			var button = radioButtons[index];
+			
+			button.Width = step + 5.0f;
+			button.X = step * index - 5.0f;
+		}
+	}
+
+	private void BuildTimelines() {
+		AddTimeline(new TimelineBuilder()
+			.BeginFrameSet(1, 20)
+			.AddLabel(1, 101, AtkTimelineJumpBehavior.PlayOnce, 0)
+			.AddLabel(11, 102, AtkTimelineJumpBehavior.PlayOnce, 0)
+			.EndFrameSet()
+			.Build()
+		);
+	}
+}
---
+++ Nodes/TabBar/TabBarRadioButtonNode.cs
@@ -0,0 +1,311 @@
+﻿using System;
+using System.Numerics;
+using Dalamud.Game.Addon.Events;
+using Dalamud.Game.Text.SeStringHandling;
+using FFXIVClientStructs.FFXIV.Component.GUI;
+using KamiToolKit.Classes.TimelineBuilding;
+
+namespace KamiToolKit.Nodes.TabBar;
+
+public unsafe class TabBarRadioButtonNode : ComponentNode<AtkComponentRadioButton, AtkUldComponentDataRadioButton> {
+
+	public readonly NineGridNode UnselectedNineGridNode;
+	public readonly NineGridNode SelectedNineGridNode;
+	public readonly TextNode LabelNode;
+	
+	public TabBarRadioButtonNode() {
+		SetInternalComponentType(ComponentType.RadioButton);
+		
+		UnselectedNineGridNode = new SimpleNineGridNode {
+			NodeId = 4,
+			Position = new Vector2(-2.0f, -1.0f),
+			TexturePath = "ui/uld/TabButtonA.tex",
+			TextureCoordinates = new Vector2(0.0f, 0.0f),
+			TextureSize = new Vector2(88.0f, 26.0f),
+			LeftOffset = 16,
+			RightOffset = 16,
+			NodeFlags = NodeFlags.AnchorTop | NodeFlags.AnchorLeft | NodeFlags.AnchorRight | NodeFlags.Enabled | NodeFlags.EmitsEvents,
+			IsVisible = true,
+		};
+		UnselectedNineGridNode.AttachNode(this);
+
+		SelectedNineGridNode = new SimpleNineGridNode {
+			NodeId = 3,
+			Position = new Vector2(-2.0f, -1.0f),
+			TexturePath = "ui/uld/TabButtonA.tex",
+			TextureCoordinates = new Vector2(0.0f, 26.0f),
+			TextureSize = new Vector2(88.0f, 26.0f),
+			LeftOffset = 16,
+			RightOffset = 16,
+			NodeFlags = NodeFlags.AnchorTop | NodeFlags.AnchorLeft | NodeFlags.AnchorRight | NodeFlags.Enabled | NodeFlags.EmitsEvents,
+		};
+		SelectedNineGridNode.AttachNode(this);
+
+		LabelNode = new TextNode {
+			NodeId = 2,
+			Position = new Vector2(13.0f, 2.0f),
+			AlignmentType = AlignmentType.Center,
+			NodeFlags = NodeFlags.AnchorLeft | NodeFlags.AnchorRight | NodeFlags.Enabled | NodeFlags.EmitsEvents,
+			IsVisible = true,
+		};
+		LabelNode.AttachNode(this);
+
+		BuildTimelines();
+
+		Data->Nodes[0] = LabelNode.NodeId;
+		Data->Nodes[1] = UnselectedNineGridNode.NodeId;
+		Data->Nodes[2] = 0;
+		Data->Nodes[3] = 0;
+		
+		AddEvent(AddonEventType.ButtonClick, ClickHandler);
+		
+		InitializeComponentEvents();
+	}
+	
+	public Action? OnClick { get; set; }
+	
+	private void ClickHandler(AddonEventData obj) {
+		OnClick?.Invoke();
+	}
+
+	public SeString Label {
+		get => LabelNode.Text;
+		set => Component->SetText(value.ToString());
+	}
+	
+	public override float Width {
+		get => base.Width;
+		set {
+			base.Width = value;
+			CollisionNode.Width = value;
+			UnselectedNineGridNode.Width = value + 4.0f;
+			SelectedNineGridNode.Width = value + 4.0f;
+			LabelNode.Width = value - 25.0f;
+		}
+	}
+
+	public bool IsSelected {
+		get => Component->IsSelected;
+		set {
+			Component->IsSelected = value;
+			if (value) {
+				SelectedNineGridNode.IsVisible = true;
+				UnselectedNineGridNode.IsVisible = false;
+			}
+			else {
+				SelectedNineGridNode.IsVisible = false;
+				UnselectedNineGridNode.IsVisible = true;
+			}
+		}
+	}
+
+	public bool IsEnabled {
+		get => Component->IsEnabled;
+		set => Component->SetEnabledState(value);
+	}
+
+	public bool IsChecked {
+		get => Component->IsChecked;
+		set => Component->SetChecked(value);
+	}
+
+	public override float Height {
+		get => base.Height;
+		set {
+			base.Height = value;
+			CollisionNode.Height = value;
+			UnselectedNineGridNode.Height = value + 2.0f;
+			SelectedNineGridNode.Height = value + 2.0f;
+			LabelNode.Height = value - 4.0f;
+		}
+	}
+
+	private void BuildTimelines() {
+		AddTimeline(new TimelineBuilder()
+			.BeginFrameSet(11, 20)
+			.AddFrame(11, position: new Vector2(525,0))
+			.EndFrameSet()
+			.Build()
+		);
+		
+		UnselectedNineGridNode.AddTimeline(new TimelineBuilder()
+			.BeginFrameSet(1, 9)
+			.AddFrame(1, alpha: 255)
+			.AddFrame(1, addColor: new Vector3(0, 0, 0), multiplyColor: new Vector3(100, 100, 100))
+			.EndFrameSet()
+			.BeginFrameSet(10, 19)
+			.AddFrame(10, alpha: 255)
+			.AddFrame(12, alpha: 255)
+			.AddFrame(10, addColor: new Vector3(0, 0, 0), multiplyColor: new Vector3(100, 100, 100))
+			.AddFrame(12, addColor: new Vector3(16, 16, 16), multiplyColor: new Vector3(100, 100, 100))
+			.EndFrameSet()
+			.BeginFrameSet(20, 29)
+			.AddFrame(20, alpha: 255)
+			.AddFrame(20, addColor: new Vector3(16, 16, 16), multiplyColor: new Vector3(100, 100, 100))
+			.EndFrameSet()
+			.BeginFrameSet(30, 39)
+			.AddFrame(30, alpha: 178)
+			.AddFrame(30, addColor: new Vector3(0, 0, 0), multiplyColor: new Vector3(50, 50, 50))
+			.EndFrameSet()
+			.BeginFrameSet(40, 49)
+			.AddFrame(40, alpha: 255)
+			.AddFrame(40, addColor: new Vector3(16, 16, 16), multiplyColor: new Vector3(100, 100, 100))
+			.EndFrameSet()
+			.BeginFrameSet(50, 59)
+			.AddFrame(50, alpha: 255)
+			.AddFrame(52, alpha: 255)
+			.AddFrame(50, addColor: new Vector3(16, 16, 16), multiplyColor: new Vector3(100, 100, 100))
+			.AddFrame(52, addColor: new Vector3(0, 0, 0), multiplyColor: new Vector3(100, 100, 100))
+			.EndFrameSet()
+			.BeginFrameSet(120, 129)
+			.AddFrame(120, alpha: 255)
+			.AddFrame(122, alpha: 0)
+			.AddFrame(120, addColor: new Vector3(16, 16, 16), multiplyColor: new Vector3(100, 100, 100))
+			.AddFrame(122, addColor: new Vector3(16, 16, 16), multiplyColor: new Vector3(100, 100, 100))
+			.EndFrameSet()
+			.BeginFrameSet(130, 139)
+			.AddFrame(130, alpha: 0)
+			.AddFrame(132, alpha: 255)
+			.AddFrame(130, addColor: new Vector3(16, 16, 16), multiplyColor: new Vector3(100, 100, 100))
+			.AddFrame(132, addColor: new Vector3(16, 16, 16), multiplyColor: new Vector3(100, 100, 100))
+			.EndFrameSet()
+			.BeginFrameSet(140, 149)
+			.AddFrame(140, alpha: 255)
+			.AddFrame(142, alpha: 0)
+			.AddFrame(140, addColor: new Vector3(0, 0, 0), multiplyColor: new Vector3(100, 100, 100))
+			.AddFrame(142, addColor: new Vector3(0, 0, 0), multiplyColor: new Vector3(100, 100, 100))
+			.EndFrameSet()
+			.BeginFrameSet(150, 159)
+			.AddFrame(150, alpha: 0)
+			.AddFrame(152, alpha: 255)
+			.AddFrame(150, addColor: new Vector3(0, 0, 0), multiplyColor: new Vector3(100, 100, 100))
+			.AddFrame(152, addColor: new Vector3(0, 0, 0), multiplyColor: new Vector3(100, 100, 100))
+			.EndFrameSet()
+			.Build()
+		);
+		
+		SelectedNineGridNode.AddTimeline(new TimelineBuilder()
+			.BeginFrameSet(60, 69)
+			.AddFrame(60, alpha: 255)
+			.AddFrame(60, addColor: new Vector3(0, 0, 0), multiplyColor: new Vector3(100, 100, 100))
+			.EndFrameSet()
+			.BeginFrameSet(70, 79)
+			.AddFrame(70, alpha: 255)
+			.AddFrame(72, alpha: 255)
+			.AddFrame(70, addColor: new Vector3(0, 0, 0), multiplyColor: new Vector3(100, 100, 100))
+			.AddFrame(72, addColor: new Vector3(16, 16, 16), multiplyColor: new Vector3(100, 100, 100))
+			.EndFrameSet()
+			.BeginFrameSet(80, 89)
+			.AddFrame(80, alpha: 255)
+			.AddFrame(80, addColor: new Vector3(16, 16, 16), multiplyColor: new Vector3(100, 100, 100))
+			.EndFrameSet()
+			.BeginFrameSet(90, 99)
+			.AddFrame(90, alpha: 178)
+			.AddFrame(90, addColor: new Vector3(0, 0, 0), multiplyColor: new Vector3(50, 50, 50))
+			.EndFrameSet()
+			.BeginFrameSet(100, 109)
+			.AddFrame(100, alpha: 255)
+			.AddFrame(100, addColor: new Vector3(16, 16, 16), multiplyColor: new Vector3(100, 100, 100))
+			.EndFrameSet()
+			.BeginFrameSet(110, 119)
+			.AddFrame(110, alpha: 255)
+			.AddFrame(112, alpha: 255)
+			.AddFrame(110, addColor: new Vector3(16, 16, 16), multiplyColor: new Vector3(100, 100, 100))
+			.AddFrame(112, addColor: new Vector3(0, 0, 0), multiplyColor: new Vector3(100, 100, 100))
+			.EndFrameSet()
+			.BeginFrameSet(120, 129)
+			.AddFrame(120, alpha: 0)
+			.AddFrame(122, alpha: 255)
+			.AddFrame(120, addColor: new Vector3(16, 16, 16), multiplyColor: new Vector3(100, 100, 100))
+			.AddFrame(122, addColor: new Vector3(16, 16, 16), multiplyColor: new Vector3(100, 100, 100))
+			.EndFrameSet()
+			.BeginFrameSet(130, 139)
+			.AddFrame(130, alpha: 255)
+			.AddFrame(132, alpha: 0)
+			.AddFrame(130, addColor: new Vector3(16, 16, 16), multiplyColor: new Vector3(100, 100, 100))
+			.AddFrame(132, addColor: new Vector3(16, 16, 16), multiplyColor: new Vector3(100, 100, 100))
+			.EndFrameSet()
+			.BeginFrameSet(140, 149)
+			.AddFrame(140, alpha: 0)
+			.AddFrame(142, alpha: 255)
+			.AddFrame(140, addColor: new Vector3(0, 0, 0), multiplyColor: new Vector3(100, 100, 100))
+			.AddFrame(142, addColor: new Vector3(0, 0, 0), multiplyColor: new Vector3(100, 100, 100))
+			.EndFrameSet()
+			.BeginFrameSet(150, 159)
+			.AddFrame(150, alpha: 255)
+			.AddFrame(152, alpha: 0)
+			.AddFrame(150, addColor: new Vector3(0, 0, 0), multiplyColor: new Vector3(100, 100, 100))
+			.AddFrame(152, addColor: new Vector3(0, 0, 0), multiplyColor: new Vector3(100, 100, 100))
+			.EndFrameSet()
+			.Build()
+		);
+		
+		LabelNode.AddTimeline(new TimelineBuilder()
+			.BeginFrameSet(1, 9)
+			.AddFrame(1, alpha: 255)
+			.AddFrame(1, addColor: new Vector3(0, 0, 0), multiplyColor: new Vector3(100, 100, 100))
+			.EndFrameSet()
+			.BeginFrameSet(10, 19)
+			.AddFrame(10, alpha: 255)
+			.AddFrame(10, addColor: new Vector3(0, 0, 0), multiplyColor: new Vector3(100, 100, 100))
+			.EndFrameSet()
+			.BeginFrameSet(20, 29)
+			.AddFrame(20, alpha: 255)
+			.AddFrame(20, addColor: new Vector3(0, 0, 0), multiplyColor: new Vector3(100, 100, 100))
+			.EndFrameSet()
+			.BeginFrameSet(30, 39)
+			.AddFrame(30, alpha: 153)
+			.AddFrame(30, addColor: new Vector3(0, 0, 0), multiplyColor: new Vector3(80, 80, 80))
+			.EndFrameSet()
+			.BeginFrameSet(40, 49)
+			.AddFrame(40, alpha: 255)
+			.AddFrame(40, addColor: new Vector3(0, 0, 0), multiplyColor: new Vector3(100, 100, 100))
+			.EndFrameSet()
+			.BeginFrameSet(50, 59)
+			.AddFrame(50, alpha: 255)
+			.AddFrame(50, addColor: new Vector3(0, 0, 0), multiplyColor: new Vector3(100, 100, 100))
+			.EndFrameSet()
+			.BeginFrameSet(60, 69)
+			.AddFrame(60, alpha: 255)
+			.AddFrame(60, addColor: new Vector3(0, 0, 0), multiplyColor: new Vector3(100, 100, 100))
+			.EndFrameSet()
+			.BeginFrameSet(70, 79)
+			.AddFrame(70, alpha: 255)
+			.AddFrame(70, addColor: new Vector3(0, 0, 0), multiplyColor: new Vector3(100, 100, 100))
+			.EndFrameSet()
+			.BeginFrameSet(80, 89)
+			.AddFrame(80, alpha: 255)
+			.AddFrame(80, addColor: new Vector3(0, 0, 0), multiplyColor: new Vector3(100, 100, 100))
+			.EndFrameSet()
+			.BeginFrameSet(90, 99)
+			.AddFrame(90, alpha: 153)
+			.AddFrame(90, addColor: new Vector3(0, 0, 0), multiplyColor: new Vector3(80, 80, 80))
+			.EndFrameSet()
+			.BeginFrameSet(100, 109)
+			.AddFrame(100, alpha: 255)
+			.AddFrame(100, addColor: new Vector3(0, 0, 0), multiplyColor: new Vector3(100, 100, 100))
+			.EndFrameSet()
+			.BeginFrameSet(110, 119)
+			.AddFrame(110, alpha: 255)
+			.AddFrame(110, addColor: new Vector3(0, 0, 0), multiplyColor: new Vector3(100, 100, 100))
+			.EndFrameSet()
+			.BeginFrameSet(120, 129)
+			.AddFrame(120, alpha: 255)
+			.AddFrame(120, addColor: new Vector3(0, 0, 0), multiplyColor: new Vector3(100, 100, 100))
+			.EndFrameSet()
+			.BeginFrameSet(130, 139)
+			.AddFrame(130, alpha: 255)
+			.AddFrame(130, addColor: new Vector3(0, 0, 0), multiplyColor: new Vector3(100, 100, 100))
+			.EndFrameSet()
+			.BeginFrameSet(140, 149)
+			.AddFrame(140, alpha: 255)
+			.AddFrame(140, addColor: new Vector3(0, 0, 0), multiplyColor: new Vector3(100, 100, 100))
+			.EndFrameSet()
+			.BeginFrameSet(150, 159)
+			.AddFrame(150, alpha: 255)
+			.AddFrame(150, addColor: new Vector3(0, 0, 0), multiplyColor: new Vector3(100, 100, 100))
+			.EndFrameSet()
+			.Build()
+		);
+	}
+}
