f5cab66dca572a54e92eff8bc0be8cc5115f2953
MidoriKami <mi_kami@outlook.com>
2025-06-22 22:38:32 -0700

    Add more HorizontalFlex options

--- Nodes/Layout/HorizontalFlexNode.cs
+++ Nodes/Layout/HorizontalFlexNode.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using KamiToolKit.Classes;
 using KamiToolKit.System;
 
 namespace KamiToolKit.Nodes;
@@ -7,27 +8,36 @@ namespace KamiToolKit.Nodes;
 public class HorizontalFlexNode<T> : SimpleComponentNode where T : NodeBase {
 
 	private readonly List<T> nodeList = [];
+
+	public FlexFlags AlignmentFlags { get; set; } = FlexFlags.FitContentHeight;
 	
-	public bool FitHeight { get; set; }
-	public bool FitWidth { get; set; }
 	public float FitPadding { get; set; } = 4.0f;
-	public bool FitContentHeight { get; set; }
 
 	public void RecalculateLayout() {
 		var step = Width / nodeList.Count;
 
-		if (nodeList.Count != 0 && FitContentHeight) {
+		if (nodeList.Count != 0 && AlignmentFlags.HasFlag(FlexFlags.FitContentHeight)) {
 			Height = nodeList.Max(node => node.Height);
 		}
 		
 		foreach (var index in Enumerable.Range(0, nodeList.Count)) {
-			nodeList[index].X = step * index;
 
-			if (FitHeight) {
-				nodeList[index].Height = Height;
+			if (AlignmentFlags.HasFlag(FlexFlags.CenterHorizontally)) {
+				nodeList[index].X = step * index + step / 2.0f - nodeList[index].Width / 2.0f;
+			}
+			else {
+				nodeList[index].X = step * index;
 			}
 
-			if (FitWidth) {
+			if (AlignmentFlags.HasFlag(FlexFlags.FitHeight)) {
+				nodeList[index].Height = Height;
+			}
+			
+			if (AlignmentFlags.HasFlag(FlexFlags.CenterVertically)) {
+				nodeList[index].Y = Height / 2 - nodeList[index].Height / 2;
+			}
+
+			if (AlignmentFlags.HasFlag(FlexFlags.FitWidth)) {
 				nodeList[index].Width = step - FitPadding;
 			}
 		}
