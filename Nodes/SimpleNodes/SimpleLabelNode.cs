26014f67e3649f85d699ee37bd3d922134bb63a3
MidoriKami <mi_kami@outlook.com>
2025-06-24 11:10:08 -0700

    Add SimpleLabelNode

---
+++ Nodes/SimpleNodes/SimpleLabelNode.cs
@@ -0,0 +1,19 @@
+﻿using System.Drawing;
+using Dalamud.Interface;
+using FFXIVClientStructs.FFXIV.Component.GUI;
+using KamiToolKit.Classes;
+
+namespace KamiToolKit.Nodes;
+
+// Simple helper class for making basic text lable, node will auto-resize to fit label
+public sealed class SimpleLabelNode : TextNode {
+	public SimpleLabelNode() {
+		Height = 16.0f;
+		TextFlags = TextFlags.AutoAdjustNodeSize;
+		TextColor = ColorHelper.GetColor(2);
+		TextOutlineColor = KnownColor.Black.Vector();
+		FontType = FontType.Axis;
+		FontSize = 14;
+		AlignmentType = AlignmentType.Left;
+	}
+}
