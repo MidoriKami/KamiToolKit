6d8c8a6fe71d9eb0cead868fe201797b543433dd
MidoriKami <mi_kami@outlook.com>
2025-06-25 18:09:43 -0700

    Add bottomleft resize

--- System/NodeBase.Resize.cs
+++ System/NodeBase.Resize.cs
@@ -90,6 +90,15 @@ public abstract unsafe partial class NodeBase {
 			EnableEventFlags = true,
 		};
 		bottomRightResizeNode.AttachNode(resizeContainer);
+
+		bottomLeftResizeNode = new ResizeButtonNode(ResizeDirection.BottomLeft) {
+			Position = new Vector2(16.0f, resizeContainer.Size.Y - 28.0f - 16.0f),
+			Size = new Vector2(28.0f, 28.0f),
+			Origin = new Vector2(14.0f, 14.0f),
+			IsVisible = true,
+			EnableEventFlags = true,
+		};
+		bottomLeftResizeNode.AttachNode(resizeContainer);
 		
 		resizeContainer.AttachNode(this);
 	}
@@ -98,27 +107,34 @@ public abstract unsafe partial class NodeBase {
 		if (resizeContainer is null) return;
 		if (bottomRightResizeNode is null) return;
 		if (resizeEventListener is null) return;
+		if (bottomLeftResizeNode is null) return;
 
 		ref var mouseData = ref atkEventData->MouseData;
 		var mousePosition = new Vector2(mouseData.PosX,  mouseData.PosY);
 		
 		switch (eventType) {
 			case AtkEventType.MouseMove when !isResizeDragging: {
-				if (resizeContainer.CheckCollision(atkEventData)) {
-					bottomRightResizeNode.IsHovered = bottomRightResizeNode.CheckCollision(atkEventData);
-				}
-				else {
-					bottomRightResizeNode.IsHovered = false;
-				}
+				bottomRightResizeNode.IsHovered = bottomRightResizeNode.CheckCollision(atkEventData);
+				bottomLeftResizeNode.IsHovered = bottomLeftResizeNode.CheckCollision(atkEventData);
+				
 			} break;
 
 			case AtkEventType.MouseMove when isResizeDragging: {
 				var newPosition = mousePosition;
 				var delta = newPosition - resizeClickStart;
 		
-				Size += delta;
-				resizeContainer.Size += delta;
-				bottomRightResizeNode.Position += delta;
+				if (bottomLeftResizeNode.IsHovered) {
+					Position += delta with { Y = 0.0f };
+					Size += new Vector2(-delta.X, delta.Y);
+				}
+
+				if (bottomRightResizeNode.IsHovered) {
+					Size += delta;
+				}
+				
+				resizeContainer.Size = Size + new Vector2(32.0f, 32.0f);
+				bottomLeftResizeNode.Position = new Vector2(16.0f, resizeContainer.Size.Y - 28.0f - 16.0f);
+				bottomRightResizeNode.Position = resizeContainer.Size - new Vector2(28.0f, 28.0f) - new Vector2(16.0f, 16.0f);
 
 				resizeClickStart = newPosition;
 				atkEvent->SetEventIsHandled(true);
@@ -145,10 +161,12 @@ public abstract unsafe partial class NodeBase {
 		ResetCursor();
 
 		if (bottomRightResizeNode.IsHovered) SetCursor(AddonCursorType.ResizeNWSR);
+		if (bottomLeftResizeNode.IsHovered) SetCursor(AddonCursorType.ResizeNESW);
 	}
 
 	private bool IsAnyButtonHovered(AtkEventData* atkEventData) {
 		if (bottomRightResizeNode?.CheckCollision(atkEventData) ?? false) return true;
+		if (bottomLeftResizeNode?.CheckCollision(atkEventData) ?? false) return true;
 
 		return false;
 	}
