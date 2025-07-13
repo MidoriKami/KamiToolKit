e13cf8d32692f72fb1eced5046328c166659706f
MidoriKami <mi_kami@outlook.com>
2025-07-13 12:16:18 -0700

    Add helper for attaching nodes

---
+++ Addon/NativeAddon.Linking.cs
@@ -0,0 +1,12 @@
+﻿using KamiToolKit.Classes;
+using KamiToolKit.System;
+
+namespace KamiToolKit.Addon;
+
+public abstract partial class NativeAddon {
+	public void AttachNode(NodeBase node, NodePosition position = NodePosition.AsLastChild)
+		=> NativeController.AttachNode(node, this,  position);
+	
+	public void AttachNode(NodeBase node, NodeBase target,  NodePosition position = NodePosition.AfterAllSiblings)
+		=> NativeController.AttachNode(node, target, position);
+}
