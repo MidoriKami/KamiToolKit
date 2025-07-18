84a1802546930e9005f5c122096171a694c1c4ef
MidoriKami <mi_kami@outlook.com>
2025-07-18 09:42:45 -0700

    Add ImageNodeExtension for getting icon id

---
+++ Extensions/AtkImageNodeExtensions.cs
@@ -0,0 +1,15 @@
+﻿using FFXIVClientStructs.FFXIV.Component.GUI;
+
+namespace KamiToolKit.Extensions;
+
+public static unsafe class AtkImageNodeExtensions {
+	public static uint GetIconId(this ref AtkImageNode node) {
+		if (node.PartsList is null) return 0;
+		if (node.PartsList->Parts is null) return 0;
+		if (node.PartsList->Parts->UldAsset is null) return 0;
+		if (node.PartsList->Parts->UldAsset->AtkTexture.TextureType is not TextureType.Resource) return 0;
+		if (node.PartsList->Parts->UldAsset->AtkTexture.Resource is null) return 0;
+		
+		return node.PartsList->Parts->UldAsset->AtkTexture.Resource->IconId;
+	}
+}
