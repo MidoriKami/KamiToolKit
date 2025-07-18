5fb1e25a3aee58061f5639adbc3ae63fd9623f84
MidoriKami <mi_kami@outlook.com>
2025-07-18 15:06:23 -0700

    Split lambda code into actual functions for debugging

--- NativeController.cs
+++ NativeController.cs
@@ -41,79 +41,88 @@ public unsafe class NativeController : IDisposable {
 		});
 
 	public void AttachNode(NodeBase customNode, NodeBase targetNode, NodePosition? position = null)
-		=> Framework.RunOnFrameworkThread(() => {
-			Log.Verbose($"[NativeController] Attaching [{customNode.GetType()}] to another Custom Node [{targetNode.GetType()}]");
-			var addon = GetAddonForNode(targetNode.InternalResNode);
-
-			switch (targetNode) {
-
-				// Don't attach directly to ComponentNode, attach to its managed RootNode
-				case ComponentNode componentNode:
-					customNode.AttachNode(componentNode, position ?? NodePosition.AfterAllSiblings);
-					customNode.EnableEvents(addon);
-					return;
-
-				default:
-					customNode.AttachNode(targetNode, position ?? NodePosition.AsLastChild);
-					customNode.EnableEvents(addon);
-					return;
-			}
-		});
+		=> Framework.RunOnFrameworkThread(() => AttachToNodeBase(customNode, targetNode, position));
 
 	public void AttachNode(NodeBase customNode, AtkResNode* targetNode, NodePosition? position = null)
-		=> Framework.RunOnFrameworkThread(() => {
-			Log.Verbose($"[NativeController] Attaching [{customNode.GetType()}:{(nint) customNode.InternalResNode:X}] to a native AtkResNode");
-			var addon = GetAddonForNode(targetNode);
+		=> Framework.RunOnFrameworkThread(() => AttachToAtkResNode(customNode, targetNode, position));
 
-			customNode.RegisterAutoDetach(addon);
-			customNode.AttachNode(targetNode, position ?? NodePosition.AsLastChild);
-			customNode.EnableEvents(addon);
-		});
+	public void AttachNode(NodeBase customNode, AtkComponentNode* targetNode, NodePosition position = NodePosition.AfterAllSiblings)
+		=> Framework.RunOnFrameworkThread(() => AttachToAtkComponentNode(customNode, targetNode, position));
 
-	public void AttachNode(NodeBase customNode, AtkComponentNode* targetNode, NodePosition position = NodePosition.AfterAllSiblings) {
-		Framework.RunOnFrameworkThread(() => {
-			if (targetNode->GetNodeType() is not NodeType.Component) {
-				Log.Error("TargetNode type was expected to be Component but was not. Aborting attach.");
-				return;
-			}
-			
-			Log.Verbose($"[NativeController] Attaching [{customNode.GetType()}:{(nint) customNode.InternalResNode:X}] to a native AtkComponentNode");
-
-			var addon = GetAddonForNode((AtkResNode*) targetNode);
-			if (addon is not null) {
-				Log.Verbose($"[NativeController] Tried to get Addon from native AtkComponentNode, found: {addon->NameString}");
-
-				customNode.RegisterAutoDetach(addon);
-				customNode.AttachNode(targetNode, position);
-				customNode.EnableEvents(addon);
-			}
-			else {
-				Log.Error($"[NativeController] Attempted to attach [{customNode.GetType()}:{(nint) customNode.InternalResNode:X}] to a native AtkComponentNode, but could not find parent addon. Aborting.");
-			}
-		});
-	}
-
-	public void AttachNode(NodeBase customNode, NativeAddon targetAddon, NodePosition? position = null) {
-		Framework.RunOnFrameworkThread(() => {
-			Log.Verbose($"[NativeController] Attaching [{customNode.GetType()}:{(nint) customNode.InternalResNode:X}] to a Custom Addon [{targetAddon.GetType()}]");
-
-			customNode.AttachNode(targetAddon, position ?? NodePosition.AsLastChild);
-			customNode.EnableEvents(targetAddon.InternalAddon);
-		});
-	}
+	public void AttachNode(NodeBase customNode, NativeAddon targetAddon, NodePosition? position = null)
+		=> Framework.RunOnFrameworkThread(() => AttachToNativeAddon(customNode, targetAddon, position));
 
 	public void DetachNode(NodeBase? customNode, Action? disposeAction = null)
-		=> Framework.RunOnFrameworkThread(() => {
-			if (customNode is not null) {
-				Log.Verbose($"[NativeController] Detaching [{customNode.GetType()}:{(nint) customNode.InternalResNode:X}] from all sources.");
-			}
+		=> Framework.RunOnFrameworkThread(() => DetachNodeTask(customNode, disposeAction));
+	
+	private void AttachToNodeBase(NodeBase customNode, NodeBase targetNode, NodePosition? position) {
 
-			customNode?.DisableEditMode(NodeEditMode.Move | NodeEditMode.Resize);
-			customNode?.UnregisterAutoDetach();
-			customNode?.DisableEvents();
-			customNode?.DetachNode();
-			disposeAction?.Invoke();
-		});
+		Log.Verbose($"[NativeController] Attaching [{customNode.GetType()}] to another Custom Node [{targetNode.GetType()}]");
+		var addon = GetAddonForNode(targetNode.InternalResNode);
+
+		switch (targetNode) {
+
+			// Don't attach directly to ComponentNode, attach to its managed RootNode
+			case ComponentNode componentNode:
+				customNode.AttachNode(componentNode, position ?? NodePosition.AfterAllSiblings);
+				customNode.EnableEvents(addon);
+				return;
+
+			default:
+				customNode.AttachNode(targetNode, position ?? NodePosition.AsLastChild);
+				customNode.EnableEvents(addon);
+				return;
+		}
+	}
+	
+	private void AttachToAtkResNode(NodeBase customNode, AtkResNode* targetNode, NodePosition? position) {
+		Log.Verbose($"[NativeController] Attaching [{customNode.GetType()}:{(nint) customNode.InternalResNode:X}] to a native AtkResNode");
+		var addon = GetAddonForNode(targetNode);
+
+		customNode.RegisterAutoDetach(addon);
+		customNode.AttachNode(targetNode, position ?? NodePosition.AsLastChild);
+		customNode.EnableEvents(addon);
+	}
+
+	private static void AttachToNativeAddon(NodeBase customNode, NativeAddon targetAddon, NodePosition? position) {
+		Log.Verbose($"[NativeController] Attaching [{customNode.GetType()}:{(nint) customNode.InternalResNode:X}] to a Custom Addon [{targetAddon.GetType()}]");
+
+		customNode.AttachNode(targetAddon, position ?? NodePosition.AsLastChild);
+		customNode.EnableEvents(targetAddon.InternalAddon);
+	}
+
+	private void AttachToAtkComponentNode(NodeBase customNode, AtkComponentNode* targetNode, NodePosition position) {
+		if (targetNode->GetNodeType() is not NodeType.Component) {
+			Log.Error("TargetNode type was expected to be Component but was not. Aborting attach.");
+			return;
+		}
+			
+		Log.Verbose($"[NativeController] Attaching [{customNode.GetType()}:{(nint) customNode.InternalResNode:X}] to a native AtkComponentNode");
+
+		var addon = GetAddonForNode((AtkResNode*) targetNode);
+		if (addon is not null) {
+			Log.Verbose($"[NativeController] Tried to get Addon from native AtkComponentNode, found: {addon->NameString}");
+
+			customNode.RegisterAutoDetach(addon);
+			customNode.AttachNode(targetNode, position);
+			customNode.EnableEvents(addon);
+		}
+		else {
+			Log.Error($"[NativeController] Attempted to attach [{customNode.GetType()}:{(nint) customNode.InternalResNode:X}] to a native AtkComponentNode, but could not find parent addon. Aborting.");
+		}
+	}
+
+	private static void DetachNodeTask(NodeBase? customNode, Action? disposeAction) {
+		if (customNode is not null) {
+			Log.Verbose($"[NativeController] Detaching [{customNode.GetType()}:{(nint) customNode.InternalResNode:X}] from all sources.");
+		}
+
+		customNode?.DisableEditMode(NodeEditMode.Move | NodeEditMode.Resize);
+		customNode?.UnregisterAutoDetach();
+		customNode?.DisableEvents();
+		customNode?.DetachNode();
+		disposeAction?.Invoke();
+	}
 
 	private AtkUnitBase* GetAddonForNode(AtkResNode* node)
 		=> RaptureAtkUnitManager.Instance()->GetAddonByNode(node);
