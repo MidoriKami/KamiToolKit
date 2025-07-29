3ac70b14bbecd0d2eba6540ad46382b9a0f500d9
MidoriKami <mi_kami@outlook.com>
2025-07-29 15:56:03 -0700

    Add OrderedVerticalListNode

---
+++ Nodes/Layout/OrderedVerticalListNode.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using KamiToolKit.Classes;
+using KamiToolKit.System;
+
+namespace KamiToolKit.Nodes;
+
+public class OrderedVerticalListNode<T, TU> : VerticalListNode where T : NodeBase {
+
+    public Func<T,TU>? OrderSelector { get; set; }
+
+    public override void RecalculateLayout() {
+        if (NodeList is not List<T> typedNodeList || OrderSelector is null) {
+            base.RecalculateLayout();
+            return;
+        }
+        
+        var orderedList = typedNodeList.OrderBy(OrderSelector).ToList();
+        
+        var startY = Alignment switch {
+            VerticalListAnchor.Top => 0.0f + FirstItemSpacing,
+            VerticalListAnchor.Bottom => Height,
+            _ => 0.0f,
+        };
+
+        foreach (var node in orderedList) {
+            if (!node.IsVisible) continue;
+
+            if (Alignment is VerticalListAnchor.Bottom) {
+                startY -= node.Height + ItemSpacing;
+            }
+
+            node.Y = startY;
+            AdjustNode(node);
+
+            if (Alignment is VerticalListAnchor.Top) {
+                startY += node.Height + ItemSpacing;
+            }
+        }
+
+        if (FitContents) {
+            Height = orderedList.Sum(node => node.IsVisible ? node.Height + ItemSpacing : 0.0f) + FirstItemSpacing;
+        }
+    }
+}
