e1b81602eff80264911c1b9c12e34ae6b5a9350b
MidoriKami <mi_kami@outlook.com>
2025-08-10 16:17:42 -0700

    Add span extension

---
+++ Extensions/ReadOnlySpanExtensions.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Text;
+
+namespace KamiToolKit.Extensions;
+
+public static class ReadOnlySpanExtensions {
+    public static string GetString(this ReadOnlySpan<byte> span)
+        => Encoding.UTF8.GetString(span);
+}
