5139ac2b1d3563837ca657a956f25bbd650ba83c
MidoriKami <mi_kami@outlook.com>
2025-08-10 20:16:06 -0700

    Revert list populator data to normal AtkUnitBase

--- Classes/ListPopulatorData.cs
+++ Classes/ListPopulatorData.cs
@@ -3,8 +3,8 @@ using ListItemInfo = FFXIVClientStructs.FFXIV.Component.GUI.AtkComponentListItem
 
 namespace KamiToolKit.Classes;
 
-public unsafe class ListPopulatorData<T> where T : unmanaged {
-    public T* Addon { get; init; }
+public unsafe class ListPopulatorData {
+    public AtkUnitBase* Addon { get; init; }
     public ListItemInfo* ItemInfo { get; init; }
     public AtkResNode** NodeList { get; init; }
     public uint Index { get; init; }
