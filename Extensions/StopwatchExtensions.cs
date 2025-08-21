7ecd3e5f0e544f0f894853ae1099f817f015c684
MidoriKami <mi_kami@outlook.com>
2025-08-20 23:08:05 -0700

    Add stopwatch extensions

---
+++ Extensions/StopwatchExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System.Diagnostics;
+using KamiToolKit.Classes;
+
+namespace KamiToolKit.Extensions;
+
+public static class StopwatchExtensions {
+    public static void LogTime(this Stopwatch stopwatch, string logMessage) {
+        DalamudInterface.Instance.Log.Debug($"{logMessage, -15}: {stopwatch, 15} :: {stopwatch.ElapsedMilliseconds} ms");
+        stopwatch.Restart();
+    }
+}
