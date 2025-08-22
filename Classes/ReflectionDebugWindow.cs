d297c0d77749f5b0223b2442cb7db48000d0f5dc
MidoriKami <mi_kami@outlook.com>
2025-08-22 09:54:38 -0700

    Add ReflectionDebugWindow.cs

---
+++ Classes/ReflectionDebugWindow.cs
@@ -0,0 +1,29 @@
+﻿using Dalamud.Bindings.ImGui;
+using Dalamud.Interface.Utility.Raii;
+using Dalamud.Interface.Windowing;
+
+namespace KamiToolKit.Classes;
+
+public class ReflectionDebugWindow() : Window("Reflection Debug Window") {
+
+    public static void Open() {
+        var newDebugWindow = new ReflectionDebugWindow();
+        DalamudInterface.Instance.PluginInterface.UiBuilder.Draw += newDebugWindow.Draw;
+        newDebugWindow.IsOpen = true;
+    }
+
+    public override void Draw() {
+        foreach (var (type, subtypes) in NativeController.ChildMembers) {
+            using var header = ImRaii.TreeNode(type.ToString());
+            if (header) {
+                foreach (var subtype in subtypes) {
+                    ImGui.Text(subtype.Name);
+                }
+            }
+        }
+    }
+
+    public override void OnClose() {
+        DalamudInterface.Instance.PluginInterface.UiBuilder.Draw -= Draw;
+    }
+}
