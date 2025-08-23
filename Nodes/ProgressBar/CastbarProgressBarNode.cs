4b6c23f9ffbd330e6a45d325b1347894c81245b7
MidoriKami <mi_kami@outlook.com>
2025-08-22 18:54:48 -0700

    Rework CastbarProgressBarNode.cs

--- Nodes/ProgressBar/CastbarProgressBarNode.cs
+++ Nodes/ProgressBar/CastbarProgressBarNode.cs
@@ -2,7 +2,6 @@
 using System.Numerics;
 using Dalamud.Interface;
 using Dalamud.Interface.Utility.Raii;
-using FFXIVClientStructs.FFXIV.Component.GUI;
 using KamiToolKit.Extensions;
 using Newtonsoft.Json;
 
@@ -11,55 +10,45 @@ namespace KamiToolKit.Nodes;
 [JsonObject(MemberSerialization.OptIn)]
 public unsafe class CastBarProgressBarNode : SimpleComponentNode {
 
-    [JsonProperty] public readonly SimpleNineGridNode BackgroundImageNode;
-    [JsonProperty] public readonly SimpleNineGridNode BorderImageNode;
-    [JsonProperty] public readonly SimpleNineGridNode ProgressNode;
+    [JsonProperty] public readonly NineGridNode BackgroundImageNode;
+    [JsonProperty] public readonly NineGridNode ProgressNode;
+    [JsonProperty] public readonly NineGridNode BorderImageNode;
 
-    // Recommended aspect ratio for width:height is 8:1
     public CastBarProgressBarNode() {
         BackgroundImageNode = new SimpleNineGridNode {
             NodeId = 2,
-            Size = new Vector2(160.0f, 20.0f),
+            TexturePath = "ui/uld/Parameter_Gauge.tex",
             TextureSize = new Vector2(160.0f, 20.0f),
             TextureCoordinates = new Vector2(0.0f, 100.0f),
-            NodeFlags = NodeFlags.Visible,
-            PartsRenderType = 4,
-            LeftOffset = 15,
-            RightOffset = 15,
-            TexturePath = "ui/uld/Parameter_Gauge.tex",
+            LeftOffset = 20,
+            RightOffset = 20,
+            IsVisible = true,
         };
-
         BackgroundImageNode.AttachNode(this);
-
-        BorderImageNode = new SimpleNineGridNode {
-            NodeId = 4,
-            Size = new Vector2(160.0f, 20.0f),
-            TextureSize = new Vector2(160.0f, 20.0f),
-            TextureCoordinates = new Vector2(0.0f, 0.0f),
-            NodeFlags = NodeFlags.Visible,
-            PartsRenderType = 4,
-            LeftOffset = 15,
-            RightOffset = 15,
-            TexturePath = "ui/uld/Parameter_Gauge.tex",
-        };
-
-        BorderImageNode.AttachNode(this);
 
         ProgressNode = new SimpleNineGridNode {
             NodeId = 3,
-            Size = new Vector2(160.0f, 20.0f),
+            TexturePath = "ui/uld/Parameter_Gauge.tex",
             TextureSize = new Vector2(160.0f, 20.0f),
             TextureCoordinates = new Vector2(0.0f, 40.0f),
-            NodeFlags = NodeFlags.Visible,
-            Width = 80,
-            Color = KnownColor.Yellow.Vector(),
+            MultiplyColor = new Vector3(90.0f, 75.0f, 75.0f) / 255.0f,
+            AddColor = KnownColor.Yellow.Vector().AsVector3Color() / 255.0f,
             LeftOffset = 10,
             RightOffset = 10,
-            PartsRenderType = 4,
-            TexturePath = "ui/uld/Parameter_Gauge.tex",
+            IsVisible = true,
         };
-
         ProgressNode.AttachNode(this);
+        
+        BorderImageNode = new SimpleNineGridNode {
+            NodeId = 4,
+            TexturePath = "ui/uld/Parameter_Gauge.tex",
+            TextureSize = new Vector2(160.0f, 20.0f),
+            TextureCoordinates = new Vector2(0.0f, 0.0f),
+            LeftOffset = 20,
+            RightOffset = 20,
+            IsVisible = true,
+        };
+        BorderImageNode.AttachNode(this);
     }
 
     public float Progress {
@@ -70,7 +59,7 @@ public unsafe class CastBarProgressBarNode : SimpleComponentNode {
     public Vector4 BackgroundColor {
         get => new(BackgroundImageNode.AddColor.X, BackgroundImageNode.AddColor.Y, BackgroundImageNode.AddColor.Z, BackgroundImageNode.InternalResNode->Color.A / 255.0f);
         set {
-            BackgroundImageNode.InternalResNode->Color = new Vector4(0.0f, 0.0f, 0.0f, value.W).ToByteColor();
+            BackgroundImageNode.InternalResNode->Color = new Vector4(1.0f, 1.0f, 1.0f, value.W).ToByteColor();
             BackgroundImageNode.AddColor = value.AsVector3Color();
         }
     }
@@ -78,7 +67,7 @@ public unsafe class CastBarProgressBarNode : SimpleComponentNode {
     public Vector4 BorderColor {
         get => new(BorderImageNode.AddColor.X, BorderImageNode.AddColor.Y, BorderImageNode.AddColor.Z, BorderImageNode.InternalResNode->Color.A / 255.0f);
         set {
-            BorderImageNode.InternalResNode->Color = new Vector4(0.0f, 0.0f, 0.0f, value.W).ToByteColor();
+            BorderImageNode.InternalResNode->Color = new Vector4(1.0f, 1.0f, 1.0f, value.W).ToByteColor();
             BorderImageNode.AddColor = value.AsVector3Color();
         }
     }
@@ -86,7 +75,7 @@ public unsafe class CastBarProgressBarNode : SimpleComponentNode {
     public Vector4 BarColor {
         get => new(ProgressNode.AddColor.X, ProgressNode.AddColor.Y, ProgressNode.AddColor.Z, ProgressNode.InternalResNode->Color.A / 255.0f);
         set {
-            ProgressNode.InternalResNode->Color = new Vector4(0.0f, 0.0f, 0.0f, value.W).ToByteColor();
+            ProgressNode.InternalResNode->Color = new Vector4(1.0f, 1.0f, 1.0f, value.W).ToByteColor();
             ProgressNode.AddColor = value.AsVector3Color();
         }
     }
@@ -98,6 +87,7 @@ public unsafe class CastBarProgressBarNode : SimpleComponentNode {
 
     protected override void OnSizeChanged() {
         base.OnSizeChanged();
+
         BackgroundImageNode.Size = Size;
         ProgressNode.Size = Size;
         BorderImageNode.Size = Size;
