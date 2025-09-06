ec4704b299c78b85d618059e3dc17c045b6bf845
MidoriKami <mi_kami@outlook.com>
2025-09-05 21:58:32 -0700

    Potentially fix focus related crashes

--- Classes/Experimental.cs
+++ Classes/Experimental.cs
@@ -1,4 +1,6 @@
-﻿using FFXIVClientStructs.FFXIV.Client.UI.Agent;
+﻿using Dalamud.Utility.Signatures;
+using FFXIVClientStructs.FFXIV.Client.UI.Agent;
+using FFXIVClientStructs.FFXIV.Component.GUI;
 
 namespace KamiToolKit.Classes;
 #if DEBUG
@@ -13,6 +15,11 @@ internal unsafe class Experimental {
     public void EnableHooks() { }
 
     public void DisposeHooks() { }
+
+    public delegate bool SetFocusDelegate(AtkInputManager* inputManager, AtkResNode* resNode, AtkUnitBase* addon, int focusParam);
+
+    [Signature("E8 ?? ?? ?? ?? 49 8B 84 FF ?? ?? ?? ??")]
+    public SetFocusDelegate? SetFocus = null;
 
 #if DEBUG
     // WARNING: May result in undefined state or accidental network requests
