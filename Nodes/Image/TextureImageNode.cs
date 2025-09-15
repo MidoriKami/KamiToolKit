f0da3daaa8a19cdfc22161fc1fa81ccaca346e86
MidoriKami <mi_kami@outlook.com>
2025-09-15 14:35:26 -0700

    Add textureimagenode

---
+++ Nodes/Image/TextureImageNode.cs
@@ -0,0 +1,26 @@
+﻿using FFXIVClientStructs.FFXIV.Client.Graphics.Kernel;
+using FFXIVClientStructs.FFXIV.Component.GUI;
+
+namespace KamiToolKit.Nodes;
+
+/// <summary>
+/// WARNING: This is a non-owning texture image node.
+/// This node is meant to reference a texture that is owned elsewhere.
+/// </summary>
+public unsafe class TextureImageNode : SimpleImageNode {
+    public void SetTexture(Texture* texture) {
+        var asset = PartsList[0].InternalPart->UldAsset;
+        asset->AtkTexture.KernelTexture = texture;
+        asset->AtkTexture.TextureType = TextureType.KernelTexture;
+    }
+
+    protected override void Dispose(bool disposing) {
+        if (disposing) {
+            var asset = PartsList[0].InternalPart->UldAsset;
+            asset->AtkTexture.KernelTexture = null;
+            asset->AtkTexture.TextureType = 0;
+        }
+
+        base.Dispose(disposing);
+    }
+}
