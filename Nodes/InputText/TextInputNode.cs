21a411bff89dbca6b658b4ad14e6da0326d26ed0
Middo <18119928570@163.com>
2025-09-29 18:44:42 +0800

    Upgrade the TextMultiLineInputNode as an inheritance of TextInputNode

--- Nodes/InputText/TextInputNode.cs
+++ Nodes/InputText/TextInputNode.cs
@@ -183,12 +183,12 @@ public unsafe class TextInputNode : ComponentNode<AtkComponentTextInput, AtkUldC
         }
     }
 
-    public SeString SeString {
+    public virtual SeString SeString {
         get => SeString.Parse(Component->UnkText1);
         set => Component->SetText(value.ToString());
     }
 
-    public string String {
+    public virtual string String {
         get => Component->UnkText1.ToString();
         set => Component->SetText(value);
     }
@@ -219,7 +219,7 @@ public unsafe class TextInputNode : ComponentNode<AtkComponentTextInput, AtkUldC
         virtualTable->UpdateCursor = (delegate* unmanaged<AtkTextInput.AtkTextInputEventInterface*, TextSelectionInfo*, void>)Marshal.GetFunctionPointerForDelegate(pinnedFunction);
     }
     
-    private void OnCursorChanged(AtkTextInput.AtkTextInputEventInterface* listener, TextSelectionInfo* numEvents) {
+    protected virtual void OnCursorChanged(AtkTextInput.AtkTextInputEventInterface* listener, TextSelectionInfo* numEvents) {
         var applySelectAll = !FocusNode.IsVisible && AutoSelectAll;
 
         if (applySelectAll) {
