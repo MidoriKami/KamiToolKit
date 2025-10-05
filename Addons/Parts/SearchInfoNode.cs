4e4d02648b3fd6191affdb66500f18d36968e578
MidoriKami <mi_kami@outlook.com>
2025-10-05 08:05:27 -0700

    Add prefab search window

---
+++ Addons/Parts/SearchInfoNode.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Drawing;
+using System.Numerics;
+using Dalamud.Interface;
+using Dalamud.Utility;
+using FFXIVClientStructs.FFXIV.Component.GUI;
+using KamiToolKit.Classes;
+using KamiToolKit.Nodes;
+
+namespace KamiToolKit.Addons.Parts;
+
+internal unsafe class SearchInfoNode<T> : SimpleComponentNode {
+    private readonly NineGridNode hoveredNineGridNode;
+    private readonly NineGridNode selectedNineGridNode;
+    
+    private readonly IconImageNode iconNode;
+    private readonly TextNode labelTextNode;
+    private readonly TextNode subLabelTextNode;
+    private readonly TextNode idTextNode;
+
+    private CustomEventListener? eventListener;
+
+    public SearchInfoNode() {
+        eventListener = new CustomEventListener(HandleEvents);
+        
+        hoveredNineGridNode = new SimpleNineGridNode {
+            NodeId = 2,
+            TexturePath = "ui/uld/ListItemA.tex",
+            TextureCoordinates = new Vector2(0.0f, 22.0f),
+            TextureSize = new Vector2(64.0f, 22.0f),
+            TopOffset = 6,
+            BottomOffset = 6,
+            LeftOffset = 16,
+            RightOffset = 1,
+            IsVisible = false,
+        };
+        hoveredNineGridNode.AttachNode(this);
+
+        selectedNineGridNode = new SimpleNineGridNode {
+            NodeId = 3,
+            TexturePath = "ui/uld/ListItemA.tex",
+            TextureCoordinates = new Vector2(0.0f, 0.0f),
+            TextureSize = new Vector2(64.0f, 22.0f),
+            TopOffset = 6,
+            BottomOffset = 6,
+            LeftOffset = 16,
+            RightOffset = 1,
+            IsVisible = false,
+        };
+        selectedNineGridNode.AttachNode(this);
+
+        iconNode = new IconImageNode {
+            IsVisible = true,
+            FitTexture = true,
+            IconId = 60072,
+        };
+        iconNode.AttachNode(this);
+
+        labelTextNode = new TextNode {
+            TextFlags = TextFlags.Ellipsis,
+            FontSize = 14,
+            AlignmentType = AlignmentType.BottomLeft,
+            IsVisible = true,
+        };
+        labelTextNode.AttachNode(this);
+
+        subLabelTextNode = new TextNode {
+            TextFlags = TextFlags.Ellipsis,
+            FontSize = 12,
+            AlignmentType = AlignmentType.TopLeft,
+            TextColor = KnownColor.Gray.Vector(),
+        };
+        subLabelTextNode.AttachNode(this);
+
+        idTextNode = new TextNode {
+            FontSize = 10,
+            AlignmentType = AlignmentType.BottomRight,
+            TextColor = KnownColor.Gray.Vector(),
+            IsVisible = true,
+        };
+        idTextNode.AttachNode(this);
+        
+        CollisionNode.InternalResNode->AtkEventManager.RegisterEvent(
+            AtkEventType.MouseOver,
+            0,
+            null,
+            (AtkEventTarget*)CollisionNode.InternalResNode,
+            eventListener.EventListener,
+            false);
+
+        CollisionNode.InternalResNode->AtkEventManager.RegisterEvent(
+            AtkEventType.MouseOut,
+            0,
+            null,
+            (AtkEventTarget*)CollisionNode.InternalResNode,
+            eventListener.EventListener,
+            false);
+        
+        CollisionNode.InternalResNode->AtkEventManager.RegisterEvent(
+            AtkEventType.MouseClick,
+            0,
+            null,
+            (AtkEventTarget*)CollisionNode.InternalResNode,
+            eventListener.EventListener,
+            false);
+
+        CollisionNode.DrawFlags |= DrawFlags.ClickableCursor;
+    }
+
+    protected override void DisposeManagedResources() {
+        base.DisposeManagedResources();
+
+        eventListener?.Dispose();
+        eventListener = null;
+    }
+
+    private void HandleEvents(AtkEventListener* thisPtr, AtkEventType eventType, int eventParam, AtkEvent* atkEvent, AtkEventData* atkEventData) {
+        switch (eventType) {
+            case AtkEventType.MouseOver:
+                IsHovered = true;
+                break;
+            
+            case AtkEventType.MouseOut:
+                IsHovered = false;
+                break;
+            
+            case AtkEventType.MouseClick:
+                OnClicked(this);
+                break;
+        }
+    }
+
+    protected override void OnSizeChanged() {
+        base.OnSizeChanged();
+
+        hoveredNineGridNode.Size = Size;
+        selectedNineGridNode.Size = Size;
+
+        iconNode.Size = new Vector2(Height - 4.0f, Height - 4.0f);
+        iconNode.Position = new Vector2(2.0f, 2.0f);
+
+        labelTextNode.Size = new Vector2(Width - Height - 2.0f - 25.0f, Height / 2.0f);
+        labelTextNode.Position = new Vector2(Height + 2.0f, 0.0f);
+
+        subLabelTextNode.Size = new Vector2(Width - Height - 2.0f - 10.0f, Height / 2.0f);
+        subLabelTextNode.Position = new Vector2(Height + 2.0f + 10.0f, Height / 2.0f);
+        
+        idTextNode.Size = new Vector2(25.0f, Height / 2.0f);
+        idTextNode.Position = new Vector2(Width - 25.0f, 0.0f);
+    }
+
+    public required OptionInfo<T> OptionInfo {
+        get;
+        init {
+            field = value;
+
+            labelTextNode.String = value.Label;
+            subLabelTextNode.String = value.SubLabel ?? string.Empty;
+            idTextNode.String = value.Id?.ToString() ?? string.Empty;
+
+            if (value.IconId is { } iconId) {
+                iconNode.IconId = iconId;
+            }
+
+            if (!value.TexturePath.IsNullOrEmpty()) {
+                iconNode.LoadTexture(value.TexturePath);
+            }
+        }
+    }
+
+    public bool IsHovered {
+        get => hoveredNineGridNode.IsVisible;
+        set => hoveredNineGridNode.IsVisible = value;
+    }
+    
+    public bool IsSelected {
+        get => selectedNineGridNode.IsVisible;
+        set => selectedNineGridNode.IsVisible = value;
+    }
+    
+    public required Action<SearchInfoNode<T>> OnClicked { get; init; }
+}
---
+++ Addons/SearchAddon.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using FFXIVClientStructs.FFXIV.Component.GUI;
+using KamiToolKit.Addon;
+using KamiToolKit.Addons.Parts;
+using KamiToolKit.Nodes;
+using KamiToolKit.System;
+using KamiToolKit.Widgets;
+
+namespace KamiToolKit.Addons;
+
+public class SearchAddon<T> : NativeAddon {
+
+    public required List<string> SortingOptions { get; init; }
+
+    private SearchWidget? searchWidget;
+    private ScrollingAreaNode<VerticalListNode>? listNode;
+
+    private TextButtonNode? cancelButton;
+    private TextButtonNode? confirmButton;
+
+    private SearchInfoNode<T>? selectedOption;
+
+    protected override unsafe void OnSetup(AtkUnitBase* addon) {
+        searchWidget = new SearchWidget {
+            Size = ContentSize,
+            Position = ContentStartPosition,
+            FilterOptions = SortingOptions,
+            OnSortOrderChanged = OnSortOrderUpdated,
+            OnSearchUpdated = OnSearchUpdated,
+            IsVisible = true,
+        };
+        AttachNode(searchWidget);
+
+        listNode = new ScrollingAreaNode<VerticalListNode> {
+            Position = new Vector2(ContentStartPosition.X, searchWidget.Y + searchWidget.Height + 8.0f),
+            Size = new Vector2(ContentSize.X, ContentSize.Y - searchWidget.Height - 16.0f - 24.0f - 8.0f),
+            IsVisible = true,
+            AutoHideScrollBar = true,
+            ContentHeight = 10.0f,
+        };
+        AttachNode(listNode);
+
+        const float buttonPadding = 20.0f;
+        var contentWidth = ContentSize.X - buttonPadding * 2;
+        var buttonWidth = contentWidth / 3.0f;
+        
+        cancelButton = new TextButtonNode {
+            Size = new Vector2(buttonWidth, 24.0f),
+            Position = new Vector2(ContentStartPosition.X, ContentStartPosition.Y + ContentSize.Y - 24.0f - 8.0f),
+            IsVisible = true,
+            String = "Cancel",
+            OnClick = OnCancelClicked,
+        };
+        AttachNode(cancelButton);
+        
+        confirmButton = new TextButtonNode {
+            Size = new Vector2(buttonWidth, 24.0f),
+            Position = new Vector2(ContentStartPosition.X + buttonWidth * 2 + buttonPadding * 2, ContentStartPosition.Y + ContentSize.Y - 24.0f - 8.0f),
+            IsVisible = true,
+            IsEnabled = false,
+            String = "Confirm",
+            OnClick = OnConfirmClicked,
+        };
+        AttachNode(confirmButton);
+        
+        foreach (var option in SearchOptions) {
+            listNode.ContentNode.AddNode(BuildOptionNode(GetOptionInfo(option)), true);
+        }
+        
+        listNode.ContentHeight = listNode.ContentNode.Nodes.Sum(node => node.IsVisible ? node.Height : 0.0f) + 10.0f;
+        listNode.ContentNode.RecalculateLayout();
+    }
+
+    private void OnCancelClicked() {
+        selectedOption = null;
+        Close();
+    }
+
+    private void OnConfirmClicked() {
+        if (selectedOption != null) {
+            SelectionResult.Invoke(selectedOption.OptionInfo.Option);
+        }
+        
+        selectedOption = null;
+        Close();
+    }
+
+    private SearchInfoNode<T> BuildOptionNode(OptionInfo<T> option) => new() {
+        Width = listNode!.ContentNode.Width,
+        Height = 48.0f,
+        OptionInfo = option,
+        IsVisible = true,
+        OnClicked = OnOptionClicked,
+    };
+
+    private void OnOptionClicked(SearchInfoNode<T> clickedOption) {
+        if (confirmButton is null) return;
+        
+        // Unselect Previous Option
+        if (selectedOption is not null) {
+            selectedOption.IsSelected = false;
+            selectedOption = null;
+        }
+
+        // Select New Option
+        selectedOption = clickedOption;
+        selectedOption.IsSelected = true;
+
+        // Enable Confirm Button
+        confirmButton.IsEnabled = true;
+    }
+
+    private void OnSortOrderUpdated(string filterString, bool reversed)
+        => listNode?.ContentNode.ReorderNodes((left, right) => 
+            NodeComparison(left, right, filterString, reversed));
+
+    private int NodeComparison(NodeBase x, NodeBase y, string filterString, bool isReversed) {
+        if (x is not SearchInfoNode<T> left || y is not SearchInfoNode<T> right) return 0;
+
+        var leftValue = left.OptionInfo;
+        var rightValue = right.OptionInfo;
+
+        var comparisonResult = ItemComparison(leftValue.Option, rightValue.Option, filterString);
+        
+        return isReversed ? -comparisonResult : comparisonResult;
+    }
+
+    private void OnSearchUpdated(string searchString) {
+        if (listNode is null) return;
+
+        foreach (var node in listNode.ContentNode.GetNodes<SearchInfoNode<T>>()) {
+            node.IsVisible = node.OptionInfo.ContainsSearchTerm(searchString);
+        }
+
+        listNode.ContentHeight = listNode.ContentNode.Nodes.Sum(node => node.IsVisible ? node.Height : 0.0f) + 10.0f;
+        listNode.ContentNode.RecalculateLayout();
+    }
+
+    public required List<T> SearchOptions { get; init; }
+
+    public delegate OptionInfo<T> GetOptionInfoDelegate(T option);
+    public required GetOptionInfoDelegate GetOptionInfo { get; init; }
+
+    public delegate int Comparison(T left, T right, string selectedOrder);
+    public required Comparison ItemComparison { get; init; }
+
+    public required Action<T> SelectionResult { get; init; }
+}
