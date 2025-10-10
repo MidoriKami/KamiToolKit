52c41657a29ad9c7ef59b7f0a675db88d7f07fe2
MidoriKami <mi_kami@outlook.com>
2025-10-10 08:47:21 -0700

    Add default footer padding to not clip into window frame

--- Addon/NativeAddon.Properties.cs
+++ Addon/NativeAddon.Properties.cs
@@ -137,7 +137,7 @@ public abstract unsafe partial class NativeAddon {
 
     public Vector2 ContentStartPosition => WindowNode.ContentStartPosition + ContentPadding;
 
-    public Vector2 ContentSize => WindowNode.ContentSize - ContentPadding * 2.0f;
+    public Vector2 ContentSize => WindowNode.ContentSize - ContentPadding * 2.0f - new Vector2(0.0f, 4.0f);
 
     public Vector2 ContentPadding => new(8.0f, 0.0f);
 
