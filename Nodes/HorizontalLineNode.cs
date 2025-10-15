a74149002432ef7bd4a9aeb955738a0ea835c746
MidoriKami <mi_kami@outlook.com>
2025-10-15 16:34:30 -0700

    Add Horizontal and Vertical Line Nodes

---
+++ Nodes/HorizontalLineNode.cs
@@ -0,0 +1,13 @@
+﻿using System.Numerics;
+
+namespace KamiToolKit.Nodes;
+
+public class HorizontalLineNode : SimpleNineGridNode {
+    public HorizontalLineNode() {
+        TexturePath = "ui/uld/WindowA_Line.tex";
+        TextureCoordinates = Vector2.Zero;
+        TextureSize = new Vector2(32.0f, 4.0f);
+        LeftOffset = 12.0f;
+        RightOffset = 12.0f;
+    }
+}
---
+++ Nodes/VerticalLineNode.cs
@@ -0,0 +1,17 @@
+﻿namespace KamiToolKit.Nodes;
+
+public sealed unsafe class VerticalLineNode : HorizontalLineNode {
+    public VerticalLineNode() {
+        RotationDegrees = 90.0f;
+    }
+
+    public override float Height {
+        get => InternalResNode->GetWidth();
+        set => InternalResNode->SetWidth((ushort) value);
+    }
+
+    public override float Width { 
+        get => InternalResNode->GetHeight();
+        set => InternalResNode->SetHeight((ushort) value);
+    }
+}
