492c3fb9d4b203bbb8ce29c21084944b34ed801a
MidoriKami <mi_kami@outlook.com>
2025-10-19 17:10:15 -0700

    Add standalone alphabar node

--- Widgets/ColorPickerWidget.cs
+++ Widgets/ColorPickerWidget.cs
@@ -1,31 +1,18 @@
 ﻿using System.Numerics;
-using Dalamud.Game.Addon.Events;
-using Dalamud.Game.Addon.Events.EventDataTypes;
 using Dalamud.Interface;
-using FFXIVClientStructs.FFXIV.Component.GUI;
-using KamiToolKit.Classes;
 using KamiToolKit.Nodes;
 using KamiToolKit.Widgets.Parts;
 
 namespace KamiToolKit.Widgets;
 
-public unsafe class ColorPickerWidget : SimpleComponentNode {
+public class ColorPickerWidget : SimpleComponentNode {
     public readonly ColorRingWithSquareNode ColorPickerNode;
-    
-    public readonly ImGuiImageNode AlphaBarBackgroundNode;
-    public readonly ImGuiImageNode AlphaBarNode;
-    public readonly ImGuiImageNode AlphaBarSelectorNode;
-
+    public readonly AlphaBarNode AlphaBarNode;
     public readonly ColorPreviewWithInput ColorPreviewWithInput;
-
-    private ViewportEventListener alphaEventListener;
-    private bool isAlphaDragging;
 
     public ColorHelpers.HsvaColor CurrentColor { get; set; }
     
     public ColorPickerWidget() {
-        alphaEventListener = new ViewportEventListener(AlphaSliderEvent);
-        
         ColorPickerNode = new ColorRingWithSquareNode {
             IsVisible = true,
             OnHueChanged = SetHue,
@@ -34,31 +21,12 @@ public unsafe class ColorPickerWidget : SimpleComponentNode {
         };
         ColorPickerNode.AttachNode(this);
 
-        AlphaBarBackgroundNode = new ImGuiImageNode {
-            TexturePath = DalamudInterface.Instance.GetAssetPath("alpha_background.png"),
+        AlphaBarNode = new AlphaBarNode {
             IsVisible = true,
-            WrapMode = WrapMode.Tile,
-        };
-        AlphaBarBackgroundNode.AttachNode(this);
-
-        AlphaBarNode = new ImGuiImageNode {
-            TexturePath = DalamudInterface.Instance.GetAssetPath("VerticalGradient_WhiteToAlpha.png"),
-            IsVisible = true,
-            FitTexture = true,
-            EnableEventFlags = true,
+            OnAlphaChanged = SetAlpha,
         };
         AlphaBarNode.AttachNode(this);
-        AlphaBarNode.AddEvent(AddonEventType.MouseDown, OnAlphaBarMouseDown);
-
-        AlphaBarSelectorNode = new ImGuiImageNode {
-            TexturePath = DalamudInterface.Instance.GetAssetPath("alpha_selector.png"),
-            IsVisible = true,
-            FitTexture = true,
-            EnableEventFlags = true,
-        };
-        AlphaBarSelectorNode.AttachNode(this);
-        AlphaBarSelectorNode.AddEvent(AddonEventType.MouseDown, OnAlphaBarMouseDown);
-
+        
         ColorPreviewWithInput = new ColorPreviewWithInput {
             IsVisible = true,
             OnHsvaColorChanged = newColor => {
@@ -77,14 +45,6 @@ public unsafe class ColorPickerWidget : SimpleComponentNode {
         SetAlpha(CurrentColor.A);
     }
 
-    protected override void Dispose(bool disposing, bool isNativeDestructor) {
-        if (disposing) {
-            base.Dispose(disposing, isNativeDestructor);
-
-            alphaEventListener.Dispose();
-        }
-    }
-
     protected override void OnSizeChanged() {
         base.OnSizeChanged();
 
@@ -92,50 +52,15 @@ public unsafe class ColorPickerWidget : SimpleComponentNode {
         
         ColorPickerNode.Size = new Vector2(mainWidgetWidth, mainWidgetWidth);
 
-        AlphaBarBackgroundNode.Size = new Vector2(Width / 16.0f, mainWidgetWidth - 60.0f);
-        AlphaBarBackgroundNode.Position = new Vector2(mainWidgetWidth + (Width - mainWidgetWidth) / 3.0f - AlphaBarBackgroundNode.Width / 2.0f, 30.0f);
-
         AlphaBarNode.Size = new Vector2(Width / 16.0f, mainWidgetWidth - 60.0f);
-        AlphaBarNode.Position = new Vector2(mainWidgetWidth + (Width - mainWidgetWidth) / 3.0f - AlphaBarBackgroundNode.Width / 2.0f, 30.0f);
-
-        AlphaBarSelectorNode.Size = new Vector2(AlphaBarNode.Width + 4.0f, 10.0f);
-        AlphaBarSelectorNode.Position = new Vector2(AlphaBarNode.X - 2.0f, AlphaBarNode.Y);
+        AlphaBarNode.Position = new Vector2(mainWidgetWidth + (Width - mainWidgetWidth) / 3.0f - AlphaBarNode.Width / 2.0f, 30.0f);
 
         ColorPreviewWithInput.Size = new Vector2(150.0f, 32.0f);
         ColorPreviewWithInput.Position = new Vector2(Width / 2.0f - 75.0f, ColorPickerNode.Y + ColorPickerNode.Height - 1.0f);
     }
-    
-    private void OnAlphaBarMouseDown(AddonEventData obj) {
-        if (!isAlphaDragging) {
-            alphaEventListener.AddEvent(AtkEventType.MouseMove, AlphaBarNode);
-            alphaEventListener.AddEvent(AtkEventType.MouseUp, AlphaBarNode);
-            isAlphaDragging = true;
-        }
-    }
-    
-    private void AlphaSliderEvent(AtkEventListener* thisPtr, AtkEventType eventType, int eventParam, AtkEvent* atkEvent, AtkEventData* atkEventData) {
-        if (eventType is AtkEventType.MouseUp) {
-            alphaEventListener.RemoveEvent(AtkEventType.MouseMove);
-            alphaEventListener.RemoveEvent(AtkEventType.MouseUp);
-            isAlphaDragging = false;
-        }
-
-        if (eventType is AtkEventType.MouseMove) {
-            var mousePosition = new Vector2(atkEventData->MouseData.PosX, atkEventData->MouseData.PosY);
-            var minY = AlphaBarNode.ScreenY;
-            var maxY = AlphaBarNode.ScreenY + AlphaBarNode.Height;
-
-            if (mousePosition.Y >= minY && mousePosition.Y <= maxY) {
-                var alphaRatio = 1.0f - (mousePosition.Y - AlphaBarNode.ScreenY) / AlphaBarNode.Height;
-                SetAlpha(alphaRatio);
-            }
-        }
-    }
 
     public void SetAlpha(float alpha) {
         CurrentColor = CurrentColor with { A = alpha };
-
-        AlphaBarSelectorNode.Y = AlphaBarNode.Y + AlphaBarNode.Height - AlphaBarNode.Height * CurrentColor.A - 5.0f;
 
         ColorPreviewWithInput.HsvaColor = CurrentColor;
     }
@@ -149,7 +74,7 @@ public unsafe class ColorPickerWidget : SimpleComponentNode {
 
         ColorPreviewWithInput.HsvaColor = CurrentColor;
 
-        AlphaBarNode.HsvaMultiplyColor = CurrentColor with { A = 1.0f };
+        AlphaBarNode.HsvaColor = CurrentColor with { A = 1.0f };
     }
 
     public void SetSaturation(float saturation) {
@@ -158,7 +83,7 @@ public unsafe class ColorPickerWidget : SimpleComponentNode {
         ColorPreviewWithInput.HsvaColor = CurrentColor;
         ColorPickerNode.SelectorColor = CurrentColor;
         
-        AlphaBarNode.HsvaMultiplyColor = CurrentColor with { A = 1.0f };
+        AlphaBarNode.HsvaColor = CurrentColor with { A = 1.0f };
     }
 
     public void SetValue(float value) {
@@ -167,6 +92,6 @@ public unsafe class ColorPickerWidget : SimpleComponentNode {
         ColorPreviewWithInput.HsvaColor = CurrentColor;
         ColorPickerNode.SelectorColor = CurrentColor;
 
-        AlphaBarNode.HsvaMultiplyColor = CurrentColor with { A = 1.0f };
+        AlphaBarNode.HsvaColor = CurrentColor with { A = 1.0f };
     }
 }
---
+++ Widgets/Parts/AlphaBarNode.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Numerics;
+using Dalamud.Game.Addon.Events;
+using Dalamud.Game.Addon.Events.EventDataTypes;
+using Dalamud.Interface;
+using FFXIVClientStructs.FFXIV.Component.GUI;
+using KamiToolKit.Classes;
+using KamiToolKit.Nodes;
+
+namespace KamiToolKit.Widgets.Parts;
+
+public unsafe class AlphaBarNode : SimpleComponentNode {
+    public readonly ImGuiImageNode AlphaBarBackgroundNode;
+    public readonly ImGuiImageNode AlphaBarGradientNode;
+    public readonly ImGuiImageNode AlphaBarSelectorNode;
+    
+    private ViewportEventListener alphaEventListener;
+    private bool isAlphaDragging;
+
+    public AlphaBarNode() {
+        alphaEventListener = new ViewportEventListener(AlphaSliderEvent);
+
+        AlphaBarBackgroundNode = new ImGuiImageNode {
+            TexturePath = DalamudInterface.Instance.GetAssetPath("alpha_background.png"),
+            IsVisible = true,
+            WrapMode = WrapMode.Tile,
+        };
+        AlphaBarBackgroundNode.AttachNode(this);
+
+        AlphaBarGradientNode = new ImGuiImageNode {
+            TexturePath = DalamudInterface.Instance.GetAssetPath("VerticalGradient_WhiteToAlpha.png"),
+            IsVisible = true,
+            FitTexture = true,
+            EnableEventFlags = true,
+        };
+        AlphaBarGradientNode.AttachNode(this);
+        AlphaBarGradientNode.AddEvent(AddonEventType.MouseDown, OnAlphaBarMouseDown);
+
+        AlphaBarSelectorNode = new ImGuiImageNode {
+            TexturePath = DalamudInterface.Instance.GetAssetPath("alpha_selector.png"),
+            IsVisible = true,
+            FitTexture = true,
+            EnableEventFlags = true,
+        };
+        AlphaBarSelectorNode.AttachNode(this);
+        AlphaBarSelectorNode.AddEvent(AddonEventType.MouseDown, OnAlphaBarMouseDown);
+    }
+    
+    protected override void Dispose(bool disposing, bool isNativeDestructor) {
+        if (disposing) {
+            base.Dispose(disposing, isNativeDestructor);
+
+            alphaEventListener.Dispose();
+        }
+    }
+
+    protected override void OnSizeChanged() {
+        base.OnSizeChanged();
+
+        AlphaBarBackgroundNode.Size = Size;
+        AlphaBarGradientNode.Size = Size;
+
+        AlphaBarSelectorNode.Size = new Vector2(Width + 4.0f, 10.0f);
+        AlphaBarSelectorNode.Position = new Vector2(-2.0f, 0.0f);
+    }
+
+    private void OnAlphaBarMouseDown(AddonEventData obj) {
+        if (!isAlphaDragging) {
+            alphaEventListener.AddEvent(AtkEventType.MouseMove, AlphaBarGradientNode);
+            alphaEventListener.AddEvent(AtkEventType.MouseUp, AlphaBarGradientNode);
+            isAlphaDragging = true;
+        }
+    }
+    
+    private void AlphaSliderEvent(AtkEventListener* thisPtr, AtkEventType eventType, int eventParam, AtkEvent* atkEvent, AtkEventData* atkEventData) {
+        switch (eventType) {
+            case AtkEventType.MouseUp:
+                alphaEventListener.RemoveEvent(AtkEventType.MouseMove);
+                alphaEventListener.RemoveEvent(AtkEventType.MouseUp);
+                isAlphaDragging = false;
+                break;
+
+            case AtkEventType.MouseMove: {
+                var mousePosition = new Vector2(atkEventData->MouseData.PosX, atkEventData->MouseData.PosY);
+                var minY = AlphaBarGradientNode.ScreenY;
+                var maxY = AlphaBarGradientNode.ScreenY + AlphaBarGradientNode.Height;
+
+                if (mousePosition.Y >= minY && mousePosition.Y <= maxY) {
+                    var alphaRatio = 1.0f - (mousePosition.Y - AlphaBarGradientNode.ScreenY) / AlphaBarGradientNode.Height;
+
+                    AlphaBarSelectorNode.Y = Height - Height * alphaRatio - 5.0f;
+                    OnAlphaChanged?.Invoke(alphaRatio);
+                }
+                else if (mousePosition.Y < minY) {
+                    AlphaBarSelectorNode.Y = -4.0f;
+                    OnAlphaChanged?.Invoke(1.0f);
+                }
+                else if (mousePosition.Y > maxY) {
+                    AlphaBarSelectorNode.Y = Height - 4.0f;
+                    OnAlphaChanged?.Invoke(0.0f);
+                }
+                
+                break;
+            }
+        }
+    }
+
+    public Action<float>? OnAlphaChanged { get; init; }
+
+    public override Vector4 Color {
+        get => AlphaBarGradientNode.Color;
+        set => AlphaBarGradientNode.Color = value;
+    }
+
+    public override ColorHelpers.HsvaColor HsvaColor {
+        get => AlphaBarGradientNode.HsvaMultiplyColor;
+        set => AlphaBarGradientNode.HsvaMultiplyColor = value;
+    }
+}
