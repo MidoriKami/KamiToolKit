d58dfa1cb4178bcbaab4d1efbef5198f226e91a2
MidoriKami <mi_kami@outlook.com>
2025-10-26 08:41:19 -0700

    add generic StringInfoNode

---
+++ Addons/Parts/StringInfoNode.cs
@@ -0,0 +1,20 @@
+﻿using KamiToolKit.Addons.Interfaces;
+
+namespace KamiToolKit.Addons.Parts;
+
+public abstract class StringInfoNode : IInfoNodeData {
+    public string Label { get; set; } = string.Empty;
+
+    public string GetLabel() => Label;
+
+    public abstract string? GetSubLabel();
+
+    public abstract uint? GetId();
+
+    public abstract uint? GetIconId();
+
+    public abstract string? GetTexturePath();
+
+    public int Compare(IInfoNodeData other, string sortingMode)
+        => string.CompareOrdinal(Label, (other as StringInfoNode)?.Label);
+}
