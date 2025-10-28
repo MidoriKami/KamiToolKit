c5ff11d41c5efc37fde04b500bd8da9f3c354968
MidoriKami <mi_kami@outlook.com>
2025-10-27 22:50:34 -0700

    Add IsUnloading helper to Framework

---
+++ Extensions/FrameworkExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.InteropServices;
+using FFXIVClientStructs.FFXIV.Client.System.Framework;
+
+namespace KamiToolKit.Extensions;
+
+public static unsafe class FrameworkExtensions {
+    public static bool IsUnloading(ref this Framework framework) {
+        fixed (Framework* pointer = &framework) {
+            return Marshal.ReadByte((nint)pointer, 0x08) == 1;
+        }
+    }
+}
