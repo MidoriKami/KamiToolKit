c832cf4d2826674794064399be77a9b26608ef1f
MidoriKami <mi_kami@outlook.com>
2025-10-27 22:51:10 -0700

    Add MainThreadSafety Helper

---
+++ Extensions/MainThreadSafety.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.CompilerServices;
+using Dalamud.Utility;
+using FFXIVClientStructs.FFXIV.Client.System.Framework;
+using KamiToolKit.Classes;
+
+namespace KamiToolKit.Extensions;
+
+public static unsafe class MainThreadSafety {
+
+    /// <summary>
+    /// Returns true if <em>not</em> on the main thread. Use this to return early.
+    /// </summary>
+    public static bool TryAssertMainThread([CallerFilePath] string? callerFilePath = null, [CallerMemberName] string? callerName = null) {
+        if (Framework.Instance()->IsUnloading()) return true;
+
+        if (!ThreadSafety.IsMainThread) {
+            Log.Error($"{callerFilePath?.Split(@"\")[^1][..^2]}{callerName} must be invoked from the main thread.");
+            return true;
+        }
+
+        return false;
+    }
+}
