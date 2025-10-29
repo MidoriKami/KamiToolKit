d10af5fa040f2ea884c848272c97067c2b23d689
MidoriKami <mi_kami@outlook.com>
2025-10-29 14:22:34 -0700

    Inline destroying check

--- Extensions/FrameworkExtensions.cs
+++
@@ -1,8 +0,0 @@
-﻿using FFXIVClientStructs.FFXIV.Client.System.Framework;
-
-namespace KamiToolKit.Extensions;
-
-public static class FrameworkExtensions {
-    public static bool IsUnloading(ref this Framework framework)
-        => framework.IsDestroying;
-}
--- Extensions/MainThreadSafety.cs
+++ Extensions/MainThreadSafety.cs
@@ -11,7 +11,7 @@ public static unsafe class MainThreadSafety {
     /// Returns true if <em>not</em> on the main thread. Use this to return early.
     /// </summary>
     public static bool TryAssertMainThread([CallerFilePath] string? callerFilePath = null, [CallerMemberName] string? callerName = null) {
-        if (Framework.Instance()->IsUnloading()) return true;
+        if (Framework.Instance()->IsDestroying) return true;
 
         if (!ThreadSafety.IsMainThread) {
             Log.Error($"{callerFilePath?.Split(@"\")[^1][..^2]}{callerName} must be invoked from the main thread.");
