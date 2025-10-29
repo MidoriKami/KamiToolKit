37674a91ade31df34f273d7e90d499d62741cd91
MidoriKami <mi_kami@outlook.com>
2025-10-29 13:56:19 -0700

    Use clientstructs member

--- Extensions/FrameworkExtensions.cs
+++ Extensions/FrameworkExtensions.cs
@@ -1,12 +1,8 @@
-﻿using System.Runtime.InteropServices;
-using FFXIVClientStructs.FFXIV.Client.System.Framework;
+﻿using FFXIVClientStructs.FFXIV.Client.System.Framework;
 
 namespace KamiToolKit.Extensions;
 
-public static unsafe class FrameworkExtensions {
-    public static bool IsUnloading(ref this Framework framework) {
-        fixed (Framework* pointer = &framework) {
-            return Marshal.ReadByte((nint)pointer, 0x08) == 1;
-        }
-    }
+public static class FrameworkExtensions {
+    public static bool IsUnloading(ref this Framework framework)
+        => framework.IsDestroying;
 }
