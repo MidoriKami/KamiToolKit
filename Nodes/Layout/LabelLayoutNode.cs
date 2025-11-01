977f0567d1afdd955a8868d8bffdefca6d1360e5
MidoriKami <mi_kami@outlook.com>
2025-11-01 13:34:17 -0700

    Add LabelLayoutNode.cs
    Allows fitting subsequent elements more easily, while keeping first element fixed size

---
+++ Nodes/Layout/LabelLayoutNode.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using System.Numerics;
+
+namespace KamiToolKit.Nodes;
+
+public class LabelLayoutNode : LayoutListNode {
+
+    public bool FillWidth { get; set; }
+    
+    protected override void InternalRecalculateLayout() {
+        if (Nodes.Count is 0) return;
+        
+        var labelNode = Nodes[0];
+        
+        
+        var labelNodeWidth = labelNode.Width;
+        labelNode.Position = new Vector2(0.0f, 0.0f);
+
+        var position = labelNodeWidth + FirstItemSpacing;
+        foreach (var node in Nodes.Skip(1)) {
+            node.X = position;
+
+            if (FillWidth) {
+                node.Width = (Width - labelNodeWidth - FirstItemSpacing) / (Nodes.Count - 1); 
+            }
+
+            position += node.Width + ItemSpacing;
+        }
+    }
+}
