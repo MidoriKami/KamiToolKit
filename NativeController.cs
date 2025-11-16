ac689abce2d69e669d9e15b82d758a6da1e06ef5
MidoriKami <mi_kami@outlook.com>
2025-11-15 18:39:43 -0800

    Remove Reflection

--- NativeController.cs
+++ NativeController.cs
@@ -1,9 +1,4 @@
 using System;
-using System.Collections.Concurrent;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Threading;
 using Dalamud.Plugin;
@@ -21,9 +16,6 @@ namespace KamiToolKit;
 ///     also keep track of the allocated nodes to prevent memory leaks.
 /// </summary>
 public unsafe class NativeController : IDisposable {
-
-    internal static readonly ConcurrentDictionary<Type, MemberInfo[]> ChildMembers = [];
-    internal static readonly ConcurrentDictionary<Type, MemberInfo[]> EnumerableMembers = [];
     
     public NativeController(IDalamudPluginInterface pluginInterface) {
         pluginInterface.Inject(this);
@@ -37,9 +29,6 @@ public unsafe class NativeController : IDisposable {
         DalamudInterface.Instance.GameInteropProvider.InitializeFromAttributes(Experimental.Instance);
 
         Experimental.Instance.EnableHooks();
-
-        GenerateKamiToolKitTypeMaps();
-        GenerateCallingAssemblyTypeMap(Assembly.GetCallingAssembly());
 
         // Force enable Verbose so that users are able to get advanced logging information on request.
         DalamudInterface.Instance.Log.MinimumLogLevel = LogEventLevel.Verbose;
@@ -124,118 +113,5 @@ public unsafe class NativeController : IDisposable {
         node?.DisableEditMode(NodeEditMode.Move | NodeEditMode.Resize);
         node?.DetachNode();
         node?.Dispose();
-    }
-
-    private static void GenerateKamiToolKitTypeMaps() {
-        var stopwatch = Stopwatch.StartNew();
-        
-        var types = Assembly.GetExecutingAssembly().GetTypes();
-        var nodeBaseTypes = types.Where(type => typeof(NodeBase).IsAssignableFrom(type));
-
-        foreach (var type in nodeBaseTypes) {
-            Log.Verbose($"Generating TypeMap for: {type}");
-            
-            var members = GetMembers(type);
-            if (members.Length is not 0) {
-                ChildMembers.TryAdd(type, members);
-            }
-
-            var enumerableMembers = GetEnumerables(type);
-            if (enumerableMembers.Length is not 0) {
-                EnumerableMembers.TryAdd(type, enumerableMembers);
-            }
-        }
-        
-        stopwatch.LogTime("KTK TYPEMAP");
-    }
-
-    private static void GenerateCallingAssemblyTypeMap(Assembly callingAssembly) {
-        var stopwatch = Stopwatch.StartNew();
-        
-        var types = callingAssembly.GetTypes();
-        var nodeBaseTypes = types.Where(type => typeof(NodeBase).IsAssignableFrom(type));
-
-        foreach (var type in nodeBaseTypes) {
-            Log.Verbose($"Generating TypeMap for: {type}");
-            
-            var members = GetMembers(type);
-            if (members.Length is not 0) {
-                ChildMembers.TryAdd(type, members);
-            }
-
-            var enumerableMembers = GetEnumerables(type);
-            if (enumerableMembers.Length is not 0) {
-                EnumerableMembers.TryAdd(type, enumerableMembers);
-            }
-        }
-        
-        stopwatch.LogTime("CALLER ASSEMBLY");
-    }
-    
-    private static MemberInfo[] GetMembers(Type type) {
-        var members = type.GetMembers(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
-        var targetMembers = members.Where(member => member.MemberType is MemberTypes.Field or MemberTypes.Property);
-        var assignableMembers = targetMembers.Where(member => typeof(NodeBase).IsAssignableFrom(GetMemberType(member)));
-        var indexableMembers = assignableMembers.Where(IsMemberSingleIndexable);
-        var finalList = indexableMembers.ToArray();
-        
-        return finalList;
-    }
-
-    private static MemberInfo[] GetEnumerables(Type type) {
-        var members = type.GetMembers(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
-        var targetMembers = members.Where(member => member.MemberType is MemberTypes.Field or MemberTypes.Property);
-        var enumerableMembers = targetMembers.Where(IsEnumerableOrArray);
-        var indexableMembers = enumerableMembers.Where(IsMemberSingleIndexable);
-        var finalList = indexableMembers.ToArray();
-        
-        return finalList;
-    }
-
-    private static Type? GetMemberType(MemberInfo member) => member.MemberType switch {
-        MemberTypes.Field => (member as FieldInfo)?.FieldType,
-        MemberTypes.Property => (member as PropertyInfo)?.PropertyType,
-        _ => null,
-    };
-    
-    private static bool IsMemberSingleIndexable(MemberInfo member) {
-        if (member is not PropertyInfo property) return true;
-
-        return property.GetIndexParameters().Length is 0;
-    }
-
-    private static bool IsEnumerableOrArray(MemberInfo member) {
-        var memberType = GetMemberType(member);
-
-        if (typeof(NodeBase).IsAssignableFrom(memberType?.GetGenericArguments().FirstOrDefault())) return true;
-        if (typeof(NodeBase).IsAssignableFrom(memberType?.GetElementType())) return true;
-        
-        return false;
-    }
-
-    private static readonly HashSet<Type> ParsedRuntimeTypes = [];
-
-    internal static void TryAddRuntimeType(Type type) {
-        if (!type.IsGenericType) return;
-        if (!ParsedRuntimeTypes.Add(type)) return;
-
-        Log.Debug($"Generating Runtime Type Mapping for: {type}");
-        var stopwatch = Stopwatch.StartNew();
-
-        if (!ChildMembers.ContainsKey(type)) {
-            var members = GetMembers(type);
-            if (members.Length is not 0) {
-                ChildMembers.TryAdd(type, members);
-            }
-        }
-
-        if (!EnumerableMembers.ContainsKey(type)) {
-            var enumerableMembers = GetEnumerables(type);
-            if (enumerableMembers.Length is not 0) {
-                EnumerableMembers.TryAdd(type, enumerableMembers);
-            }
-        }
-        
-        stopwatch.LogTime("RUNTIMETYPE");
     }
 }
