4703c118f168b13606a9913fa1a4fc1e98f1b75a
MidoriKami <mi_kami@outlook.com>
2025-11-23 17:40:24 -0800

    Add additional overlay layer

--- Classes/OverlayLayer.cs
+++ Classes/OverlayLayer.cs
@@ -3,30 +3,34 @@
 namespace KamiToolKit.Classes;
 
 public enum OverlayLayer {
-    BackLayer,
-    MiddleLayer,
-    FrontLayer,
+    Background,
+    BehindUserInterface,
+    AboveUserInterface,
+    Foreground,
 }
 
 public static class OverlayLayerExtensions {
     public static string GetDescription(this OverlayLayer layer) => layer switch {
-        OverlayLayer.BackLayer => "KTK_Overlay_Back",
-        OverlayLayer.MiddleLayer => "KTK_Overlay_Middle",
-        OverlayLayer.FrontLayer => "KTK_Overlay_Front",
+        OverlayLayer.Background => "KTK_Overlay_Back",
+        OverlayLayer.BehindUserInterface => "KTK_Overlay_Middle",
+        OverlayLayer.AboveUserInterface => "KTK_Overlay_Higher",
+        OverlayLayer.Foreground => "KTK_Overlay_Front",
         _ => throw new Exception("Out of Range"),
     };
 
     public static int GetOverlayLayer(this OverlayLayer layer) => layer switch {
-        OverlayLayer.BackLayer => 1,
-        OverlayLayer.MiddleLayer => 3,
-        OverlayLayer.FrontLayer => 7,
+        OverlayLayer.Background => 1,
+        OverlayLayer.BehindUserInterface => 3,
+        OverlayLayer.AboveUserInterface => 7,
+        OverlayLayer.Foreground => 13,
         _ => 1,
     };
 
     public static OverlayLayer GetOverlayLayer(this uint layer) => layer switch {
-        1 - 1 => OverlayLayer.BackLayer,
-        3 - 1 => OverlayLayer.MiddleLayer,
-        7 - 1 => OverlayLayer.FrontLayer,
+        1 - 1 => OverlayLayer.Background,
+        3 - 1 => OverlayLayer.BehindUserInterface,
+        7 - 1 => OverlayLayer.AboveUserInterface,
+        13 - 1 => OverlayLayer.Foreground,
         _ => throw new Exception("Unknown depth layer: " + layer),
     };
 }
