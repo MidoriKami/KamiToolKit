6680006077d0df44828fbdf453b24fa28b8ca074
MidoriKami <mi_kami@outlook.com>
2025-12-02 12:51:52 -0800

    Implement OverlayAddon that plays well with other plugins

---
+++ Overlay/OverlayController.Addon.cs
@@ -0,0 +1,21 @@
+﻿using FFXIVClientStructs.FFXIV.Component.GUI;
+
+namespace KamiToolKit.Overlay;
+
+internal class OverlayAddon : NativeAddon {
+
+    // This is only for OverlayAddons that are intended to be shared.
+    // You should not attempt to do this for your ui elements.
+    // There is a fixed hard limit on the number of addons that can be loaded at once, we have to share and not be wasteful.
+    protected override unsafe void OnSetup(AtkUnitBase* addon) {
+        addon->WindowNode = null;
+        addon->WindowHeaderCollisionNode = null;
+        addon->WindowCollisionNode = null;
+
+        WindowNode.DetachNode();
+        InternalAddon->UldManager.RemoveNodeFromObjectList(WindowNode);
+        InternalAddon->UldManager.UpdateDrawNodeList();
+
+        WindowNode.Dispose();
+    }
+}
