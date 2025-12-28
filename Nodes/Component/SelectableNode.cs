89dd266fc09ac25aaf2bcee93a7f1bbdbd9bfdad
MidoriKami <mi_kami@outlook.com>
2025-12-28 12:37:16 -0800

    Add SelectableNode

---
+++ Nodes/Component/SelectableNode.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Numerics;
+using FFXIVClientStructs.FFXIV.Component.GUI;
+
+namespace KamiToolKit.Nodes;
+
+public class SelectableNode : SimpleComponentNode {
+    private readonly NineGridNode hoveredBackgroundNode;
+    private readonly NineGridNode selectedBackgroundNode;
+
+    public SelectableNode() {
+        hoveredBackgroundNode = new SimpleNineGridNode {
+            TexturePath = "ui/uld/ListItemA.tex",
+            TextureCoordinates = new Vector2(0.0f, 22.0f),
+            TextureSize = new Vector2(64.0f, 22.0f),
+            TopOffset = 6,
+            BottomOffset = 6,
+            LeftOffset = 16,
+            RightOffset = 1,
+            IsVisible = false,
+        };
+        hoveredBackgroundNode.AttachNode(this);
+        
+        selectedBackgroundNode = new SimpleNineGridNode {
+            TexturePath = "ui/uld/ListItemA.tex",
+            TextureCoordinates = new Vector2(0.0f, 0.0f),
+            TextureSize = new Vector2(64.0f, 22.0f),
+            TopOffset = 6,
+            BottomOffset = 6,
+            LeftOffset = 16,
+            RightOffset = 1,
+            IsVisible = false,
+        };
+        selectedBackgroundNode.AttachNode(this);
+        
+        CollisionNode.AddEvent(AtkEventType.MouseOver, () => {
+            if (!IsSelected) {
+                IsHovered = true;
+            }
+        });
+        CollisionNode.AddEvent(AtkEventType.MouseDown, () => {
+            if (EnableSelection) {
+                OnClick?.Invoke();
+            }
+        });
+        CollisionNode.AddEvent(AtkEventType.MouseOut, () => {
+            IsHovered = false;
+        });
+    }
+
+    protected override void OnSizeChanged() {
+        base.OnSizeChanged();
+        
+        hoveredBackgroundNode.Size = Size + new Vector2(6.0f, 6.0f);
+        hoveredBackgroundNode.Position = new Vector2(-3.0f, -3.0f);
+        
+        selectedBackgroundNode.Size = Size + new Vector2(6.0f, 6.0f);
+        selectedBackgroundNode.Position = new Vector2(-3.0f, -3.0f);
+    }
+
+    public Action? OnClick {
+        get;
+        set {
+            field = value;
+            CollisionNode.ShowClickableCursor = value is not null;
+        }
+    }
+
+    public bool EnableSelection { get; set; } = true;
+    
+    public bool IsHovered {
+        get => hoveredBackgroundNode.IsVisible;
+        set => hoveredBackgroundNode.IsVisible = value;
+    }
+    
+    public bool IsSelected {
+        get => selectedBackgroundNode.IsVisible;
+        set {
+            selectedBackgroundNode.IsVisible = value;
+            hoveredBackgroundNode.IsVisible = !value;
+        }
+    }
+}
