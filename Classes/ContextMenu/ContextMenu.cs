9c63f8b6bcf51736749cf0cd7dc43f22e4aab3a8
Zeffuro <Jeffroiscool@gmail.com>
2026-01-02 21:48:12 +0100

    Add Submenu Item support

--- Classes/ContextMenu/ContextMenu.cs
+++ Classes/ContextMenu/ContextMenu.cs
@@ -7,10 +7,16 @@ using Lumina.Text.ReadOnly;
 
 namespace KamiToolKit.Classes.ContextMenu;
 
-public unsafe class ContextMenu : IDisposable {
+public unsafe class ContextMenu :  IDisposable {
     private readonly CustomEventInterface contextMenuEventInterface;
 
-    private Dictionary<int, ContextMenuItem>? generatedEntries;
+    private Dictionary<long, ContextMenuItem>? mainMenuEntries;
+    private Dictionary<long, ContextMenuSubItem>? mainMenuSubMenus;
+    private Dictionary<long, ContextMenuItem>? subMenuEntries;
+
+    // Prevent the return entry from colliding with submenu items
+    private const int SubMenuIndexOffset = 1000;
+
     private List<ContextMenuItem> Items { get; set; } = [];
     private IOrderedEnumerable<ContextMenuItem> OrderedItems => Items.OrderBy(item => item.DisplayPriority);
 
@@ -23,18 +29,40 @@ public unsafe class ContextMenu : IDisposable {
     }
 
     private AtkValue* ContextMenuEventHandler(AtkModuleInterface.AtkEventInterface* thisPtr, AtkValue* returnValue, AtkValue* values, uint valueCount, ulong eventKind) {
-        if (generatedEntries is null) return returnValue;
+        var handlerParam = (long)eventKind;
 
-        var indexClicked = values[1].Int;
-
-        if (generatedEntries?.TryGetValue(indexClicked, out var item) ?? false) {
-            item.OnClick();
+        if (handlerParam >= SubMenuIndexOffset) {
+            if (subMenuEntries?.TryGetValue(handlerParam, out var subItem) ?? false) {
+                subItem.OnClick();
+                ClearAll();
+            }
+            return returnValue;
         }
 
-        generatedEntries?.Clear();
-        generatedEntries = null;
+        if (mainMenuSubMenus?.TryGetValue(handlerParam, out var subMenuItem) ?? false) {
+            OpenSubMenu(subMenuItem);
+            return returnValue;
+        }
+
+        if (mainMenuEntries?.TryGetValue(handlerParam, out var item) ?? false) {
+            item.OnClick();
+            ClearAll();
+            return returnValue;
+        }
+
+        subMenuEntries?.Clear();
+        subMenuEntries = null;
 
         return returnValue;
+    }
+
+    private void ClearAll() {
+        mainMenuEntries?.Clear();
+        mainMenuEntries = null;
+        mainMenuSubMenus?.Clear();
+        mainMenuSubMenus = null;
+        subMenuEntries?.Clear();
+        subMenuEntries = null;
     }
 
     public void AddItem(ReadOnlySeString name, Action callback) {
@@ -50,7 +78,7 @@ public unsafe class ContextMenu : IDisposable {
 
         Items.Remove(targetItem);
     }
-    
+
     public void AddItem(ContextMenuItem item, params ContextMenuItem[] items) {
         foreach (var entry in items.Prepend(item)) {
             Items.Add(entry);
@@ -67,22 +95,54 @@ public unsafe class ContextMenu : IDisposable {
 
     public void Open() {
         var agentContextMenu = AgentContext.Instance();
-        
+
         agentContextMenu->ClearMenu();
 
-        var indexer = 0;
-        generatedEntries = [];
+        var indexer = 1;
+        mainMenuEntries = [];
+        mainMenuSubMenus = [];
+        subMenuEntries = null;
+
         foreach (var item in OrderedItems) {
-            generatedEntries.Add(indexer++, item);
-            agentContextMenu->AddMenuItem(item.Name, contextMenuEventInterface, indexer, !item.IsEnabled);
+            if (item is ContextMenuSubItem subItem) {
+                mainMenuSubMenus.Add(indexer, subItem);
+                agentContextMenu->AddMenuItem(item.Name, contextMenuEventInterface, indexer, !item.IsEnabled, submenu: true);
+            } else {
+                mainMenuEntries.Add(indexer, item);
+                agentContextMenu->AddMenuItem(item.Name, contextMenuEventInterface, indexer, !item.IsEnabled, submenu: false);
+            }
+            indexer++;
         }
 
         agentContextMenu->OpenContextMenu();
     }
 
+    private void OpenSubMenu(ContextMenuSubItem subItem) {
+        var agentContextMenu = AgentContext.Instance();
+
+        // Set the state again to prevent the menu closing when going back and forth between the submenus
+        agentContextMenu->SubContextMenu.SelectedContextItemIndex = 0;
+        agentContextMenu->SubContextMenu.CurrentEventIndex = 8;
+
+        agentContextMenu->OpenSubMenu();
+
+        var indexer = 0;
+        subMenuEntries = [];
+
+        foreach (var item in subItem.SubItems.OrderBy(i => i.DisplayPriority)) {
+            if (item is ContextMenuSubItem) continue;
+
+            var paramIndex = SubMenuIndexOffset + indexer;
+            subMenuEntries.Add(paramIndex, item);
+            agentContextMenu->AddMenuItem(item.Name, contextMenuEventInterface, paramIndex, !item.IsEnabled, submenu: false);
+            indexer++;
+        }
+    }
+
     public void Close() {
         var agentContextMenu = AgentContext.Instance();
-        
+
         agentContextMenu->ClearMenu();
+        ClearAll();
     }
 }
---
+++ Classes/ContextMenu/ContextMenuSubItem.cs
@@ -0,0 +1,24 @@
+using System;
+using System. Collections.Generic;
+using Lumina.Text. ReadOnly;
+
+namespace KamiToolKit.Classes.ContextMenu;
+
+/// <summary>
+/// One level of submenu only.  Nested submenus not supported.
+/// </summary>
+public class ContextMenuSubItem : ContextMenuItem
+{
+    public List<ContextMenuItem> SubItems { get; set; } = [];
+
+    public void AddItem(ReadOnlySeString name, Action callback)
+    {
+        SubItems.Add(new ContextMenuItem
+        {
+            Name = name,
+            OnClick = callback,
+        });
+    }
+
+    public void AddItem(ContextMenuItem item) => SubItems.Add(item);
+}
