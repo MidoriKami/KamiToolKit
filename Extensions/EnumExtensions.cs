56171a9e934577ef9de79a76589b194870acf3e2
MidoriKami <mi_kami@outlook.com>
2026-01-04 11:33:08 -0800

    Add MultiStateButtonNode

---
+++ Extensions/EnumExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel;
+using Dalamud.Utility;
+
+namespace KamiToolKit.Extensions;
+
+public static class EnumExtensions {
+    extension(Enum enumValue) {
+        public string Description => enumValue.GetDescription();
+
+        private string GetDescription() {
+            var attribute = enumValue.GetAttribute<DescriptionAttribute>();
+            return attribute?.Description ?? enumValue.ToString();
+        }
+    }
+}
