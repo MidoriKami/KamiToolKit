762c8bf4fbb4c2ce81596c9fe153ea76d8e3905f
MidoriKami <mi_kami@outlook.com>
2026-01-10 10:48:21 -0800

    Add search option enum (currently unused)

---
+++ Enums/SearchOption.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel;
+
+namespace KamiToolKit.Enums;
+
+public enum SearchOption {
+    [Description("Alphabetical")]
+    Alphabetical,
+    
+    [Description("ID")]
+    Id,
+}
