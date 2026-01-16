ec7ff25f4cff9be9e4a975d76f4979d0c90c3292
MidoriKami <mi_kami@outlook.com>
2026-01-16 09:51:28 -0800

    Add update suppression bool

--- Nodes/Layout/ListItemNode.cs
+++ Nodes/Layout/ListItemNode.cs
@@ -10,7 +10,9 @@ public abstract class ListItemNode<T> : SelectableNode {
         set {
             if (value is not null) {
                 if (!GenericUtil.AreEqual(field, value)) {
+                    IsSettingNodeData = true;
                     SetNodeData(value);
+                    IsSettingNodeData = false;
                 }
             }
 
@@ -20,6 +22,12 @@ public abstract class ListItemNode<T> : SelectableNode {
         }
     }
 
+    /// <summary>
+    /// Bool that indicates if SetNodeDate when different is being called.
+    /// Used to prevent things like checkboxes from trigger a file save due to the value being changed.
+    /// </summary>
+    protected bool IsSettingNodeData { get; private set; }
+    
     protected abstract void SetNodeData(T itemData);
 
     public virtual void Update() { }
