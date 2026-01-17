56969ad00cc340ee0e85877b8fd2843cfc886ccc
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2026-01-17 06:43:24 -0800

    Update ProgressNode.cs

--- Nodes/Component/ProgressNode.cs
+++ Nodes/Component/ProgressNode.cs
@@ -2,8 +2,7 @@ using System.Numerics;
 
 namespace KamiToolKit.Nodes;
 
-public abstract class ProgressNode : SimpleComponentNode
-{
+public abstract class ProgressNode : SimpleComponentNode {
     public abstract float Progress { get; set; }
     public abstract Vector4 BarColor { get; set; }
     public abstract Vector4 BackgroundColor { get; set; }
