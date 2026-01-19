cbb1b8ae336c7af6e660061734dfca9ba6900f03
MidoriKami <mi_kami@outlook.com>
2026-01-18 16:11:55 -0800

    Remove redundant timeline code

--- Nodes/Component/DropDownListButtonNode.cs
+++ Nodes/Component/DropDownListButtonNode.cs
@@ -1,27 +1,5 @@
-﻿using FFXIVClientStructs.FFXIV.Component.GUI;
-using KamiToolKit.Timelines;
-
-namespace KamiToolKit.Nodes;
+﻿namespace KamiToolKit.Nodes;
 
 public class DropDownListButtonNode : DropDownListNode<string> {
-
-    public DropDownListButtonNode() {
-        BuildTimelines();
-    }
-
     protected override string GetLabelForOption(string option) => option;
-
-    private void BuildTimelines() {
-        AddTimeline(new TimelineBuilder()
-            .BeginFrameSet(1, 29)
-            .AddLabel(1, 17, AtkTimelineJumpBehavior.Start, 0)
-            .AddLabel(9, 0, AtkTimelineJumpBehavior.PlayOnce, 0)
-            .AddLabel(10, 18, AtkTimelineJumpBehavior.Start, 0)
-            .AddLabel(19, 0, AtkTimelineJumpBehavior.PlayOnce, 0)
-            .AddLabel(20, 7, AtkTimelineJumpBehavior.Start, 0)
-            .AddLabel(29, 0, AtkTimelineJumpBehavior.PlayOnce, 0)
-            .EndFrameSet()
-            .Build()
-        );
-    }
 }
