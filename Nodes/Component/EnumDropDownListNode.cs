b54ebec3aae633876d0f6256f72b54ef1a6c9425
MidoriKami <mi_kami@outlook.com>
2026-01-18 16:12:01 -0800

    Add enum dropdown

---
+++ Nodes/Component/EnumDropDownListNode.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace KamiToolKit.Nodes;
+
+public class EnumDropDownListNode<T> : DropDownListNode<T> where T : Enum {
+
+    protected override string GetLabelForOption(T option)
+        => option.Description;
+}
---
+++ Nodes/Component/EnumDropDownNode.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace KamiToolKit.Nodes;
+
+public class EnumDropDownNode<T> : DropDownNode<EnumDropDownListNode<T>, T> where T : Enum{
+
+    public EnumDropDownNode() {
+        OptionListNode.OnOptionSelected += OptionSelectedHandler;
+    }
+    
+    public Action<T>? OnOptionSelected { get; set; }
+
+    public required List<T>? Options {
+        get => OptionListNode.Options;
+        set {
+            OptionListNode.Options = value;
+            OptionListNode.SelectDefaultOption();
+            UpdateLabel(OptionListNode.SelectedOption);
+        }
+    }
+
+    private void OptionSelectedHandler(T option) {
+        OnOptionSelected?.Invoke(option);
+        UpdateLabel(option);
+        Toggle(false);
+    }
+    
+    protected override void UpdateLabel(T? option) {
+        LabelNode.String = option?.Description;
+    }
+}
