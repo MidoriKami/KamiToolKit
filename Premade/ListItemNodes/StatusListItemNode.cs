2f74c2cac3d17484bf014f490e0e05320e38c90b
MidoriKami <mi_kami@outlook.com>
2026-01-24 16:40:36 -0800

    Add status search addon

---
+++ Premade/ListItemNodes/StatusListItemNode.cs
@@ -0,0 +1,47 @@
+﻿using System.Numerics;
+using FFXIVClientStructs.FFXIV.Component.GUI;
+using KamiToolKit.Classes;
+using KamiToolKit.Nodes;
+using Lumina.Excel.Sheets;
+
+namespace KamiToolKit.Premade.ListItemNodes;
+
+public class StatusListItemNode : ListItemNode<Status> {
+    public override float ItemHeight => 48.0f;
+
+    protected readonly IconImageNode IconImageNode;
+    protected readonly TextNode StatusLabelNode;
+    
+    public StatusListItemNode() {
+        IconImageNode = new IconImageNode {
+            FitTexture = true,
+            IconId = 60072,
+        };
+        IconImageNode.AttachNode(this);
+
+        StatusLabelNode = new TextNode {
+            TextFlags = TextFlags.Ellipsis | TextFlags.Emboss,
+            FontSize = 14,
+            LineSpacing = 14,
+            AlignmentType = AlignmentType.Left,
+            TextColor = ColorHelper.GetColor(8),
+            TextOutlineColor = ColorHelper.GetColor(7),
+        };
+        StatusLabelNode.AttachNode(this);
+    }
+
+    protected override void OnSizeChanged() {
+        base.OnSizeChanged();
+        
+        IconImageNode.Size = new Vector2((Height - 4.0f) * 0.75f , Height - 4.0f);
+        IconImageNode.Position = new Vector2(2.0f, 2.0f);
+
+        StatusLabelNode.Size = new Vector2(Width - IconImageNode.Width - 6.0f, Height);
+        StatusLabelNode.Position = new Vector2(IconImageNode.Bounds.Right + 6.0f, 0.0f);
+    }
+
+    protected override void SetNodeData(Status itemData) {
+        IconImageNode.IconId = itemData.Icon;
+        StatusLabelNode.String = itemData.Name;
+    }
+}
---
+++ Premade/SearchAddons/StatusSearchAddon.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using System.Text.RegularExpressions;
+using KamiToolKit.Classes;
+using KamiToolKit.Premade.ListItemNodes;
+using Lumina.Excel.Sheets;
+
+namespace KamiToolKit.Premade.SearchAddons;
+
+public class StatusSearchAddon : BaseSearchAddon<Status, StatusListItemNode> {
+    public StatusSearchAddon() {
+        SearchOptions = DalamudInterface.Instance.DataManager.GetExcelSheet<Status>()
+            .Where(territory => territory.RowId is not 0)
+            .Where(territory => !territory.Name.IsEmpty)
+            .ToList();
+    }
+    
+    protected override int Comparer(Status left, Status right, string sortingString, bool reversed){
+        var result = sortingString switch {
+            "Alphabetical" => string.CompareOrdinal(left.Name.ToString(), right.Name.ToString()),
+            "Id" => left.RowId.CompareTo(right.RowId), 
+            _ => 0,
+        };
+        
+        return reversed ? -result : result;
+    }
+
+    protected override bool IsMatch(Status item, string searchString) {
+        var regex = new Regex(searchString,RegexOptions.CultureInvariant | RegexOptions.IgnoreCase);
+
+        if (regex.IsMatch(item.RowId.ToString())) return true;
+        if (regex.IsMatch(item.Name.ToString())) return true;
+
+        return false;
+    }
+}
