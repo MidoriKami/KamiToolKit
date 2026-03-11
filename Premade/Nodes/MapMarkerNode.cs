54242aeafdcfa5e9c2a029aa48c994cbe40f7624
MidoriKami <mi_kami@outlook.com>
2026-03-10 19:59:08 -0700

    Automatically adjust node sizes and positions according to the map

--- Premade/Nodes/MapMarkerNode.cs
+++ Premade/Nodes/MapMarkerNode.cs
@@ -1,10 +1,10 @@
-﻿using Dalamud.Interface.Textures.TextureWraps;
+﻿using System.Numerics;
+using Dalamud.Interface.Textures.TextureWraps;
 using FFXIVClientStructs.FFXIV.Client.UI.Agent;
 using KamiToolKit.Classes;
 using KamiToolKit.Nodes;
 using Lumina.Excel.Sheets;
 using Lumina.Text.ReadOnly;
-using Vector2 = System.Numerics.Vector2;
 
 namespace KamiToolKit.Premade.Nodes;
 
@@ -14,17 +14,9 @@ public unsafe class MapMarkerNode : SimpleOverlayNode {
     
     public override bool IsVisible { get; set; } = true;
 
-    public override Vector2 Position {
-        get => base.Position;
-        set {
-            if (DalamudInterface.Instance.DataManager.GetExcelSheet<Map>().TryGetRow(MapId, out var mapRow)) {
-                base.Position = value * (mapRow.SizeFactor / 100.0f) + (new Vector2(mapRow.OffsetX, mapRow.OffsetY) + new Vector2(1024.0f, 1024.0f)) - Size * Scale / 2.0f;
-            }
-            else {
-                base.Position = value;
-            }
-        }
-    }
+    public new Vector2 Size { get; set; }
+
+    public new Vector2 Position { get; set; }
 
     public new ReadOnlySeString TextTooltip { get; set; } = string.Empty;
 
@@ -35,6 +27,11 @@ public unsafe class MapMarkerNode : SimpleOverlayNode {
         set {
             if (value is null) return;
             field = value;
+
+            if (iconNode is not null) {
+                iconNode.IconId = value.Value;
+                return;
+            }
             
             imGuiImageNode?.Dispose();
             imGuiImageNode = null;
@@ -88,16 +85,27 @@ public unsafe class MapMarkerNode : SimpleOverlayNode {
         }
     } = null;
 
-    protected override void OnSizeChanged() {
-        base.OnSizeChanged();
-
-        iconNode?.Size = Size;
-        imGuiImageNode?.Size = Size;
-    }
-
     public void Update() {
         OnUpdate();
 
+        if (!DalamudInterface.Instance.DataManager.GetExcelSheet<Map>().TryGetRow(MapId, out var mapRow)) {
+            IsVisible = false;
+            return;
+        }
+
+        var mapScale = mapRow.SizeFactor / 100.0f;
+        var mapOffset = new Vector2(mapRow.OffsetX, mapRow.OffsetY) + new Vector2(1024.0f, 1024.0f);
+
+        base.Size = Size * mapScale;
+        base.Origin = Size / 2.0f;
+
+        iconNode?.Size = base.Size;
+        iconNode?.Origin = base.Size / 2.0f;
+
+        imGuiImageNode?.Size = base.Size;
+        imGuiImageNode?.Origin = base.Size / 2.0f;
+        
+        base.Position = Position * mapScale + mapOffset - base.Size / 2.0f;
         base.IsVisible = IsVisible && AgentMap.Instance()->SelectedMapId == MapId;
     }
 
