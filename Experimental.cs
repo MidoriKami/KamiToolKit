c95402b79590239b51fe29c53abdd59bb1df6d5a
MidoriKami <mi_kami@outlook.com>
2026-05-01 15:04:26 -0700

    Remove experimental temp code

--- Experimental.cs
+++ Experimental.cs
@@ -1,15 +1,7 @@
 ﻿// ReSharper disable RedundantUnsafeContext
 
-using Dalamud.Utility.Signatures;
-using FFXIVClientStructs.FFXIV.Client.UI;
-using FFXIVClientStructs.FFXIV.Component.GUI;
-using InteropGenerator.Runtime;
-
 namespace KamiToolKit;
 
 internal unsafe class Experimental {
-    public delegate bool InitializeAddonDelegate(RaptureAtkUnitManager* atkUnitManager, AtkUnitBase** atkUnitBase, CStringPointer addonName, uint valueCount, AtkValue* atkValues);
-
-    [Signature("E8 ?? ?? ?? ?? 48 8B 45 9F 48 8D 7D 17")]
-    public static InitializeAddonDelegate? InitializeAddonFunction = null!;
+    // Nothing Experimental at the moment.
 }
