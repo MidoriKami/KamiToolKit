3a3509d95a1896dbe05828d6e7f60596d8ad6d4b
MidoriKami <mi_kami@outlook.com>
2026-05-06 16:57:13 -0700

    Add AddonFactoryController.cs

---
+++ Controllers/AddonFactoryController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using FFXIVClientStructs.FFXIV.Client.UI;
+using FFXIVClientStructs.FFXIV.Component.GUI;
+using InteropGenerator.Runtime;
+using KamiToolKit.Dalamud;
+
+namespace KamiToolKit.Controllers;
+
+/// <summary>
+/// Controller intended to interact with the games native Addon Factory system
+/// to fully replace a built-in game addon with a custom <see cref="NativeAddon"/>.
+/// </summary>
+public unsafe class AddonFactoryController : IDisposable {
+    public required string AddonName { get; init; }
+
+    private nint? originalFactoryCreateAddress;
+    private RaptureAtkModule.AddonFactoryInfo.CreateDelegate? pinnedFactoryCreateMethod;
+    private NativeAddon? nativeAddon;
+
+    /// <summary>
+    /// Function to allocate the <see cref="NativeAddon"/> that will replace the named addon.
+    /// </summary>
+    /// <remarks>
+    /// KamiToolKit will take ownership of the created addon.
+    /// </remarks>
+    public required Func<NativeAddon> CreateNativeAddonFunction { get; init; }
+
+    /// <summary>
+    /// Enables the addon factory replacement.
+    /// </summary>
+    public void Enable() => Services.Framework.RunOnFrameworkThread(() => {
+        var factoryInfo = RaptureAtkModule.Instance()->GetAddonFactoryInfo(AddonName);
+        if (factoryInfo is null) return;
+
+        originalFactoryCreateAddress = (nint?)factoryInfo->Create;
+        pinnedFactoryCreateMethod = CreateCustomAddon;
+        factoryInfo->Create = (delegate* unmanaged<RaptureAtkModule*, CStringPointer, uint, AtkValue*, AtkUnitBase*>) Marshal.GetFunctionPointerForDelegate(pinnedFactoryCreateMethod);
+    });
+
+    /// <summary>
+    /// Disables addon factory replacement and disposes any open replaced addons.
+    /// </summary>
+    public void Disable() {
+        nativeAddon?.Dispose();
+        nativeAddon = null;
+
+        Services.Framework.RunOnFrameworkThread(() => {
+            var factoryInfo = RaptureAtkModule.Instance()->GetAddonFactoryInfo(AddonName);
+            if (factoryInfo is null) return;
+
+            // This is dumb, but the compiler will warn otherwise.
+            if (originalFactoryCreateAddress is null) {
+                factoryInfo->Create = null;
+            }
+            else {
+                factoryInfo->Create = (delegate* unmanaged<RaptureAtkModule*, CStringPointer, uint, AtkValue*, AtkUnitBase*>) originalFactoryCreateAddress;
+            }
+
+            pinnedFactoryCreateMethod = null;
+            originalFactoryCreateAddress = null;
+        });
+    }
+
+    private AtkUnitBase* CreateCustomAddon(RaptureAtkModule* raptureAtkModule, CStringPointer addonName, uint valueCount, AtkValue* values) {
+        try {
+            nativeAddon?.Dispose();
+            nativeAddon = CreateNativeAddonFunction();
+            nativeAddon.InitializeForAddonFactory(valueCount, values);
+
+            return nativeAddon;
+        }
+        catch (Exception e) {
+            Services.Log.Exception(e);
+        }
+
+        return null;
+    }
+
+    public void Dispose()
+        => Disable();
+}
---
+++ Extensions/RaptureAtkModuleExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using Dalamud.Utility;
+using FFXIVClientStructs.FFXIV.Client.UI;
+
+namespace KamiToolKit.Extensions;
+
+public static unsafe class RaptureAtkModuleExtensions {
+    extension(ref RaptureAtkModule raptureAtkModule) {
+        public RaptureAtkModule.AddonFactoryInfo* GetAddonFactoryInfo(string addonName) {
+            var addonIndex = raptureAtkModule.AddonNames.FindIndex(name => string.Equals(name.ExtractText(), addonName, StringComparison.OrdinalIgnoreCase));
+            if (addonIndex is -1) return null;
+
+            var addonFactories = raptureAtkModule.AddonFactories;
+            if (addonFactories.Length < addonIndex) return null;
+
+            return (RaptureAtkModule.AddonFactoryInfo*) Unsafe.AsPointer(ref addonFactories[addonIndex]);
+        }
+    }
+}
