00c2033007680bfc9f1a6e62ac49809b20a81548
MidoriKami <mi_kami@outlook.com>
2026-06-03 22:57:27 -0700

    Reformat and document EventListeners and EventInterfaces

--- Classes/CustomEventInterface.cs
+++ Classes/CustomEventInterface.cs
@@ -1,31 +1,36 @@
 using System;
 using System.Runtime.InteropServices;
 using FFXIVClientStructs.FFXIV.Component.GUI;
+using KamiToolKit.Dalamud;
 using static FFXIVClientStructs.FFXIV.Component.GUI.AtkModuleInterface;
 
 namespace KamiToolKit.Classes;
 
+/// <summary>
+/// Managed wrapper around a AtkEventInterface.
+/// This class is intended to be used to wire native events to managed event handlers.
+/// </summary>
+/// <remarks>
+/// This version specifically to be used for ContextMenus, as those use AtkEventInterface to trigger their callbacks.
+/// </remarks>
 public unsafe class CustomEventInterface : IDisposable {
 
-    private readonly AtkEventInterface* eventInterface;
-
-    private AtkEventInterface.Delegates.ReceiveEvent? receiveEventDelegate;
-    private AtkEventInterface.Delegates.ReceiveEventWithResult? receiveEventWithResultDelegate;
+    /// <summary>
+    /// Public implicit operator to be able to use this instance as a AtkEventInterface* directly.
+    /// </summary>
+    public static implicit operator AtkEventInterface*(CustomEventInterface listener) => listener.eventInterface;
 
     public CustomEventInterface(AtkEventInterface.Delegates.ReceiveEvent eventHandler, AtkEventInterface.Delegates.ReceiveEventWithResult? receiveEventWithResult = null) {
         receiveEventDelegate = eventHandler;
         receiveEventWithResultDelegate = receiveEventWithResult;
 
+        receiveWrapper = ReceiveEventWrapper;
+        receiveWithResultWrapper = ReceiveWithResultWrapper;
+
         eventInterface = NativeMemoryHelper.UiAlloc<AtkEventInterface>();
         eventInterface->VirtualTable = (AtkEventInterface.AtkEventInterfaceVirtualTable*)NativeMemoryHelper.Malloc((ulong)sizeof(void*) * 2);
-        eventInterface->VirtualTable->ReceiveEvent = (delegate* unmanaged<AtkEventInterface*, AtkValue*, AtkValue*, uint, ulong, AtkValue*>)Marshal.GetFunctionPointerForDelegate(receiveEventDelegate);
-
-        if (receiveEventWithResultDelegate is not null) {
-            eventInterface->VirtualTable->ReceiveEventWithResult = (delegate* unmanaged<AtkEventInterface*, AtkValue*, AtkValue*, uint, ulong, AtkValue*>)Marshal.GetFunctionPointerForDelegate(receiveEventWithResultDelegate);
-        }
-        else {
-            eventInterface->VirtualTable->ReceiveEventWithResult = (delegate* unmanaged<AtkEventInterface*, AtkValue*, AtkValue*, uint, ulong, AtkValue*>)(delegate* unmanaged<void>)&NullSub;
-        }
+        eventInterface->VirtualTable->ReceiveEvent = (delegate* unmanaged<AtkEventInterface*, AtkValue*, AtkValue*, uint, ulong, AtkValue*>)Marshal.GetFunctionPointerForDelegate(receiveWrapper);
+        eventInterface->VirtualTable->ReceiveEventWithResult = (delegate* unmanaged<AtkEventInterface*, AtkValue*, AtkValue*, uint, ulong, AtkValue*>)Marshal.GetFunctionPointerForDelegate(receiveWithResultWrapper);
     }
 
     public void Dispose() {
@@ -36,9 +41,44 @@ public unsafe class CustomEventInterface : IDisposable {
 
         receiveEventDelegate = null;
         receiveEventWithResultDelegate = null;
+
+        receiveWrapper = null;
+        receiveWithResultWrapper = null;
     }
+
+    private readonly AtkEventInterface* eventInterface;
+
+    private AtkEventInterface.Delegates.ReceiveEvent? receiveEventDelegate;
+    private AtkEventInterface.Delegates.ReceiveEventWithResult? receiveEventWithResultDelegate;
+
+    private AtkEventInterface.Delegates.ReceiveEvent? receiveWrapper;
+    private AtkEventInterface.Delegates.ReceiveEventWithResult? receiveWithResultWrapper;
 
     [UnmanagedCallersOnly] private static void NullSub() { }
 
-    public static implicit operator AtkEventInterface*(CustomEventInterface listener) => listener.eventInterface;
+    private AtkValue* ReceiveEventWrapper(AtkEventInterface* thisPtr, AtkValue* returnValue, AtkValue* values, uint valueCount, ulong eventKind) {
+        try {
+            if (receiveEventDelegate is not null) {
+                return receiveEventDelegate.Invoke(thisPtr, returnValue, values, valueCount, eventKind);
+            }
+        }
+        catch (Exception e) {
+            Services.Log.Exception(e);
+        }
+
+        return returnValue;
+    }
+
+    private AtkValue* ReceiveWithResultWrapper(AtkEventInterface* thisPtr, AtkValue* returnValue, AtkValue* values, uint valueCount, ulong eventKind) {
+        try {
+            if (receiveEventWithResultDelegate is not null) {
+                return receiveEventWithResultDelegate.Invoke(thisPtr, returnValue, values, valueCount, eventKind);
+            }
+        }
+        catch (Exception e) {
+            Services.Log.Exception(e);
+        }
+
+        return returnValue;
+    }
 }
--- Classes/ViewportEventListener.cs
+++ Classes/ViewportEventListener.cs
@@ -4,7 +4,20 @@ using KamiToolKit.Dalamud;
 
 namespace KamiToolKit.Classes;
 
+/// <summary>
+/// Event listener that wires events to the global viewport.
+/// <em>Warning, these events may be triggered every frame multiple times per frame, and are not intended to be long-lived.</em>
+/// </summary>
 public unsafe class ViewportEventListener(AtkEventListener.Delegates.ReceiveEvent eventHandler) : CustomEventListener(eventHandler) {
+
+    /// <summary>
+    /// Registers a viewport event for the specified node.
+    /// </summary>
+    /// <remarks>
+    /// This can only be called from the games main thread.
+    /// </remarks>
+    /// <param name="eventType">Event Type to listen for.</param>
+    /// <param name="node">Node to pass when the callback is triggered.</param>
     public void AddEvent(AtkEventType eventType, AtkResNode* node) {
         ThreadSafety.AssertMainThread();
 
@@ -12,6 +25,13 @@ public unsafe class ViewportEventListener(AtkEventListener.Delegates.ReceiveEven
         AtkStage.Instance()->ViewportEventManager.RegisterEvent(eventType, 0, node, &node->AtkEventTarget, this, false);
     }
 
+    /// <summary>
+    /// Removes a viewport event by type.
+    /// </summary>
+    /// <remarks>
+    /// This can only be called from the games main thread.
+    /// </remarks>
+    /// <param name="eventType">Event Type to no longer listen for.</param>
     public void RemoveEvent(AtkEventType eventType) {
         ThreadSafety.AssertMainThread();
 
@@ -19,6 +39,12 @@ public unsafe class ViewportEventListener(AtkEventListener.Delegates.ReceiveEven
         AtkStage.Instance()->ViewportEventManager.UnregisterEvent(eventType, 0, this, false);
     }
 
+    /// <summary>
+    /// Unregisters all events and disposes this instance.
+    /// </summary>
+    /// <remarks>
+    /// This can only be called from the games main thread.
+    /// </remarks>
     public override void Dispose() {
         ThreadSafety.AssertMainThread();
 
