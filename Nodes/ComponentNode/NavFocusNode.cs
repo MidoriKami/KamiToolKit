5afe0ddf1e379e289f3935219c43212a3203c6db
MidoriKami <mi_kami@outlook.com>
2026-06-06 16:00:56 -0700

    Add nav focus node

---
+++ Nodes/ComponentNode/NavFocusNode.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using FFXIVClientStructs.FFXIV.Client.System.Input;
+using FFXIVClientStructs.FFXIV.Component.GUI;
+using KamiToolKit.Nodes.Simplified;
+
+namespace KamiToolKit.Nodes;
+
+/// <summary>
+/// A basic component node with callbacks for navigation events.
+/// </summary>
+public unsafe class NavFocusNode : SimpleComponentNode {
+
+    /// <summary>
+    /// Action that is invoked when this node is selected via Return.
+    /// </summary>
+    public Action? OnSelected { get; set; }
+
+    public NavFocusNode()
+        => AddEvent(AtkEventType.InputReceived, Callback);
+
+    private void Callback(AtkEventListener* thisPtr, AtkEventType eventType, int eventParam, AtkEvent* atkEvent, AtkEventData* atkEventData) {
+        if (eventType is not AtkEventType.InputReceived) return;
+
+        var inputState = atkEventData->InputData.State;
+
+        switch ((InputId)atkEventData->InputData.InputId) {
+            case InputId.OK when inputState is InputState.Down:
+                OnSelected?.Invoke();
+                break;
+        }
+    }
+}
