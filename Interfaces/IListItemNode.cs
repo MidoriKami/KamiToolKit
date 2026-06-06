628d01cecb66f922abe210f76d9ddba5c13aba0d
MidoriKami <mi_kami@outlook.com>
2026-06-06 15:02:31 -0700

    Add docs

--- Interfaces/IListItemNode.cs
+++ Interfaces/IListItemNode.cs
@@ -1,5 +1,15 @@
-﻿namespace KamiToolKit.Interfaces;
+﻿using KamiToolKit.Nodes;
 
+namespace KamiToolKit.Interfaces;
+
+/// <summary>
+/// Interface for <see cref="ListNode{T,TU}"/>
+/// </summary>
 public interface IListItemNode {
+
+    /// <summary>
+    /// Gets the height of this list item's entry.
+    /// This is a statically per-type defined value.
+    /// </summary>
     abstract static float ItemHeight { get; }
 }
