3bbf3c182e3e8b1419d81ab7e263ca6c1360e306
Zeffuro <Jeffroiscool@gmail.com>
2026-07-30 22:48:02 +0200

    Remove passthrough methods

--- Nodes/ComponentNode/ScrollingNode.cs
+++ Nodes/ComponentNode/ScrollingNode.cs
@@ -81,18 +81,6 @@ public class ScrollingNode<T> : ResNode where T : NodeBase, new() {
     }
 
     /// <summary>
-    /// Sets the scroll position to the top.
-    /// </summary>
-    public void ScrollToTop()
-        => ScrollToStart();
-
-    /// <summary>
-    /// Sets the scroll position to the bottom.
-    /// </summary>
-    public void ScrollToBottom()
-        => ScrollToEnd();
-
-    /// <summary>
     /// Recalculates sizes to update scroll params correctly.
     /// </summary>
     public void RecalculateSizes() {
