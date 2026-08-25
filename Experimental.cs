2c6e52d54cec6e34c2e3d7775e67989f9c4a0a3f
MidoriKami <mi_kami@outlook.com>
2026-08-24 20:21:59 -0700

    Stubb out HotbarNode prototype

--- Experimental.cs
+++ Experimental.cs
@@ -1,7 +1,50 @@
 ﻿// ReSharper disable RedundantUnsafeContext
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+
+using System.Runtime.InteropServices;
+using Dalamud.Utility.Signatures;
+using FFXIVClientStructs.FFXIV.Client.System.String;
+using FFXIVClientStructs.FFXIV.Client.UI.Misc;
+using InteropGenerator.Runtime;
 
 namespace KamiToolKit;
 
-internal unsafe class Experimental {
-    // Nothing Experimental at the moment.
+/// <summary>
+/// Warning, anything in this class is subject to change at any time.
+/// This is mostly a staging platform for features that haven't made it into live ClientStructs.
+/// These are not intended for external use, other than for experimenting.
+/// </summary>
+public unsafe class Experimental {
+
+    public unsafe delegate void PrepareSlotForRender(
+        RaptureHotbarModule* thisPtr,
+        RaptureHotbarModule.HotbarSlot* slot,
+        HotbarUiIntermediate* outIntermediate);
+
+    [Signature("E8 ?? ?? ?? ?? FF C6 83 C5 11")]
+    public static PrepareSlotForRender? UpdateHotbarSlotIntermediateData = null;
+
+    [StructLayout(LayoutKind.Explicit, Size = 0x43)]
+    public unsafe struct HotbarUiIntermediate {
+        [FieldOffset(0x00)] public Utf8String* PopUpHelpText;   // to StringArray idx slotBase + 14
+        [FieldOffset(0x08)] public CStringPointer CostTextPtr;  // to StringArray idx slotBase + 1
+        [FieldOffset(0x10)] public uint IntermediateActionType; // to NumberArray idx slotBase + 0
+        [FieldOffset(0x14)] public uint ActionId;               // to NumberArray idx slotBase + 3
+        [FieldOffset(0x18)] public uint IconId;                 // to NumberArray idx slotBase + 4
+        [FieldOffset(0x1C)] public uint CooldownMode;           // to NumberArray idx slotBase + 7
+        [FieldOffset(0x20)] public uint CooldownSeconds;
+        [FieldOffset(0x24)] public uint CooldownPercent; // to NumberArray idx slotBase + 8
+        [FieldOffset(0x28)] public uint LastCooldownPercent;
+        [FieldOffset(0x2C)] public uint ChargePercent; // to NumberArray idx slotBase + 9
+        [FieldOffset(0x30)] public uint LastChargePercent;
+        [FieldOffset(0x34)] public uint CurrentCharges;        // to NumberArray idx slotBase + 13
+        [FieldOffset(0x38)] public uint CostValue;             // to NumberArray idx slotBase + 10
+        [FieldOffset(0x3C)] public byte CostType;              // to NumberArray idx slotBase + 1
+        [FieldOffset(0x3D)] public byte CostDisplayMode;       // to NumberArray idx slotBase + 2
+        [FieldOffset(0x3E)] public bool ActionAvailable1;      // to NumberArray idx slotBase + 5
+        [FieldOffset(0x3F)] public bool ActionAvailable2;      // to NumberArray idx slotBase + 6
+        [FieldOffset(0x40)] public bool ActionTargetSatisfied; // to NumberArray idx slotBase + 15
+        [FieldOffset(0x41)] public bool DrawAnts;              // to NumberArray idx slotBase + 14
+        [FieldOffset(0x42)] public byte Unk0x42;
+    }
 }
--- Nodes/ComponentNode/HotbarNode.cs
+++ Nodes/ComponentNode/HotbarNode.cs
@@ -1,10 +1,52 @@
 ﻿
+using FFXIVClientStructs.FFXIV.Client.UI;
+using FFXIVClientStructs.FFXIV.Client.UI.Misc;
+using FFXIVClientStructs.FFXIV.Component.GUI;
+using KamiToolKit.Classes;
+
 namespace KamiToolKit.Nodes;
 
 /// <summary>
 /// Specialization of <see cref="DragDropNode"/> that has handy accessors for things used to represent a hotbar slot.
 /// </summary>
 public class HotbarNode : DragDropNode {
+
+    /// <summary>
+    /// Updates the hotbar slots current state, cost, icon, and various other fields.
+    /// </summary>
+    public unsafe void Update() {
+        var hotbarModule = RaptureHotbarModule.Instance();
+        if (hotbarModule is null) return;
+
+        fixed (RaptureHotbarModule.HotbarSlot* data = &hotbarData)
+        fixed (Experimental.HotbarUiIntermediate* state = &HotbarState)
+        {
+            RaptureHotbarModule.HotbarSlotType outType;
+            uint outActionId;
+            ushort unkC4;
+
+            RaptureHotbarModule.GetSlotAppearance(&outType, &outActionId, &unkC4, hotbarModule, data);
+            hotbarData.ApparentActionId = outActionId;
+            hotbarData.ApparentSlotType = outType;
+
+            Experimental.UpdateHotbarSlotIntermediateData?.Invoke(RaptureHotbarModule.Instance(), data, state);
+
+            IconId = HotbarState.IconId;
+
+            ShowResourceCost = HotbarState.CostType is 2;
+            ResourceCost = HotbarState.CostValue;
+
+            ShowChargeCount = HotbarState.CostType is 0;
+            ChargeCount = HotbarState.CurrentCharges;
+            ChargePercent = HotbarState.ChargePercent / 100.0f;
+
+            ShowCooldownSeconds = HotbarState.CooldownSeconds is not 0;
+            CooldownSeconds = HotbarState.CooldownSeconds;
+
+            ShowCooldownPercent = HotbarState.CooldownPercent is not 0;
+            CooldownPercent = HotbarState.CooldownPercent / 100.0f;
+        }
+    }
 
     /// <summary>
     /// Gets or sets the primary resource cost text.
@@ -103,4 +145,67 @@ public class HotbarNode : DragDropNode {
             IconNode.IconExtras.CooldownNode.CooldownImage.IsVisible = value;
         }
     }
+
+    /// <summary>
+    /// Sets this hotbar slot to the specified action.
+    /// </summary>
+    /// <param name="actionId"></param>
+    public void SetAction(uint actionId) {
+        Payload.Type = DragDropType.Action;
+        Payload.Int2 = (int) actionId;
+
+        hotbarData.Set(UIGlobals.GetHotbarSlotTypeFromDragDropType(Payload.Type), (uint) Payload.Int2);
+    }
+
+    /// <inheritdoc />
+    public HotbarNode() {
+        OnRollOver = OnHotbarNodeRollOver;
+        OnRollOut = OnHotbarNodeRollOut;
+        OnPayloadAccepted = OnHotbarNodePayloadAccepted;
+        OnClicked = OnHotbarNodeClicked;
+        OnDiscard = OnHotbarNodeDiscard;
+    }
+
+    // todo: figure out how to make this behave better
+    private void OnHotbarNodeRollOver(DragDropNode thisNode) {
+        switch (Payload.Type) {
+            case DragDropType.Action:
+                HideTooltip();
+                ActionTooltip = (uint) Payload.Int2;
+                ShowTooltip();
+                break;
+        }
+    }
+
+    private void OnHotbarNodeRollOut(DragDropNode thisNode) {
+        HideTooltip();
+    }
+
+    private void OnHotbarNodePayloadAccepted(DragDropNode thisNode, DragDropPayload payload) {
+        Payload.Type = payload.Type;
+        Payload.Int2 = payload.Int2;
+
+        hotbarData.Set(UIGlobals.GetHotbarSlotTypeFromDragDropType(payload.Type), (uint) payload.Int2);
+    }
+
+    private unsafe void OnHotbarNodeClicked(DragDropNode thisNode) {
+        var hotbarModule = RaptureHotbarModule.Instance();
+        if (hotbarModule is null) return;
+
+        fixed (RaptureHotbarModule.HotbarSlot* hotbarSlotData = &hotbarData) {
+            hotbarModule->ExecuteSlot(hotbarSlotData);
+        }
+    }
+
+    private void OnHotbarNodeDiscard(DragDropNode thisNode) {
+        // todo: this
+    }
+
+    private RaptureHotbarModule.HotbarSlot hotbarData;
+
+    // todo: only available temporarily for debugging.
+    /// <summary>
+    /// Gets the current hotbars state values. Not intended for external use.
+    /// </summary>
+    public readonly Experimental.HotbarUiIntermediate HotbarState;
 }
