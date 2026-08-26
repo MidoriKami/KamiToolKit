c017407cf6e45e983313cfa38ad382f13373ca8f
MidoriKami <mi_kami@outlook.com>
2026-08-26 07:51:47 -0700

    Extract TextNode into ITextNode

---
+++ Interfaces/Nodes/ITextNode.cs
@@ -0,0 +1,118 @@
+﻿using System.Numerics;
+using FFXIVClientStructs.FFXIV.Component.GUI;
+using Lumina.Data.Parsing.Uld;
+using Lumina.Text.ReadOnly;
+
+namespace KamiToolKit.Interfaces;
+
+/// <summary>
+/// Interfacing representing the capabilities of a TextNode.
+/// </summary>
+public interface ITextNode {
+    /// <summary>
+    /// Gets or sets the text color.
+    /// </summary>
+    /// <remarks>
+    /// Expects values between 0.0f and 1.0f.
+    /// </remarks>
+    Vector4 TextColor { get; set; }
+
+    /// <summary>
+    /// Gets or sets the outline color.
+    /// </summary>
+    /// <remarks>
+    /// Expects values between 0.0f and 1.0f.
+    /// </remarks>
+    Vector4 TextOutlineColor { get; set; }
+
+    /// <summary>
+    /// Gets or sets the background color.
+    /// </summary>
+    Vector4 BackgroundColor { get; set; }
+
+    /// <summary>
+    /// Gets or sets the selection start.
+    /// </summary>
+    /// <remarks>
+    /// This is used in conjunction with <see cref="BackgroundColor"/> and <see cref="SelectEnd"/>.
+    /// </remarks>
+    uint SelectStart { get; set; }
+
+    /// <summary>
+    /// Gets or sets the selection end.
+    /// </summary>
+    /// <remarks>
+    /// This is used in conjunction with <see cref="BackgroundColor"/> and <see cref="SelectStart"/>.
+    /// </remarks>
+    uint SelectEnd { get; set; }
+
+    /// <summary>
+    /// Gets or sets the text alignment.
+    /// </summary>
+    AlignmentType AlignmentType { get; set; }
+
+    /// <summary>
+    /// Gets or sets the used font.
+    /// </summary>
+    FontType FontType { get; set; }
+
+    /// <summary>
+    /// Gets or sets the text flags.
+    /// </summary>
+    TextFlags TextFlags { get; set; }
+
+    /// <summary>
+    /// Gets or sets the font size.
+    /// </summary>
+    uint FontSize { get; set; }
+
+    /// <summary>
+    /// Gets or sets the vertical line spacing.
+    /// </summary>
+    uint LineSpacing { get; set; }
+
+    /// <summary>
+    /// Gets or sets the character spacing.
+    /// </summary>
+    uint CharSpacing { get; set; }
+
+    /// <summary>
+    /// Gets or sets the sheet type used when setting text via <see cref="TextId"/>
+    /// </summary>
+    NodeData.SheetType SheetType { get; set; }
+
+    /// <summary>
+    /// Gets or sets the textId, this is a row in a <see cref="NodeData.SheetType"/>.
+    /// </summary>
+    uint TextId { get; set; }
+
+    /// <summary>
+    /// Gets or sets the displayed string.
+    /// </summary>
+    ReadOnlySeString String { get; set; }
+
+    /// <summary>
+    /// Adds the specified text flags.
+    /// </summary>
+    void AddTextFlags(params TextFlags[] flags);
+
+    /// <summary>
+    /// Removes the specified text flags.
+    /// </summary>
+    void RemoveTextFlags(params TextFlags[] flags);
+
+    /// <summary>
+    /// Sets the specified number using the provided formatting params.
+    /// </summary>
+    void SetNumber(int number, bool showCommas = false, bool showPlusSign = false, int digits = 0, bool zeroPad = false);
+
+    /// <summary>
+    /// Gets the size of the specified text if it were drawn with this nodes given params.
+    /// </summary>
+    Vector2 GetTextDrawSize(ReadOnlySeString text, bool considerScale = true);
+
+    /// <summary>
+    /// Gets the size of this nodes text.
+    /// </summary>
+    Vector2 GetTextDrawSize(bool considerScale = true);
+}
--- Nodes/TextNode.cs
+++ Nodes/TextNode.cs
@@ -3,6 +3,7 @@ using Dalamud.Utility;
 using FFXIVClientStructs.FFXIV.Component.GUI;
 using KamiToolKit.BaseTypes;
 using KamiToolKit.Classes;
+using KamiToolKit.Interfaces;
 using Lumina.Data.Parsing.Uld;
 using Lumina.Text.ReadOnly;
 
@@ -11,63 +12,39 @@ namespace KamiToolKit.Nodes;
 /// <summary>
 /// Implementation of the games TextNode.
 /// </summary>
-public unsafe class TextNode : NodeBase<AtkTextNode> {
+public unsafe class TextNode : NodeBase<AtkTextNode>, ITextNode {
 
-    /// <summary>
-    /// Gets or sets the text color.
-    /// </summary>
-    /// <remarks>
-    /// Expects values between 0.0f and 1.0f.
-    /// </remarks>
+    /// <inheritdoc />
     public Vector4 TextColor {
         get => Node->TextColor.ToVector4();
         set => Node->TextColor = value.ToByteColor();
     }
 
-    /// <summary>
-    /// Gets or sets the outline color.
-    /// </summary>
-    /// <remarks>
-    /// Expects values between 0.0f and 1.0f.
-    /// </remarks>
+    /// <inheritdoc />
     public Vector4 TextOutlineColor {
         get => Node->EdgeColor.ToVector4();
         set => Node->EdgeColor = value.ToByteColor();
     }
 
-    /// <summary>
-    /// Gets or sets the background color.
-    /// </summary>
+    /// <inheritdoc />
     public Vector4 BackgroundColor {
         get => Node->BackgroundColor.ToVector4();
         set => Node->BackgroundColor = value.ToByteColor();
     }
 
-    /// <summary>
-    /// Gets or sets the selection start.
-    /// </summary>
-    /// <remarks>
-    /// This is used in conjunction with <see cref="BackgroundColor"/> and <see cref="SelectEnd"/>.
-    /// </remarks>
+    /// <inheritdoc />
     public uint SelectStart {
         get => Node->SelectStart;
         set => Node->SelectStart = value;
     }
 
-    /// <summary>
-    /// Gets or sets the selection end.
-    /// </summary>
-    /// <remarks>
-    /// This is used in conjunction with <see cref="BackgroundColor"/> and <see cref="SelectStart"/>.
-    /// </remarks>
+    /// <inheritdoc />
     public uint SelectEnd {
         get => Node->SelectEnd;
         set => Node->SelectEnd = value;
     }
 
-    /// <summary>
-    /// Gets or sets the text alignment.
-    /// </summary>
+    /// <inheritdoc />
     public AlignmentType AlignmentType {
         get => Node->AlignmentType;
         set {
@@ -76,9 +53,7 @@ public unsafe class TextNode : NodeBase<AtkTextNode> {
         }
     }
 
-    /// <summary>
-    /// Gets or sets the used font.
-    /// </summary>
+    /// <inheritdoc />
     public FontType FontType {
         get => Node->FontType;
         set {
@@ -87,9 +62,7 @@ public unsafe class TextNode : NodeBase<AtkTextNode> {
         }
     }
 
-    /// <summary>
-    /// Gets or sets the text flags.
-    /// </summary>
+    /// <inheritdoc />
     public TextFlags TextFlags {
         get => Node->TextFlags;
         set {
@@ -98,9 +71,7 @@ public unsafe class TextNode : NodeBase<AtkTextNode> {
         }
     }
 
-    /// <summary>
-    /// Gets or sets the font size.
-    /// </summary>
+    /// <inheritdoc />
     public uint FontSize {
         get => Node->FontSize;
         set {
@@ -109,9 +80,7 @@ public unsafe class TextNode : NodeBase<AtkTextNode> {
         }
     }
 
-    /// <summary>
-    /// Gets or sets the vertical line spacing.
-    /// </summary>
+    /// <inheritdoc />
     public uint LineSpacing {
         get => Node->LineSpacing;
         set {
@@ -120,9 +89,7 @@ public unsafe class TextNode : NodeBase<AtkTextNode> {
         }
     }
 
-    /// <summary>
-    /// Gets or sets the character spacing.
-    /// </summary>
+    /// <inheritdoc />
     public uint CharSpacing {
         get => Node->CharSpacing;
         set {
@@ -131,25 +98,19 @@ public unsafe class TextNode : NodeBase<AtkTextNode> {
         }
     }
 
-    /// <summary>
-    /// Gets or sets the sheet type used when setting text via <see cref="TextId"/>
-    /// </summary>
+    /// <inheritdoc />
     public NodeData.SheetType SheetType {
         get => (NodeData.SheetType)Node->SheetType;
         set => Node->SheetType = (byte)value;
     }
 
-    /// <summary>
-    /// Gets or sets the textId, this is a row in a <see cref="NodeData.SheetType"/>.
-    /// </summary>
+    /// <inheritdoc />
     public uint TextId {
         get => Node->TextId;
         set => Node->TextId = value;
     }
 
-    /// <summary>
-    /// Gets or sets the displayed string.
-    /// </summary>
+    /// <inheritdoc />
     public ReadOnlySeString String {
         get => new(Node->GetText().AsSpan());
         set {
@@ -158,9 +119,7 @@ public unsafe class TextNode : NodeBase<AtkTextNode> {
         }
     }
 
-    /// <summary>
-    /// Gets or sets the nodes size, triggering a text update.
-    /// </summary>
+    /// <inheritdoc />
     public override Vector2 Size {
         get => base.Size;
         set {
@@ -169,33 +128,25 @@ public unsafe class TextNode : NodeBase<AtkTextNode> {
         }
     }
 
-    /// <summary>
-    /// Adds the specified text flags.
-    /// </summary>
+    /// <inheritdoc />
     public void AddTextFlags(params TextFlags[] flags) {
         foreach (var flag in flags) {
             TextFlags |= flag;
         }
     }
 
-    /// <summary>
-    /// Removes the specified text flags.
-    /// </summary>
+    /// <inheritdoc />
     public void RemoveTextFlags(params TextFlags[] flags) {
         foreach (var flag in flags) {
             TextFlags &= ~flag;
         }
     }
 
-    /// <summary>
-    /// Sets the specified number using the provided formatting params.
-    /// </summary>
+    /// <inheritdoc />
     public void SetNumber(int number, bool showCommas = false, bool showPlusSign = false, int digits = 0, bool zeroPad = false)
         => Node->SetNumber(number, showCommas, showPlusSign, (byte)digits, zeroPad);
 
-    /// <summary>
-    /// Gets the size of the specified text if it were drawn with this nodes given params.
-    /// </summary>
+    /// <inheritdoc />
     public Vector2 GetTextDrawSize(ReadOnlySeString text, bool considerScale = true) {
         using var builder = new RentedSeStringBuilder();
 
@@ -208,9 +159,7 @@ public unsafe class TextNode : NodeBase<AtkTextNode> {
         return new Vector2(sizeX, sizeY);
     }
 
-    /// <summary>
-    /// Gets the size of this nodes text.
-    /// </summary>
+    /// <inheritdoc />
     public Vector2 GetTextDrawSize(bool considerScale = true) {
         ushort sizeX = 0;
         ushort sizeY = 0;
